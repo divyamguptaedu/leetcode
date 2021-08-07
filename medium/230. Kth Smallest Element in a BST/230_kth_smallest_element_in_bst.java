6841ec558192daf8a13872f8c15b6bb6b8c803d7
Divyam Gupta <dgupta92@gatech.edu>
2021-08-07 16:38:47 +0530

    Add 230. Kth Smallest Element in BST

---
+++ medium/230. Kth Smallest Element in a BST/230_kth_smallest_element_in_bst.java
@@ -0,0 +1,37 @@
+"""
+Performance:
+Runtime: 1 ms, faster than 38.37% of Java online submissions for Kth Smallest Element in a BST.
+Memory Usage: 39.4 MB, less than 32.83% of Java online submissions for Kth Smallest Element in a BST.
+"""
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        inOrderTraverse(list, root);
+        return list.get(k - 1);
+    }
+
+    private void inOrderTraverse(List<Integer> list, TreeNode root) {
+    	if (root == null) {
+    		return;
+    	}
+    	inOrderTraverse(list, root.left);
+    	list.add(root.val);
+    	inOrderTraverse(list, root.right);
+    }
+}
