dea7b4c071ce4e8087d4061b1dc4f82023e47bfc
Divyam Gupta <dgupta92@gatech.edu>
2021-08-08 13:57:34 +0530

    Add 48. Rotate Image

---
+++ medium/48. Rotate Image/48_rotate_image.java
@@ -0,0 +1,29 @@
+"""
+Performance:
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Rotate Image.
+Memory Usage: 39.1 MB, less than 47.10% of Java online submissions for Rotate Image.
+"""
+
+class Solution {
+    public void rotate(int[][] matrix) {
+    	int temp;
+        for (int i = 0; i < matrix.length; i++) {
+        	for (int j = 0; j < i; j++) {
+        		temp = matrix[i][j];
+        		matrix[i][j] = matrix[j][i];
+        		matrix[j][i] = temp;
+        	}
+        }
+        for (int k = 0; k < matrix.length; k++) {
+        	int left = 0;
+        	int right = matrix.length - 1;
+        	while (left < right) {
+        		temp = matrix[k][left];
+        		matrix[k][left] = matrix[k][right];
+        		matrix[k][right] = temp;
+        		left++;
+        		right--;
+        	}
+        }
+    }
+}
