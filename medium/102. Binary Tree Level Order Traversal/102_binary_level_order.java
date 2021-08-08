8fac24b616c2399373b8a9003374567863f97b9a
Divyam Gupta <dgupta92@gatech.edu>
2021-08-08 16:59:20 +0530

    Add 102. Binary Level Order

---
+++ medium/102. Binary Tree Level Order Traversal/102_binary_level_order.java
@@ -0,0 +1,52 @@
+"""
+Performance:
+Runtime: 1 ms, faster than 57.53% of Java online submissions for Binary Tree Level Order Traversal.
+Memory Usage: 39.4 MB, less than 38.12% of Java online submissions for Binary Tree Level Order Traversal.
+"""
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+       	List<List<Integer>> result = new ArrayList<>();
+       	if (root == null) {
+       		return result;
+       	}
+       	List<Integer> level;
+       	Queue<TreeNode> queue = new ArrayDeque<>();
+       	queue.add(root);
+       	while (!queue.isEmpty()) {
+       		int size = queue.size();
+       		level = new ArrayList<>();
+       		while (size != 0) {
+       			TreeNode temp = queue.peek();
+       			level.add(temp.val);
+       			if (temp.left != null) {
+       				queue.add(temp.left);
+       			}
+       			if (temp.right != null) {
+       				queue.add(temp.right);
+       			}
+       			queue.poll();
+       			size--;
+       		}
+       		result.add(level);
+       	}
+       	return result;
+
+    }
+}
