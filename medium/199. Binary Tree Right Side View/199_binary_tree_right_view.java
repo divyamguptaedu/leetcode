f8ca04b2af53373112e2b494a69f8e85de7e4e94
Divyam Gupta <dgupta92@gatech.edu>
2021-08-09 14:30:32 +0530

    Add 199. Binary Tree Right View

---
+++ medium/199. Binary Tree Right Side View/199_binary_tree_right_view.java
@@ -0,0 +1,51 @@
+"""
+Performance:
+Runtime: 1 ms, faster than 75.58% of Java online submissions for Binary Tree Right Side View.
+Memory Usage: 37.8 MB, less than 35.75% of Java online submissions for Binary Tree Right Side View.
+"""
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+    	List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+
+        if (root == null) {
+        	return result;
+        }
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+        	int size = queue.size();
+        	for (int i = 1; i <= size; i++) {
+        		TreeNode node = queue.poll();
+        		if (node.left != null) {
+        			queue.add(node.left);
+        		}
+        		if (node.right != null) {
+        			queue.add(node.right);
+        		}
+        		if (i == size) {
+        			result.add(node.val);
+        		}
+        	}
+        }
+        return result;
+
+    }
+}
