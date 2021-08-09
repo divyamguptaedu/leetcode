09c7eba1dee28e5ac666a07ff930b9d83a4a8590
Divyam Gupta <dgupta92@gatech.edu>
2021-08-09 14:30:09 +0530

    Add 287. Find Duplicate Number

---
+++ medium/287. Find the Duplicate Number/287_find_duplicate.java
@@ -0,0 +1,19 @@
+"""
+Performance:
+Runtime: 22 ms, faster than 29.60% of Java online submissions for Find the Duplicate Number.
+Memory Usage: 52 MB, less than 89.93% of Java online submissions for Find the Duplicate Number.
+"""
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer, Boolean> hashMap = new HashMap<>();
+        for (int x : nums) {
+        	if (hashMap.get(x) == null) {
+        		hashMap.put(x, true);
+        	} else {
+        		return x;
+        	}
+        }
+        return -1;
+    }
+}
