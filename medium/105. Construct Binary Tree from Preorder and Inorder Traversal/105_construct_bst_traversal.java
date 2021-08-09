83073f34e35142efb305ff7796c205e027da6db4
Divyam Gupta <dgupta92@gatech.edu>
2021-08-09 17:35:19 +0530

    Add 105. Construct Binary Tree from Traversal

---
+++ medium/105. Construct Binary Tree from Preorder and Inorder Traversal/105_construct_bst_traversal.java
@@ -0,0 +1,51 @@
+"""
+Performance:
+Runtime: 1 ms, faster than 98.78% of Java online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+Memory Usage: 39.3 MB, less than 30.75% of Java online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+"""
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+	int index;
+	HashMap<Integer, Integer> indexMap;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        index = 0;
+        indexMap = new HashMap<>();
+
+        for (int i = 0; i < inorder.length; i++) {
+        	indexMap.put(inorder[i], i);
+        }
+
+	    int left = 0;
+	    int right = preorder.length - 1;
+
+	    return helper(preorder, left, right);
+	}
+
+	private TreeNode helper(int[] preorder, int left, int right) {
+		if (left > right) {
+			return null;
+		}
+		int tempVal = preorder[index++];
+		TreeNode root = new TreeNode(tempVal);
+		root.left = helper(preorder, left, indexMap.get(tempVal) - 1);
+		root.right = helper(preorder, indexMap.get(tempVal) + 1, right);
+		return root;
+	}
+}
