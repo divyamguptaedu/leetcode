17ff8267de5edb2f13e9ae960310d69f089baf28
Divyam Gupta <dgupta92@gatech.edu>
2021-08-10 15:33:37 +0530

    Add 337. House Robber III

---
+++ medium/337. House Robber III/337_house_robber_iii.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        return robHelper(root, false);
+    }
+
+    public int robHelper(TreeNode root, Boolean isParentRobbed) {
+    	if (root == null) {
+    		return 0;
+    	}
+    	if (isParentRobbed) {
+    		return robHelper(root.left, false) + robHelper(root.right, false);
+    	}
+
+    	return Math.max(robHelper(root.left, false) + robHelper(root.right, false), 
+    		robHelper(root.left, true) + robHelper(root.right, true) + root.val);
+    	
+    }
+}
