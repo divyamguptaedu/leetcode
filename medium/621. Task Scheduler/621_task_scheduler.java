ac1915d86e974fcf2764e066a841f961d79db47e
Divyam Gupta <dgupta92@gatech.edu>
2021-08-10 15:34:19 +0530

    Add 621. Task Scheduler

---
+++ medium/621. Task Scheduler/621_task_scheduler.java
@@ -0,0 +1,47 @@
+"""
+Performance:
+Runtime: 15 ms, faster than 34.28% of Java online submissions for Task Scheduler.
+Memory Usage: 39.6 MB, less than 87.03% of Java online submissions for Task Scheduler.
+"""
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if (n == 0) {
+        	return tasks.length;
+        }
+        HashMap<Character, Integer> valueMap = new HashMap<>();
+        for (int i = 0; i < tasks.length; i++) {
+        	if (valueMap.get(tasks[i]) == null) {
+        		valueMap.put(tasks[i], 1);
+        	} else {
+        		valueMap.put(tasks[i], valueMap.get(tasks[i]) + 1);
+        	}
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int x : valueMap.values()) {
+        	queue.add(x);
+        }
+        int maximum = queue.poll();
+        int idleTime = (maximum - 1) * n;
+
+        while (!queue.isEmpty()) {
+        	int current = queue.poll();
+        	if (current == maximum) {
+        		idleTime = idleTime + 1;
+        	}
+        	idleTime = idleTime - current;
+        }
+
+        int result;
+
+        if (idleTime > 0) {
+        	result = idleTime + tasks.length;
+        } else {
+        	result = tasks.length;
+        }
+
+        return result;
+
+    }
+}
