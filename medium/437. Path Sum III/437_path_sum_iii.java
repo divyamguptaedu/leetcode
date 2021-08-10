672ce4677a5f361758660634e51343b5993ac5df
Divyam Gupta <dgupta92@gatech.edu>
2021-08-10 18:14:25 +0530

    Add 437. Path Sum III

---
+++ medium/437. Path Sum III/437_path_sum_iii.java
@@ -0,0 +1,43 @@
+"""
+Performance:
+Runtime: 20 ms, faster than 38.14% of Java online submissions for Path Sum III.
+Memory Usage: 38.8 MB, less than 57.18% of Java online submissions for Path Sum III.
+"""
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+        	return 0;
+        }
+        int count = helper(root, targetSum);
+        return count + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+    }
+
+    private int helper(TreeNode root, int targetSum) {
+    	if (root == null) {
+    		return 0;
+    	}
+    	int count = 0;
+    	if (root.val == targetSum) {
+    		count++;
+    	}
+    	return count + helper(root.left, targetSum - root.val) + helper(root.right, targetSum - root.val);
+    }
+
+}
