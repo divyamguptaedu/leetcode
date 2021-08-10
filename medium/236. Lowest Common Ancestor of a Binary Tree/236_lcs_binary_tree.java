b744228fd60a6bef2d5ee9e1220fe41ce09bd517
Divyam Gupta <dgupta92@gatech.edu>
2021-08-10 18:13:46 +0530

    Add 236. Lowest Common Ancestor

---
+++ medium/236. Lowest Common Ancestor of a Binary Tree/236_lcs_binary_tree.java
@@ -0,0 +1,35 @@
+"""
+Performance:
+Runtime: 4 ms, faster than 100.00% of Java online submissions for Lowest Common Ancestor of a Binary Tree.
+Memory Usage: 41.5 MB, less than 21.40% of Java online submissions for Lowest Common Ancestor of a Binary Tree.
+"""
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {        
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        } else {
+            return null;
+        }
+    }
+} 
