1eb55a07906cf8c21eb88f6e862143dce39f16cf
Divyam Gupta <dgupta92@gatech.edu>
2021-08-10 15:33:14 +0530

    Add 11. Container with Most Water

---
+++ medium/11. Container With Most Water/11_container_most_water.java
@@ -0,0 +1,28 @@
+"""
+Performance:
+Runtime: 3 ms, faster than 81.96% of Java online submissions for Container With Most Water.
+Memory Usage: 52.7 MB, less than 46.81% of Java online submissions for Container With Most Water.
+"""
+
+class Solution {
+    public int maxArea(int[] height) {
+        int result = 0;
+    	if (height.length == 0 || height.length == 1) {
+    		return result;
+    	}
+    	int left = 0;
+    	int right = height.length - 1;
+
+    	while (right > left) {
+    		int temp = (right - left) * (Math.min(height[left], height[right]));
+    		if (height[right] > height[left]) {
+    			result = Math.max(result, temp);
+    			left++;
+    		} else {
+    			result = Math.max(result, temp);
+    			right--;
+    		}
+    	}
+    	return result;
+    }
+}
