e90dec30e6305bce572eb42bdec87262a3009174
Divyam Gupta <dgupta92@gatech.edu>
2021-08-11 15:36:27 +0530

    Add 98. Validate BST

---
+++ medium/98. Validate Binary Search Tree/98_validate_bst.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        boolean result = helper(root, null, null);
+        return result;
+    }
+
+    public boolean helper(TreeNode root, TreeNode minimum, TreeNode maximum) {
+        if (root == null) {
+            return true;
+        }
+        if (minimum != null && root.val <= minimum.val) {
+            return false;
+        }
+        if (maximum != null && root.val >= maximum.val) {
+            return false;
+        }
+        return helper(root.left, minimum, root) && helper(root.right, root, maximum);
+    }
+}
