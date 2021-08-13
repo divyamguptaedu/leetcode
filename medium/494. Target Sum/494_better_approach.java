557c6b73ce8c2d5fa3d97c002d9c996cc2f577e3
Divyam Gupta <dgupta92@gatech.edu>
2021-08-13 10:12:53 +0530

    Add 494. Target Sum

---
+++ medium/494. Target Sum/494_better_approach.java
@@ -0,0 +1,25 @@
+"""
+Performance: 
+Runtime: 26 ms, faster than 55.77% of Java online submissions for Target Sum.
+Memory Usage: 51.1 MB, less than 5.12% of Java online submissions for Target Sum.
+"""
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+    
+        int origin = 1000;
+        int store[][] = new int[nums.length + 1][2001];
+
+        store[0][origin + 0] = 1;
+        
+        for (int i = 1; i < store.length; i++) {
+            for (int j = 0; j < store[0].length; j++) {
+                if (store[i - 1][j] > 0) {
+                    store[i][j + nums[i - 1]] += store[i - 1][j];
+                    store[i][j - nums[i - 1]] += store[i - 1][j];
+                }
+            }
+        }
+        return store[nums.length][origin + target];
+    }
+}
---
+++ medium/494. Target Sum/494_initial_approach.java
@@ -0,0 +1,22 @@
+"""
+Performance:
+Runtime: 497 ms, faster than 16.69% of Java online submissions for Target Sum.
+Memory Usage: 36.7 MB, less than 72.87% of Java online submissions for Target Sum.
+"""
+
+public class Solution {
+    int count = 0;
+    public int findTargetSumWays(int[] nums, int target) {
+        helper(nums, 0, 0, target);
+        return count;
+    }
+    public void helper(int[] nums, int i, int sum, int target) {
+        if (i == nums.length) {
+            if (sum == target)
+                count++;
+        } else {
+            helper(nums, i + 1, sum + nums[i], target);
+            helper(nums, i + 1, sum - nums[i], target);
+        }
+    }
+}
