ec827ffeaaabf882949a4db37c7f9e850ada9253
Divyam Gupta <dgupta92@gatech.edu>
2021-08-13 14:16:15 +0530

    Add 56. Merge Intervals

---
+++ medium/56. Merge Intervals/56_merge_intervals.java
@@ -0,0 +1,25 @@
+"""
+Performance:
+Runtime: 5 ms, faster than 94.97% of Java online submissions for Merge Intervals.
+Memory Usage: 42 MB, less than 25.01% of Java online submissions for Merge Intervals.
+"""
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> result = new ArrayList<int[]>();
+        int x = intervals[0][0];
+        int y = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= y) {
+                y = Math.max(y, intervals[i][1]);
+            } else {
+                result.add(new int[]{x, y});
+                x = intervals[i][0];
+                y = intervals[i][1];
+            }
+        }
+        result.add(new int[]{x, y});
+        return result.toArray(new int[result.size()][]);
+    }
+}
