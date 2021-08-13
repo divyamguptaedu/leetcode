8c11c3ce90943e7844bab266ff890b4e075efa25
Divyam Gupta <dgupta92@gatech.edu>
2021-08-13 14:14:53 +0530

    Add 138. Copy List with Random Pointer

---
+++ medium/138. Copy List with Random Pointer/138_copy_list_with_random_pointer.java
@@ -0,0 +1,41 @@
+"""
+Performance:
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Copy List with Random Pointer.
+Memory Usage: 42.5 MB, less than 6.04% of Java online submissions for Copy List with Random Pointer.
+"""
+
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        HashMap<Node, Node> valueMap = new HashMap<>();
+        Node temp = head;
+        while (temp != null) {
+            valueMap.put(temp, new Node(temp.val));
+            temp = temp.next;
+        }
+        temp = head;
+        while (temp != null) {
+            valueMap.get(temp).next = valueMap.get(temp.next);
+            valueMap.get(temp).random = valueMap.get(temp.random);
+            temp = temp.next;
+        }
+        return valueMap.get(head);
+    }
+}
