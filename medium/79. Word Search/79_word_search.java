738b1254af48feea5f02bdb24055a4744984d590
Divyam Gupta <dgupta92@gatech.edu>
2021-08-13 21:04:17 +0530

    Add 79. Word Search

---
+++ medium/79. Word Search/79_word_search.java
@@ -0,0 +1,39 @@
+"""
+Performance: 
+Runtime: 122 ms, faster than 32.55% of Java online submissions for Word Search.
+Memory Usage: 38.7 MB, less than 28.14% of Java online submissions for Word Search.
+"""
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (word.charAt(0) == board[i][j] && searchletter(i, j, board, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean searchletter(int i, int j, char[][] board, String word,int position) {
+        if (position == word.length()) {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
+            return false;
+        }
+        if (word.charAt(position) != board[i][j]) {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '!';
+        if (searchletter(i + 1, j, board, word, position + 1)|| 
+        	searchletter(i - 1, j, board, word, position + 1)||
+          	searchletter(i, j + 1, board, word, position + 1)||
+            searchletter(i, j - 1, board, word, position + 1)) {
+        	return true;
+        }
+        board[i][j] = temp;
+        return false;
+    }
+}
