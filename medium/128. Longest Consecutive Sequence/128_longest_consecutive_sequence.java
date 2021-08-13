7494fe500f74bf6c54b2329c2e1b02946edb0b4f
Divyam Gupta <dgupta92@gatech.edu>
2021-08-13 10:10:30 +0530

    Add 128. Longest Consecutive Sequence

---
+++ medium/128. Longest Consecutive Sequence/128_longest_consecutive_sequence.java
@@ -0,0 +1,30 @@
+"""
+Performance:
+Runtime: 52 ms, faster than 36.87% of Java online submissions for Longest Consecutive Sequence.
+Memory Usage: 61.8 MB, less than 6.42% of Java online submissions for Longest Consecutive Sequence.
+"""
+
+public class Solution {
+
+    public int longestConsecutive(int[] num) {
+        int longest = 0;
+        Map<Integer, Integer> valueMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < num.length; i++) {
+            if (valueMap.containsKey(num[i])) {
+            	continue;
+            }
+            valueMap.put(num[i], 1);  
+            int start = num[i];
+            int end = num[i];
+            if(valueMap.containsKey(num[i] + 1))
+                end = num[i] + valueMap.get(num[i] + 1);
+            if(valueMap.containsKey(num[i] - 1))
+                start = num[i] - valueMap.get(num[i] - 1);
+            longest = Math.max(longest, end - start + 1);
+            valueMap.put(end, end - start + 1);
+            valueMap.put(start, end - start + 1);
+        }
+        return longest;
+    }
+    
+}
