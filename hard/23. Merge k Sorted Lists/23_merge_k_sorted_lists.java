f9018fedf1ba4a356600b3c8e631bd41a15c384d
Divyam Gupta <dgupta92@gatech.edu>
2021-08-15 21:28:01 +0530

    Add 23. Merge K Sorted Lists

---
+++ hard/23. Merge k Sorted Lists/23_merge_k_sorted_lists.java
@@ -0,0 +1,37 @@
+"""
+Performance:
+Runtime: 4 ms, faster than 79.92% of Java online submissions for Merge k Sorted Lists.
+Memory Usage: 40.5 MB, less than 67.72% of Java online submissions for Merge k Sorted Lists.
+"""
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue((a, b) -> (a.val - b.val));
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        for (ListNode node : lists) {
+        	if (node != null) {
+        		queue.offer(node);
+        	}
+        }
+
+        while (!queue.isEmpty()) {
+            tail.next = queue.poll();
+            tail = tail.next;
+            if (tail.next != null) {
+            	queue.offer(tail.next);
+            }
+        }
+        return head.next;
+    }
+}
