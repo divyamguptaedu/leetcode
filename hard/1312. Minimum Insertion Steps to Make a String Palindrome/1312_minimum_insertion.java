5e6ba778fa99e015691eaa4b518a0082f04efe09
Divyam Gupta <dgupta92@gatech.edu>
2021-08-17 21:49:40 +0530

    Add 1312. Minimum Insertion Steps to Make a String Palindrome

---
+++ hard/1312. Minimum Insertion Steps to Make a String Palindrome/1312_minimum_insertion.java
@@ -0,0 +1,28 @@
+class Solution {
+	public int minInsertions(String s) {
+        if (s == null || s.isEmpty()) {
+        	return 0;
+        }
+        int n = s.length();
+        int[][] array = new int[n][n];
+        for (int i = 0; i < n; i++) {
+        	array[i][i] = 1;
+        }
+        for (int k = 2; k <= n; k++) {
+            for (int i = 0; i < n - k + 1; i++) {
+                int j = i + k - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (k == 2) {
+                    	array[i][j] = 2;
+                    } else {
+                    	array[i][j] = array[i + 1][j - 1] + 2;
+                    }
+                } else {
+                    array[i][j] = Math.max(array[i][j - 1], array[i + 1][j]);
+                }
+            }
+        }
+        
+        return n - array[0][n - 1];
+    }
+}
