c8a771a6b03c563907e903a2f974c563b352d910
Divyam Gupta <dgupta92@gatech.edu>
2021-08-17 15:44:58 +0530

    Add 1448. Count Good Nodes in Binary Tree

---
+++ medium/1448. Count Good Nodes in Binary Tree/1448_count_good_nodes_in_binary_tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+	
+    int good;
+
+    public int goodNodes(TreeNode root) {
+        good = 0;
+        helper(root, Integer.MIN_VALUE);
+        return good;
+    }
+
+    void helper(TreeNode root, int max){
+        if (root == null) {
+        	return;
+        }
+        if (root.val >= max) {
+        	good++;
+        }
+        max = Math.max(max, root.val);
+        helper(root.left, max);
+        helper(root.right, max);
+    }
+}
+
