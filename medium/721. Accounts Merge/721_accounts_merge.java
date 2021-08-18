2220196be6e8b20bf4b0d32395810436830e53d7
Divyam Gupta <dgupta92@gatech.edu>
2021-08-18 12:32:42 +0530

    Add 721. Accounts Merge

---
+++ medium/721. Accounts Merge/721_accounts_merge.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, String> nameMap = new HashMap<>();
+        Map<String, Set<String>> graph = new HashMap<>();
+        Set<String> emails = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+        for (List<String> account : accounts){
+            String name = account.get(0);
+            for (int i = 1; i < account.size(); ++i){
+                String email = account.get(i);
+                nameMap.put(email, name);
+                emails.add(email);
+                graph.putIfAbsent(email, new HashSet<>());
+                if (i == 1)
+                    continue;
+                graph.get(email).add(account.get(i-1));
+                graph.get(account.get(i-1)).add(email);
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (String email : emails){
+            if (visited.contains(email))
+                continue;
+            List<String> temp = new ArrayList<>();
+            helper(email, graph, visited, temp);
+            Collections.sort(temp);
+            temp.add(0, nameMap.get(email));
+            result.add(temp);   
+        }
+        return result;
+    }
+    
+    private void helper(String cur, Map<String, Set<String>> graph, Set<String> visited, List<String> temp){
+        if (visited.contains(cur)) {
+            return;
+        }
+        visited.add(cur);
+        temp.add(cur);
+        for (String email : graph.get(cur)) {
+            helper(email, graph, visited, temp);
+        }
+    }
+}
