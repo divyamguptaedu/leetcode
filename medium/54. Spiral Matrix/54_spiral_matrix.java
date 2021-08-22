4e079d437de9f0a73b8b17c07defe601e6f39354
Divyam Gupta <dgupta92@gatech.edu>
2021-08-22 15:58:56 +0530

    Add 54. Spiral Matrix

---
+++ medium/54. Spiral Matrix/54_spiral_matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    boolean[][] visited;
+    List<Integer> res;
+    
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if(matrix == null || matrix.length == 0) return new ArrayList<>();
+        
+        visited = new boolean[matrix.length][matrix[0].length];
+        res = new ArrayList<>();
+        helper(0, 0, matrix, false);
+        return res;
+    }
+    
+    public void helper(int y, int x, int[][] matrix, boolean goingUp) {
+        if (y < 0 || y >= matrix.length || x < 0 || x >= matrix[y].length || visited[y][x]) {
+        	return;
+        }
+        res.add(matrix[y][x]);
+        visited[y][x] = true;
+        
+        if (goingUp) {
+            helper(y - 1, x, matrix, true);
+        }
+        
+        helper(y, x + 1, matrix, false);
+        helper(y + 1, x, matrix, false);
+        helper(y, x - 1, matrix, false);
+        helper(y - 1, x, matrix, true);
+    }
+}
