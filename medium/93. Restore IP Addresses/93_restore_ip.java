856655935517740fe86645822dbacc275beccc8a
Divyam Gupta <dgupta92@gatech.edu>
2021-08-22 16:00:21 +0530

    Add 92. Restore IP

---
+++ medium/93. Restore IP Addresses/93_restore_ip.java
@@ -0,0 +1,29 @@
+class Solution {
+	public List<String> restoreIpAddresses(String string) {
+	    List<String> result = new LinkedList<>();
+	    int[] path = new int[4];
+	    helper(result, string, 0,  path, 0);
+	    return result;
+	}
+
+	private void helper(List<String> account, String string, int index, int[] path,  int segment){
+	    if (segment == 4 && index == string.length()) {
+	        account.add(path[0] + "." + path[1] + "."+ path[2] + "." + path[3]);
+	        return;
+	    } else if (segment == 4 || index == string.length()) {
+	        return ;
+	    }
+	    
+	    for (int length = 1; length <= 3 && index + length <= string.length() ; length++) {
+	        int val = Integer.parseInt(string.substring(index, index + length));
+	        // range check, no leading 0.
+	        if (val > 255 || length >= 2  && string.charAt(index) == '0') {
+	            break; 
+	        }
+	            
+	        path[segment] = val;
+	        helper(account, string, index + length, path, segment + 1);
+	        path[segment] = -1; // for debug. 
+	    }
+	}
+}
