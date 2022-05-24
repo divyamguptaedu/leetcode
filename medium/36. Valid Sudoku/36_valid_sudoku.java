c6ed69f15a9dd2d333feb7d741f814c9e95eb523
Divyam Gupta <dgupta92@gatech.edu>
2022-05-23 18:26:51 -0700

    Add 36. Valid Sudoku

---
+++ medium/36. Valid Sudoku/36_valid_sudoku.java
@@ -0,0 +1,31 @@
+"""
+Performance:
+Runtime: 3 ms, faster than 80.46% of Java online submissions for Valid Sudoku.
+Memory Usage: 45.5 MB, less than 69.01% of Java online submissions for Valid Sudoku.
+"""
+
+class Solution {
+    
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][10];
+        boolean[][] col = new boolean[9][10];
+        boolean[][] section = new boolean[9][10];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                int k = board[i][j] - '0'; 
+                if (row[i][k] || col[j][k]) {
+                    return false;  
+                }
+                int g = 3 * (i / 3) + j / 3;
+                if (section[g][k]) {
+                    return false;
+                }
+                row[i][k] = col[j][k] = section[g][k] = true;
+            }
+        }
+        return true;
+    }
+}
