429320e479714758ea2588c0c896346ef6b7a0ef
Divyam Gupta <dgupta92@gatech.edu>
2022-06-08 21:43:38 -0700

    Add 222.

---
+++ medium/222. Count Complete Tree Nodes/222_countNodes.java
@@ -0,0 +1,32 @@
+"
+Performance:
+Runtime: 1 ms, faster than 19.83% of Java online submissions for Count Complete Tree Nodes.
+Memory Usage: 49.9 MB, less than 45.64% of Java online submissions for Count Complete Tree Nodes.
+"
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left + right + 1;
+        
+    }
+}
