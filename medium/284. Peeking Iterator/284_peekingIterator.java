6730fa424d7a7d6eacf183530f3beafac7798e73
Divyam Gupta <dgupta92@gatech.edu>
2022-06-12 23:35:37 -0700

    Add 284.

---
+++ medium/284. Peeking Iterator/284_peekingIterator.java
@@ -0,0 +1,38 @@
+"""
+Performance:
+Runtime: 10 ms, faster than 92.38% of Java online submissions for Peeking Iterator.
+Memory Usage: 42.5 MB, less than 68.21% of Java online submissions for Peeking Iterator.
+"""
+
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+class PeekingIterator implements Iterator<Integer> {
+    private List<Integer> list;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        list = new ArrayList<>();
+        // initialize any member here.
+        while (iterator.hasNext()){
+            list.add(iterator.next());
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return list.get(0);
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer val = list.get(0);
+        list.remove(0);
+        return val;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !list.isEmpty();
+    }
+}
