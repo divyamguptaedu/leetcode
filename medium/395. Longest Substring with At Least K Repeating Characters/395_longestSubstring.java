819002ed5689a6df2d070fbb481e11b7322bb409
Divyam Gupta <dgupta92@gatech.edu>
2022-06-14 22:05:15 -0700

    Add 395.

---
+++ medium/395. Longest Substring with At Least K Repeating Characters/395_longestSubstring.java
@@ -0,0 +1,32 @@
+"""
+Performance:
+Runtime: 5 ms, faster than 43.92% of Java online submissions for Longest Substring.
+Memory Usage: 92.4 MB, less than 15.89% of Java online submissions for Longest Substring.
+"""
+
+class Solution {
+    public int longestSubstring (String s, int k) {
+        return helper(s.toCharArray(), 0, s.length(), k);
+    }
+
+    int helper(char[] ch, int start, int end, int k) {
+        if (end - start < k) {
+            return 0;
+        }
+        int[] count = new int[26];
+        for (int i = start; i < end; i++) {
+            count[ch[i] - 'a']++;
+        }
+        for (int i = start; i < end; i++) {
+            if (count[ch[i] - 'a'] < k) {
+               int j = i + 1;
+            }
+            while (j < end && count[ch[j] - 'a'] < k) {
+                j++;
+            }
+            return Math.max(helper(ch, start, i, k), helper(ch, j, end, k));
+            }
+        }
+        return end - start;
+    }
+}
