d0b53e766ec16caa90272e61ba91acf2eba3e37c
Divyam Gupta <dgupta92@gatech.edu>
2022-07-04 23:45:39 -0700

    Add 701.

---
+++ medium/701. Insert into a Binary Search Tree/701_insertBST.java
@@ -0,0 +1,34 @@
+"
+Performance:
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Insert into a Binary Search Tree.
+Memory Usage: 54.3 MB, less than 50.85% of Java online submissions for Insert into a Binary Search Tree.
+"
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
