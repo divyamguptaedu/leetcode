ecf9ac2c3e503c636f471210ff557cd7d4257058
Divyam Gupta <dgupta92@gatech.edu>
2022-07-17 19:02:55 -0700

    Add 894.

---
+++ medium/894. All Possible Full Binary Trees/894_allPossibleFBT.java
@@ -0,0 +1,45 @@
+"
+Performance:
+Runtime: 6 ms, faster than 88.25% of Java online submissions for All Possible Full Binary Trees.
+Memory Usage: 59.4 MB, less than 36.03% of Java online submissions for All Possible Full Binary Trees.
+"
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        if (n % 2 == 0)  {
+            return new ArrayList<TreeNode>();
+        }
+        List<TreeNode> result = new ArrayList<>();
+        for (int i = 1; i < n; i += 2) { 
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(n - i - 1);
+            for (TreeNode leftNode : left) {
+                for (TreeNode rightNode : right) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = leftNode;
+                    root.right = rightNode;
+                    result.add(root);
+                }
+            }
+        }
+        if (n == 1) {
+            result.add(new TreeNode(0));
+        }
+        return result;
+    }
+}
