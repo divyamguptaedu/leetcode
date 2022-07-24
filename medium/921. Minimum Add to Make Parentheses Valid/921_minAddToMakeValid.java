7aaff44c33dbccd68a9a64e87dea7925bc34715b
Divyam Gupta <dgupta92@gatech.edu>
2022-07-23 21:23:08 -0400

    Add 921.

---
+++ medium/921. Minimum Add to Make Parentheses Valid/921_minAddToMakeValid.java
@@ -0,0 +1,24 @@
+"
+Performance:
+Runtime: 1 ms, faster than 89.16% of Java online submissions for Minimum Add to Make Parentheses Valid.
+Memory Usage: 41.7 MB, less than 71.90% of Java online submissions for Minimum Add to Make Parentheses Valid.
+"
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int count = 0;
+        int error = 0;
+        for (char letter : s.toCharArray()) {
+            if (letter == '(')
+                count++;
+            else {
+                if (count == 0) {
+                    error++;
+                } else {
+                    count--;
+                }
+            }
+        }
+        return error + count;
+    }
+}
