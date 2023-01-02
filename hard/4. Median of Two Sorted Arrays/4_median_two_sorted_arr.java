1563f0897220321caaa0e4a0b3eaf956ede40972
Divyam Gupta <dgupta92@gatech.edu>
2023-01-02 13:16:32 -0800

    Add 4.

--- hard/4. Median of Two Sorted Arrays/4_median_two_sorted_arr.java
+++ hard/4. Median of Two Sorted Arrays/4_median_two_sorted_arr.java
@@ -1,39 +1,29 @@
-"""
-Performance: 
-Runtime: 2 ms, faster than 99.88% of Java online submissions for Median of Two Sorted Arrays.
-Memory Usage: 39.7 MB, less than 98.87% of Java online submissions for Median of Two Sorted Arrays.
-"""
-
 class Solution {
-	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		// calculate the two medians;
-	    int index1 = 0;
-	    int index2 = 0;
-	    int med1 = 0;
-	    int med2 = 0;
-	    for (int i=0; i<=(nums1.length+nums2.length)/2; i++) {
-	        med1 = med2;
-	        if (index1 == nums1.length) {
-	            med2 = nums2[index2];
-	            index2++;
-	        } else if (index2 == nums2.length) {
-	            med2 = nums1[index1];
-	            index1++;
-	        } else if (nums1[index1] < nums2[index2] ) {
-	            med2 = nums1[index1];
-	            index1++;
-	        }  else {
-	            med2 = nums2[index2];
-	            index2++;
-	        }
-	    }
-
-	    // find their mean;
-	    // the median is the average of two numbers
-	    if ((nums1.length+nums2.length)%2 == 0) {
-	        return (float)(med1+med2)/2;
-	    }
-
-	    return med2;
-	}
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int beg = 0;
+        int end = nums1.length;
+        while (beg <= end) {
+            int part1 = (beg + end) / 2;
+            int part2 = (nums1.length + nums2.length + 1) / 2 - part1;
+            int maxL1 = part1 == 0 ? Integer.MIN_VALUE : nums1[part1 - 1];
+            int minR1 = part1 == nums1.length ? Integer.MAX_VALUE : nums1[part1];
+            int maxL2 = part2 == 0 ? Integer.MIN_VALUE : nums2[part2 - 1];
+            int minR2 = part2 == nums2.length ? Integer.MAX_VALUE : nums2[part2];
+            if (maxL1 <= minR2 && maxL2 <= minR1) {
+                if ((nums1.length + nums2.length) % 2 == 0) {
+                    return (Math.max(maxL1, maxL2) + Math.min(minR1, minR2)) / 2.0;
+                } else {
+                    return Math.max(maxL1, maxL2);
+                }
+            } else if (maxL1 > minR2) {
+                end = part1 - 1;
+            } else {
+                beg = part1 + 1;
+            }
+        }
+        return 1;
+    }
 }
