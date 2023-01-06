a84429f76598a12dd728d2e792de8e15dcc2ddd9
Divyam Gupta <dgupta92@gatech.edu>
2023-01-05 23:41:21 -0800

    Add 48.

--- medium/48. Rotate Image/48_rotate_image.java
+++ medium/48. Rotate Image/48_rotate_image.java
@@ -1,31 +1,23 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Rotate Image.
-Memory Usage: 39.1 MB, less than 47.10% of Java online submissions for Rotate Image.
-"""
-
 class Solution {
-    public void rotate(int[][] matrix) {
-    	int temp;
-        // swap all i and j, only matrix.length / 2 times.
+    public void rotate(int[][] matrix) {        
+        //transpose
         for (int i = 0; i < matrix.length; i++) {
-        	for (int j = 0; j < i; j++) {
-        		temp = matrix[i][j];
-        		matrix[i][j] = matrix[j][i];
-        		matrix[j][i] = temp;
-        	}
+            for (int j = i; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
         }
-        // reverse each row
-        for (int k = 0; k < matrix.length; k++) {
-        	int left = 0;
-        	int right = matrix.length - 1;
-        	while (left < right) {
-        		temp = matrix[k][left];
-        		matrix[k][left] = matrix[k][right];
-        		matrix[k][right] = temp;
-        		left++;
-        		right--;
-        	}
+        
+        //reverse each row
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
         }
+        
     }
 }
