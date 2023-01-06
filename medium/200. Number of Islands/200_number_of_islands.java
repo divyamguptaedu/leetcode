3da7165e40e7eb35b13f5e85ad74fb315b4f3d13
Divyam Gupta <dgupta92@gatech.edu>
2023-01-05 23:41:03 -0800

    Add 200.

--- medium/200. Number of Islands/200_number_of_islands.java
+++ medium/200. Number of Islands/200_number_of_islands.java
@@ -1,39 +1,29 @@
-"""
-Performance:
-Runtime: 1 ms, faster than 99.95% of Java online submissions for Number of Islands.
-Memory Usage: 41.7 MB, less than 27.65% of Java online submissions for Number of Islands.
-"""
-
 class Solution {
+    boolean[][] visited;
     public int numIslands(char[][] grid) {
-        if (grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
+        int count = 0;
         int m = grid.length;
         int n = grid[0].length;
-        int count = 0;
-
-        // make all adjacent 1s as X and increment count;
-        for (int i = 0; i < m ; i++) {
+        visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    count ++;
-                    checkAdjacent(grid, m, n, i, j);
+                if (grid[i][j] == '1' && markVisited(i, j, grid)) {
+                    count++;
                 }
             }
         }
         return count;
     }
-    private void checkAdjacent(char[][] grid, int m, int n, int i, int j) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') {
-            return;
-        }
-        grid[i][j] = 'X';
 
-        // check all adjacents;
-        checkAdjacent(grid, m, n, i - 1, j);
-        checkAdjacent(grid, m, n, i + 1, j);
-        checkAdjacent(grid, m, n, i, j - 1);
-        checkAdjacent(grid, m, n, i, j + 1);
+    public boolean markVisited(int i, int j, char[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0' || visited[i][j]) {
+            return false;
+        }
+        visited[i][j] = true;
+        markVisited(i+1, j, grid);
+        markVisited(i, j+1, grid);
+        markVisited(i-1, j, grid);
+        markVisited(i, j-1, grid);
+        return true;
     }
 }
