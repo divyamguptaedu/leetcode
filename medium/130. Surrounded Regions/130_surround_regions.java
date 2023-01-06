fb855bd9d06d22602bb01d64dab478065de58e90
Divyam Gupta <dgupta92@gatech.edu>
2023-01-05 23:40:54 -0800

    Add 130.

--- medium/130. Surrounded Regions/130_surround_regions.java
+++ medium/130. Surrounded Regions/130_surround_regions.java
@@ -1,55 +1,41 @@
-/*
-Runtime: 1 ms, faster than 99.64% of Java online submissions for Surrounded Regions.
-Memory Usage: 41.4 MB, less than 33.10% of Java online submissions for Surrounded Regions.
-*/
-
 class Solution {
     public void solve(char[][] board) {
-        int left = 0;
-        int right = board[0].length - 1;
-        int top = 0;
-        int bottom = board.length - 1; 
-        
-        for (int i = left; i <= right; i++) {
-            markBoundryRegions(board, top, i);
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
         }
-        
-        for (int i = top; i <= bottom; i++) {
-            markBoundryRegions(board, i, right);
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
         }
-        
-        for (int i = right; i >= left; i--) {
-            markBoundryRegions(board, bottom, i);
-        }
-        
-        for (int i = bottom; i >= top; i--) {
-            markBoundryRegions(board, i, left);
-        }
-        
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == '1') {
-                    board[i][j] = 'O';
-                } else if (board[i][j] == 'O') {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O'){
                     board[i][j] = 'X';
+                }
+                if (board[i][j] == '*'){
+                    board[i][j] = 'O';
                 }
             }
         }
-        
     }
-    
-    private void markBoundryRegions(char[][] board, int row, int col) {
-        
-        if (row < 0 || row >= board.length 
-            || col < 0 || col >= board[0].length 
-            || board[row][col] == 'X' || board[row][col] == '1') {
+
+    public void dfs(char[][] board, int i, int j){
+        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length) {
             return;
         }
-        
-        board[row][col] = '1';
-        markBoundryRegions(board, row, col + 1);
-        markBoundryRegions(board, row, col - 1);
-        markBoundryRegions(board, row + 1, col);
-        markBoundryRegions(board, row - 1, col);
+
+        if (board[i][j] == 'X' || board[i][j] == '*') {
+            return;
+        }
+
+        board[i][j] = '*';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
     }
 }
