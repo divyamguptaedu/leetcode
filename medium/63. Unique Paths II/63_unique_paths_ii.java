160aa168fe6e8b44dbe51176121293a8f5442ed6
Divyam Gupta <dgupta92@gatech.edu>
2023-01-05 23:41:38 -0800

    Add 63.

--- medium/63. Unique Paths II/63_unique_paths_ii.java
+++ medium/63. Unique Paths II/63_unique_paths_ii.java
@@ -9,37 +9,32 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if (obstacleGrid[0][0] == 1) {
-            return 0;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1)  {
+                dp[i][0] = 0;
+                break;  
+            } else
+                dp[i][0] = 1;
         }
         
-        obstacleGrid[0][0] = 1;
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[0][j] == 1)  {
+                dp[0][j] = 0;
+                break; 
+            } else
+                dp[0][j] = 1;
+        }
         
         for (int i = 1; i < m; i++) {
-            if ((obstacleGrid[i][0] == 0) && (obstacleGrid[i - 1][0] == 1)) {
-                obstacleGrid[i][0] = 1;
-            } else {
-                obstacleGrid[i][0] = 0;
-            }
-        }
-        
-        for (int i = 1; i < n; i++) {
-            if (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) {
-                obstacleGrid[0][i] = 1;
-            } else {
-                obstacleGrid[0][i] = 0;
-            }
-        }
-        
-        for (int i = 1; i < m ; i++) {
             for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 0) {
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
-                } else {
-                    obstacleGrid[i][j] = 0;
-                }
+                if (obstacleGrid[i][j] == 1) 
+                    dp[i][j] = 0;
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return obstacleGrid[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 }
