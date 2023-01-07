238153e37e902ef124ca050c8ff83a7c79803ef8
Divyam Gupta <dgupta92@gatech.edu>
2023-01-06 22:30:34 -0800

    Add 207.

--- medium/207. Course Schedule/207_course_schedule.java
+++ medium/207. Course Schedule/207_course_schedule.java
@@ -1,54 +1,43 @@
-"""
-Performance:
-Runtime: 7 ms, faster than 31.70% of Java online submissions for Course Schedule.
-Memory Usage: 44.5 MB, less than 12.36% of Java online submissions for Course Schedule.
-"""
-
-public class Solution {
-    
+class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // graph to track pointers;
-        ArrayList[] graph = new ArrayList[numCourses];
-        int[] degree = new int[numCourses];
-
-        // for cycle detection;
-        Queue queue = new LinkedList();
+        int[] indegree = new int[numCourses];
+        Queue<Integer> queue = new LinkedList<>();
         int count = 0;
         
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
-            graph[i] = new ArrayList();
-        }
-            
+            adjList.add(new ArrayList<>());
+        } 
+
         for (int i = 0; i < prerequisites.length; i++) {
-            degree[prerequisites[i][1]]++;
-            graph[prerequisites[i][0]].add(prerequisites[i][1]);
+            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
-        for (int i = 0; i < degree.length; i++) {
-            if (degree[i] == 0) {
-                queue.add(i);
-                count++;
+
+        for (int i = 0; i < numCourses; i++) {
+            for (int adj : adjList.get(i)) {
+                indegree[adj]++;
             }
         }
-        
-        // increase count till the queue is not empty;
-        while (queue.size() != 0) {
-            int course = (int) queue.poll();
-            for (int i = 0; i < graph[course].size(); i++) {
-                int pointer = (int) graph[course].get(i);
-                degree[pointer]--;
-                if (degree[pointer] == 0) {
-                    queue.add(pointer);
-                    count++;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            count++;
+            for (int adj : adjList.get(node)) {
+                indegree[adj]--;
+                if (indegree[adj] == 0) {
+                    queue.add(adj);
                 }
             }
         }
-
-        // if the courses are exactly what we want;
-        if (count == numCourses) {
+        if (count == numCourses) 
             return true;
-        }
-        else {
+        else
             return false;
-        }    
     }
 }
