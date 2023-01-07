4b9687231c49bcc03658e8b318ebe0397f09bbc5
Divyam Gupta <dgupta92@gatech.edu>
2023-01-06 22:30:25 -0800

    Add 128.

--- medium/128. Longest Consecutive Sequence/128_longest_consecutive_sequence.java
+++ medium/128. Longest Consecutive Sequence/128_longest_consecutive_sequence.java
@@ -1,34 +1,23 @@
-"""
-Performance:
-Runtime: 52 ms, faster than 36.87% of Java online submissions for Longest Consecutive Sequence.
-Memory Usage: 61.8 MB, less than 6.42% of Java online submissions for Longest Consecutive Sequence.
-"""
-
-public class Solution {
-
-    public int longestConsecutive(int[] num) {
-        int longest = 0;
-        Map<Integer, Integer> valueMap = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < num.length; i++) {
-            // remove duplicates;
-            if (valueMap.containsKey(num[i])) {
-            	continue;
-            }
-            // put all uniques in the hashmap;
-            valueMap.put(num[i], 1);  
-            int start = num[i];
-            int end = num[i];
-            if(valueMap.containsKey(num[i] + 1))
-                end = num[i] + valueMap.get(num[i] + 1);
-            if(valueMap.containsKey(num[i] - 1))
-                start = num[i] - valueMap.get(num[i] - 1);
-            longest = Math.max(longest, end - start + 1);
-            valueMap.put(end, end - start + 1);
-            valueMap.put(start, end - start + 1);
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0){
+            return 0;
         }
-        // return the longest;
-        return longest;
+        Arrays.sort(nums);
+
+        int longest = 1;
+        int current = 1;
+        for (int i = 1; i<nums.length;i++) {
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    current += 1;
+                }
+                else{
+                longest = Math.max(longest,current);
+                current = 1;
+                }
+            }
+        }
+        return Math.max(longest, current);
     }
-    
 }
