35bc5ad948f854dab08e3cb5087dc61f8f65ecd8
Divyam Gupta <dgupta92@gatech.edu>
2023-01-06 22:30:54 -0800

    Add 909.

--- medium/909. Snakes and Ladders/909_snakesAndLadders.java
+++ medium/909. Snakes and Ladders/909_snakesAndLadders.java
@@ -1,60 +1,50 @@
-"
-Performance:
-Runtime: 6 ms, faster than 91.90% of Java online submissions for Snakes and Ladders.
-Memory Usage: 46.9 MB, less than 73.49% of Java online submissions for Snakes and Ladders.
-"
-
 class Solution {
     public int snakesAndLadders(int[][] board) {
-        int size = board.length;
-        int index = 1;
-        int array[] = new int[size * size + 1];
-        int temp = 0;
-        if (size % 2 == 0) {
-            temp = 1;
-        }
+        int minSteps = 0;
+        int rows = board.length;
+        int start = 1;
+        int end = rows * rows;
+        boolean[][] isVisited = new boolean[rows][rows];
+        LinkedList<Integer> queue = new LinkedList<>();
 
-        for (int i = size - 1; i >= 0 ; i--) {
-           if (i % 2 == temp) {
-               for (int j = 0 ; j < size;  j++) {
-                   array[index] = board[i][j];
-                   index++;
-               }
-           } else {
-               for (int j = size - 1; j >= 0; j--) {
-                   array[index] = board[i][j];
-                   index++;
-               }
-           }
-       }
+        isVisited[rows - 1][0] = true;
+        queue.add(start);
 
-        return helper(array);
-    }
-
-    public int helper(int[] array) {
-        int steps = 0;
-        int size = array.length;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        boolean[] visited = new boolean[size + 1];
-        visited[1] = true;
         while (!queue.isEmpty()) {
-            int temp = queue.size();
-            while (temp --> 0) {
-                int curr = queue.poll();
-                if (curr == size - 1) {
-                    return (steps);
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int currentPosition = queue.pop();
+                if (currentPosition == end) {
+                    return minSteps;
                 }
-                for (int i = curr + 1; i <= curr + 6 && i < array.length; i++) {
-                    if (!visited[i]) {
-                        visited[i] = true;
-                        int done = array[i] == -1 ? i : array[i];
-                        queue.add(done);
+                for (int dice = 1; dice <= 6; dice++) {
+                    if (dice + currentPosition > end) {
+                        break;
+                    }
+                    int[] nextPosition = findCoordinates(currentPosition + dice, rows);
+                    int nextRow = nextPosition[0], nextColumn = nextPosition[1];
+                    if (!isVisited[nextRow][nextColumn]) {
+                        isVisited[nextRow][nextColumn] = true;
+                        if (board[nextRow][nextColumn] != -1) {
+                            queue.add(board[nextRow][nextColumn]);
+                        } else {
+                            queue.add(currentPosition + dice);
+                        }
                     }
                 }
             }
-            steps++;
+            minSteps++;
         }
         return -1;
+    }
+    
+    private int[] findCoordinates(int currentPosition, int n) {
+        int r = n - (currentPosition - 1) / n - 1;
+        int c = (currentPosition - 1) % n;
+        if (r % 2 == n % 2) {
+            return new int[]{r, n - 1 - c};
+        } else {
+            return new int[]{r, c};
+        }
     }
 }
