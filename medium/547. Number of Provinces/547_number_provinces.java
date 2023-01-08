3f6a88998ff6d72d5975bf878d0c480662165271
Divyam Gupta <dgupta92@gatech.edu>
2023-01-08 00:05:54 -0500

    Add 547.

--- medium/547. Number of Provinces/547_number_provinces.java
+++ medium/547. Number of Provinces/547_number_provinces.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int count = 0;
-        HashSet<Integer> visited = new HashSet<Integer>();
-        for (int i = 0; i < isConnected.length; i++)
-            if (helper(isConnected, visited, i)) {
-                count++;
+    public int findCircleNum(int[][] grid) {
+        boolean[] visited = new boolean[grid.length];
+        int c = 0;
+        for (int i = 0; i < grid.length; i++) {
+            if (!visited[i]) {
+                dfs(i,grid,visited);
+                c++;
             }
-        return count;
+        }
+        return c;
+    }
+
+    private void dfs(int j,int[][] grid,boolean[] visited) {
+        visited[j] = true;
+        for (int i = 0; i < grid[j].length; i++) {
+            if (!visited[i] && grid[j][i] == 1) {
+                dfs(i, grid, visited);
+            }
+        }
     }
     
-    // DFS
-    private boolean helper(int[][] isConnected, Set<Integer> visited, int node) {
-        if (visited.contains(node)) {
-            return false;
-        }
-        visited.add(node);
-        for (int i = 0; i < isConnected[node].length; i++)
-            if (isConnected[node][i] == 1) {
-                helper(isConnected, visited, i);
-            }
-        return true;
-    }
 }
