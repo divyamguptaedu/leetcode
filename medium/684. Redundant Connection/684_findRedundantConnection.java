524193eaa6cf4192257076335fa4ad317813a716
Divyam Gupta <dgupta92@gatech.edu>
2023-01-08 00:06:29 -0500

    Add 684.

--- medium/684. Redundant Connection/684_findRedundantConnection.java
+++ medium/684. Redundant Connection/684_findRedundantConnection.java
@@ -40,4 +40,39 @@ class Solution {
         }
         return false;
     }
+} 
+
+// other way
+
+class Solution {
+    int[] parent;
+    public int[] findRedundantConnection(int[][] edges) {
+        parent = new int[edges.length+1];
+        for (int i=0; i<parent.length; i++) {
+            parent[i] = i;
+        }
+        for (int[] edge : edges) {
+            if (find(edge[0]) == find(edge[1])) {
+                return edge;
+            }
+            union(edge[0], edge[1]);
+        }
+        return new int[]{-1,-1};
+    }
+    
+    private int find(int node) {
+        while (parent[node] != node) {
+            node = parent[node];
+        }
+        return node;
+    }
+    
+    private void union(int i, int j) {
+        int iRoot = find(i);
+        int jRoot = find(j);
+        
+        if (iRoot != jRoot) {
+            parent[jRoot] = iRoot;
+        }
+    }
 }
