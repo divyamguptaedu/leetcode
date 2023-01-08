70d91f86e4a0de6423783c1df9a2d0335c9a6c09
Divyam Gupta <dgupta92@gatech.edu>
2023-01-08 00:07:15 -0500

    Update

--- medium/785. Is Graph Bipartite?/785_isBipartite.java
+++ medium/785. Is Graph Bipartite?/785_isBipartite.java
@@ -34,4 +34,33 @@ class Solution {
         }
         return true;
     }
+} 
+
+// other way
+
+class Solution {
+    private boolean isSameColored(int node, int color, int[] vis, int[][] graph) {
+        vis[node] = color;
+        for (int it : graph[node]) {
+            if (vis[it] == 0) {
+                if (isSameColored(it, -color, vis, graph) == true)
+                    return true;
+            }
+            else if (vis[it] == color)
+                return true;
+        }
+        return false;
+        
+    }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] vis = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+                if (isSameColored(i, 1, vis, graph) == true)
+                    return false;
+            }
+        }
+        return true;
+    }
 }
