98a7a632d7ee60a01be1ba13a77b282173b5f316
Divyam Gupta <dgupta92@gatech.edu>
2023-01-10 22:22:07 -0500

    Add 497.

--- medium/497. Random Point in Non-overlapping Rectangles/498_pick.java
+++ medium/497. Random Point in Non-overlapping Rectangles/498_pick.java
@@ -1,34 +1,35 @@
-"
-Performance:
-Runtime: 114 ms, faster than 19.63% of Java online submissions for Random Point in Non-overlapping Rectangles.
-Memory Usage: 68.4 MB, less than 38.65% of Java online submissions for Random Point in Non-overlapping Rectangles.
-"
-
 class Solution {
+    
     Random random;
-    TreeMap<Integer,int[]> hashMap;
-    int total = 0;
+    TreeMap<Integer,int[]> map;
+    int areaSum = 0;
+    
     public Solution(int[][] rects) {
         this.random = new Random();
-        this.hashMap = new TreeMap<>();
-        for(int i = 0; i < rects.length; i++){
+        this.map = new TreeMap<>();
+        
+        for (int i = 0; i < rects.length; i++) {
             int [] rectangeCoordinates = rects[i];
-            int length = rectangeCoordinates[2] - rectangeCoordinates[0] + 1;
-            int breadth = rectangeCoordinates[3] - rectangeCoordinates[1] + 1;
-            total += length * breadth;
-            hashMap.put(total,rectangeCoordinates);
+            int l = rectangeCoordinates[2] - rectangeCoordinates[0] + 1 ;
+            int b = rectangeCoordinates[3] - rectangeCoordinates[1] + 1 ;
+            areaSum += l * b;
+            map.put(areaSum,rectangeCoordinates);
+            
         }
+        
     }
+    
     public int[] pick() {
-        int key = hashMap.ceilingKey(random.nextInt(total) + 1);
-        int[] rectangle = hashMap.get(key);
-        int length = rectangle[2] - rectangle[0] + 1 ;
-        int breadth = rectangle[3] - rectangle[1] + 1 ;
-        int xCord = rectangle[0] + random.nextInt(length);
-        int yCord = rectangle[1] + random.nextInt(breadth);
-        return new int[] {xCord, yCord};
+        int key = map.ceilingKey(random.nextInt(areaSum) + 1);
+        int [] rectangle = map.get(key);
+        int l = rectangle[2] - rectangle[0] + 1 ;
+        int b = rectangle[3] - rectangle[1] + 1 ;
+        int x = rectangle[0] + random.nextInt(l);
+        int y = rectangle[1] + random.nextInt(b);
+        return new int[]{x,y};
     }
 }
+
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(rects);
