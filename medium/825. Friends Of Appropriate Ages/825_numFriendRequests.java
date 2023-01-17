db2f68d790f4958a6015e8ea415743fa73187d07
Divyam Gupta <dgupta92@gatech.edu>
2023-01-16 23:08:58 -0500

    Add 994.

--- medium/825. Friends Of Appropriate Ages/825_numFriendRequests.java
+++ medium/825. Friends Of Appropriate Ages/825_numFriendRequests.java
@@ -1,24 +1,28 @@
-"
-Performance:
-Runtime: 4 ms, faster than 85.98% of Java online submissions for Friends Of Appropriate Ages.
-Memory Usage: 58 MB, less than 21.04% of Java online submissions for Friends Of Appropriate Ages.
-"
-
 class Solution {
     public int numFriendRequests(int[] ages) {
+        Arrays.sort(ages);
+        int n = ages.length;
+        int answer = 0;
         int[] count = new int[121];
-        int result = 0;
-        for (int age : ages) {
-            count[age]++;
-        }
-        for (int a = 0; a <= 120; a++) {
-            for (int b = (int)(0.5 * a) + 7 + 1; b <= a; b++) {
-                result += count[a] * count[b];
-                if (a == b) {
-                    result -= count[a];
+        for (int i = 0; i < n; i++) {
+            int low = 0;
+            int high = i - 1;
+            int result = -1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (0.5 * ages[i] + 7.0 >= ages[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                    result = mid;
                 }
             }
+            if (result != -1) {
+                answer += (i - result); 
+                answer += count[ages[i]];
+            }
+            count[ages[i]]++;
         }
-        return result;
+        return answer;
     }
 }
--- medium/994. Rotting Oranges/994_rotting_oranges.java
+++ medium/994. Rotting Oranges/994_rotting_oranges.java
@@ -1,45 +1,58 @@
-"
-Performance:
-Runtime: 3 ms, faster than 52.36% of Java online submissions for Rotting Oranges.
-Memory Usage: 39.2 MB, less than 16.51% of Java online submissions for Rotting Oranges.
-"
-
 class Solution {
-    
     public int orangesRotting(int[][] grid) {
         if (grid == null || grid.length == 0) {
-        	return -1;
+            return 0;
         }
-        
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        Queue<int[]> queue = new LinkedList<>();
+        int countFresh = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 if (grid[i][j] == 2) {
-                	helper(grid, i, j, 2);
+                    queue.offer(new int[] {i, j});
+                }
+                if (grid[i][j] == 1) {
+                    countFresh++;
                 }
             }
         }
-        
-        int minutes = 2;
-        for(int[] row : grid) {
-            for(int cell : row) {
-                if(cell == 1) return -1;
-                minutes = Math.max(minutes, cell);
+        if (countFresh == 0) {
+            return 0;
+        }
+        int time = 0;
+        int[] directionX = {0, 0, 1, -1};
+        int[] directionY = {1, -1, 0, 0};
+        int count = 0 - queue.size();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            count += size;
+            for (int i = 0; i < size; i++) {
+                int[] popped = queue.poll();
+                for (int j = 0; j < 4; j++) {
+                    int x = popped[0] + directionX[j];
+                    int y = popped[1] + directionY[j];
+                    if (x < 0 || y < 0 || x >= row || y >= col || grid[x][y] == 0 || grid[x][y] == 2) {
+                        continue;
+                    }
+                    grid[x][y] = 2;
+                    queue.offer(new int[] {x, y});
+                }
+            }
+            if (queue.size() != 0) {
+                time++;
             }
         }
-        
-        return minutes - 2;
-    }
-    
-    private void helper(int[][] grid, int i, int j, int minutes) {
-
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0|| (1 < grid[i][j] && grid[i][j] < minutes)) {
-        	return;
+        if (countFresh == count) {
+            return time;
         } else {
-            grid[i][j] = minutes;
-            helper(grid, i - 1, j, minutes + 1);
-            helper(grid, i + 1, j, minutes + 1);
-            helper(grid, i, j - 1, minutes + 1);
-            helper(grid, i, j + 1, minutes + 1);
+            return -1;
         }
     }
 }
+
+
+
+
+
