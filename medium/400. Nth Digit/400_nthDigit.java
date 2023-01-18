50b67430c9dedf5b6e5fb4ced79ea0da975dad7a
Divyam Gupta <dgupta92@gatech.edu>
2023-01-17 22:07:37 -0500

    Add 400.

--- medium/400. Nth Digit/400_nthDigit.java
+++ medium/400. Nth Digit/400_nthDigit.java
@@ -1,28 +1,22 @@
-"
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Nth Digit.
-Memory Usage: 40.4 MB, less than 79.27% of Java online submissions for Nth Digit.
-"
-
 class Solution {
     public int findNthDigit(int n) {
-        long nineMultiple = 9;
-        long count = 1;
-        long x = 1;
-        long limit = nineMultiple * count;
-        while (n > limit) {
-                n -= limit;
-                x = x * 10L;
-                nineMultiple *= 10L;
-                count +=1;
-                limit = nineMultiple * count;
+        if (n < 10) {
+            return n;
         }
-        long result = x + (n  - 1)  / count;
-        long temp =  (n  - 1)  % count;
-        for (int i = 0; i < count - temp -1; i++) {
-           result /= 10L;
+        double lastCount = 9;
+        double base = 10;
+        double previous = 0;
+        double count = 1;
+        while (n > lastCount) {
+            count++;
+            previous = lastCount;
+            lastCount += 9 * digitCount * base;
+            base *= 10 ;
         }
-        return (int)result % 10;
-    };
-    
+        double remainingCount = n - previous;
+        int whereItsAt = (int) ((base/10 - 1d) + Math.ceil(remainingCount / count));
+        double mod = remainingCount % count;
+        int whichChar = (int) ((mod == 0 ? count : mod) - 1d);
+        return Character.getNumericValue(String.valueOf(whereItsAt).charAt(whichChar));
+    }
 }
