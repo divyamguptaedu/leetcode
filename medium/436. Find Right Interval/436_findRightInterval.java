9bdcdb0bc4f014267f51d5f68a6b16de5a71fdb4
Divyam Gupta <dgupta92@gatech.edu>
2023-01-17 22:07:49 -0500

    Add 436.

--- medium/436. Find Right Interval/436_findRightInterval.java
+++ medium/436. Find Right Interval/436_findRightInterval.java
@@ -1,40 +1,19 @@
-"""
-Performance:
-Runtime: 28 ms, faster than 58.19% of Java online submissions for Find Right Interval.
-Memory Usage: 57.3 MB, less than 15.74% of Java online submissions for Find Right Interval.
-"""
-
 class Solution {
-    private Integer helper(int[][] intervals,int num) {
-        int start = 0;
-        int end = intervals.length - 1;
-        if (intervals[end][0] < num) {
-            return null;
+    public int[] findRightInterval(int[][] nums) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        int n = nums.length;
+        if (n == 1) {
+            return new int[]{-1};
         }
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (num == intervals[mid][0]) {
-                return num;
-            }
-            if (intervals[mid][0] < num) {
-                start = mid + 1; 
-            } else {
-                end = mid - 1;
-            }
+        for (int i = 0;i < n; i++) {
+            map.put(nums[i][0], i);
         }
-        return intervals[start][0];
-    }
-    public int[] findRightInterval(int[][] intervals) {
-        Map<Integer,Integer> valueMap = new HashMap<>();
-        int i = 0;
-        for (int[] interval : intervals) {
-            valueMap.put(interval[0], i++);
-        }
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
-        int result[] = new int[intervals.length];
-        for (int j = 0; j < intervals.length; j++) {
-            result[valueMap.get(intervals[j][0])]= valueMap.getOrDefault(helper(intervals,intervals[j][1]),-1);
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            Integer key = map.ceilingKey(nums[i][1]);
+            result[i] = key == null ? -1 : map.get(key);
         }
         return result;
+       
     }
 }
