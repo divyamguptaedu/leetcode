ed7a9ecba268ea11fb920bfc49df1b27bb2572bf
Divyam Gupta <dgupta92@gatech.edu>
2023-01-17 22:08:12 -0500

    Add 475.

--- medium/475. Heaters/475_heaters.java
+++ medium/475. Heaters/475_heaters.java
@@ -1,33 +1,33 @@
-"""
-Performance:
-Runtime: 33 ms, faster than 23.55% of Java online submissions for Heaters.
-Memory Usage: 53.8 MB, less than 66.58% of Java online submissions for Heaters.
-"""
-
 class Solution {
     public int findRadius(int[] houses, int[] heaters) {
+        int result = Integer.MIN_VALUE;
         Arrays.sort(heaters);
-        Arrays.sort(houses);
-        int result = 0;
-        for (int i = 0; i < houses.length; i++) {
-            int low = 0;
-            int high = heaters.length - 1;
-            long min = Long.MAX_VALUE;
-            while (low <= high) {
-                int mid = low - (low - high) / 2;
-                long temp = houses[i] - heaters[mid];
-                if (Math.abs(temp) < min) {
-                    min = Math.abs(temp);
-
-                }
-                if(temp > 0) {
-                    low = mid + 1; 
-                } else {
-                    high = mid-1;
-                }
-            }
-            result = Math.max(result,(int) min);
+        for (int house: houses) {
+            result = Math.max(Math.abs(heaters[nearestNumber(house, heaters)] - house), result);
         }
-        return result; 
+        return result;
+    }
+    static int nearestNumber(int n, int[] a) {
+        int low = 0;
+        int high = a.length - 1;
+        int nearestIndex = 0;
+        int distance = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (distance > Math.abs(a[mid] - n)) {
+                distance = Math.abs(a[mid] - n);
+                nearestIndex = mid;
+            }
+
+            if (a[mid] == n) {
+                return mid;
+            } else if (n<a[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return nearestIndex;
+
     }
 }
