b217f943cdcece08e8161175b02bb289e54dd60e
Divyam Gupta <dgupta92@gatech.edu>
2023-01-18 19:45:41 -0500

    Add 494.

--- medium/494. Target Sum/494_better_approach.java
+++ medium/494. Target Sum/494_better_approach.java
@@ -1,27 +1,21 @@
-"""
-Performance: 
-Runtime: 26 ms, faster than 55.77% of Java online submissions for Target Sum.
-Memory Usage: 51.1 MB, less than 5.12% of Java online submissions for Target Sum.
-"""
-
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-    
-        int origin = 1000;
-        int store[][] = new int[nums.length + 1][2001];
-
-        // store answers to increase runtime;
-        store[0][origin + 0] = 1;
-        
-        for (int i = 1; i < store.length; i++) {
-            for (int j = 0; j < store[0].length; j++) {
-                if (store[i - 1][j] > 0) {
-                    // store both increasing and decreasing sum;
-                    store[i][j + nums[i - 1]] += store[i - 1][j];
-                    store[i][j - nums[i - 1]] += store[i - 1][j];
-                }
-            }
+    public int findTargetSumWays(int[] nums, int s) {
+        Map<String, Integer> map = new HashMap();
+        return  helper(nums.length - 1, s, nums, map);
+    }
+    static int helper( int n, int s, int arr[], Map<String, Integer> map ){
+        if (n == -1 && s == 0) {
+            return 1;
         }
-        return store[nums.length][origin + target];
+        if (n <= -1 ) {
+            return 0;
+        }
+        if (map.containsKey(n +" "+ s)) {
+            return map.get(n +" "+ s);
+        }
+        int result_one = helper(n - 1, s - arr[n], arr, map);
+        int result_two = helper(n - 1, s + arr[n], arr, map);
+        map.put(n + " " + s, (result_one + result_two)); 
+        return (result_one + result_two);
     }
 }
