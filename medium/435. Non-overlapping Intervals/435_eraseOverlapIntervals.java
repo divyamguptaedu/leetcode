e16a0b5eeec3c3f0cf88c1b964c7a9723369154d
Divyam Gupta <dgupta92@gatech.edu>
2023-01-18 19:45:10 -0500

    Add 435.

--- medium/435. Non-overlapping Intervals/435_eraseOverlapIntervals.java
+++ medium/435. Non-overlapping Intervals/435_eraseOverlapIntervals.java
@@ -1,35 +1,25 @@
-"""
-Performance:
-Runtime: 105 ms, faster than 29.22% of Java online submissions for Non-overlapping Intervals.
-Memory Usage: 99.9 MB, less than 75.28% of Java online submissions for Non-overlapping Intervals.
-"""
-
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int size = intervals.length;
-        if (size == 1) {
+        int n = intervals.length;
+        if (n == 0) {
             return 0;
         }
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        Stack<int[]> stack = new Stack<>();
-        stack.add(intervals[0]);
-        int count = 0;
-        for (int i = 1; i < size; i++) {
-            int[] previous = stack.peek();
-            int[] current = intervals[i];
-            if (previous[1] > current[0]) {
-                if(previous[1] > current[1]) {
-                    stack.pop();
-                    stack.add(current);
-                    count++;
-                    continue;
-                } else {
-                    count++;
-                    continue;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        int i = 0;
+        int j = 1;
+        int temp = 0;
+        while (j < n) {
+            if (intervals[i][1] > intervals[j][0]) {
+                if (intervals[i][1] > intervals[j][1]) {
+                    i = j;
                 }
+                temp++;
+            } else {
+                i = j;
             }
-            stack.add(current);
+            j++;
         }
-        return count; 
+        
+        return temp;
     }
 }
