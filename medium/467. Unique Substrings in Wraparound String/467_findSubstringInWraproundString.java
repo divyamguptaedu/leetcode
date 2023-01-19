49b847a5974952a3e9edc30cff8a70b45c3db084
Divyam Gupta <dgupta92@gatech.edu>
2023-01-18 19:45:19 -0500

    Add 467.

--- medium/467. Unique Substrings in Wraparound String/467_findSubstringInWraproundString.java
+++ medium/467. Unique Substrings in Wraparound String/467_findSubstringInWraproundString.java
@@ -1,28 +1,22 @@
-"""
-Performance:
-Runtime: 4 ms, faster than 97.70% of Java online submissions for Unique Substrings in Wraparound String.
-Memory Usage: 42.2 MB, less than 87.10% of Java online submissions for Unique Substrings in Wraparound String.
-"""
-
-class Solution {
+ class Solution {
     public int findSubstringInWraproundString(String p) {
-        int size = p.length();
-        int result = 0;
-        int length = 1;
-        int[] counts = new int[26];
-        for (int i = 0; i < size; i++) {
-            char temp = p.charAt(i);
-            if (i > 0 && (temp - p.charAt(i-1) == 1 || temp - p.charAt(i-1) == -25)) {
-                length++;
+        int[] dp = new int[26];
+        int temp = 0;
+        for (int i=0;i<p.length();i++) {
+            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) == 1 || p.charAt(i - 1) - p.charAt(i) == 25)) {
+                temp++;
             } else {
-                length = 1;
+                temp = 1;
             }
-            int index = temp - 'a';
-            counts[index] = Math.max(counts[index], length);
+            int index = p.charAt(i) - 'a';
+            dp[index] = Math.max(dp[index], temp);
         }
-        for (int count : counts) {
-            result += count;
+        
+        int ans = 0;
+        for (int i : dp) {
+            ans += i;
         }
-        return result;
+        
+        return ans;
     }
 }
