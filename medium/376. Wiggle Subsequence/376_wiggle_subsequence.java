afde2c93fed2df9eafc2428190c112df393fce30
Divyam Gupta <dgupta92@gatech.edu>
2023-01-22 21:50:05 -0500

    Add 376.

--- medium/376. Wiggle Subsequence/376_wiggle_subsequence.java
+++ medium/376. Wiggle Subsequence/376_wiggle_subsequence.java
@@ -1,21 +1,17 @@
-"
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Wiggle Subsequence.
-Memory Usage: 39.8 MB, less than 86.79% of Java online submissions for Wiggle Subsequence.
-"
-
 class Solution {
     public int wiggleMaxLength(int[] nums) {
-        int size = nums.length;
-        int positive = 1;
-        int negative = 1;
-        for (int i = 1; i < size; i++) {
-            if (nums[i] > nums[i-1]) {
-                positive = negative+1;
-            } else if (nums[i]<nums[i-1]) {
-                negative = positive+1;
+        if(nums.length == 1) {
+            return 1;
+        }
+        int prevSign = nums[1] - nums[0]; 
+        int length = prevSign != 0 ? 2:1;
+        for(int i=2;i<nums.length;i++){
+            int currSign = nums[i] - nums[i-1]; 
+            if((currSign > 0 && prevSign <= 0) || (currSign < 0 && prevSign >= 0)){
+                length++;
+                prevSign = currSign;
             }
         }
-        return Math.max(positive, negative);
+        return length;
     }
 }
