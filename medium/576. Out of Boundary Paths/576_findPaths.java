7376902f622b8992fbd2bb71b49a7bc7b3456f0a
Divyam Gupta <dgupta92@gatech.edu>
2023-01-24 22:34:35 -0500

    Add 576.

--- medium/576. Out of Boundary Paths/576_findPaths.java
+++ medium/576. Out of Boundary Paths/576_findPaths.java
@@ -1,31 +1,32 @@
-"""
-Performance:
-Runtime: 7 ms, faster than 76.38% of Java online submissions for Out of Boundary Paths.
-Memory Usage: 43.5 MB, less than 23.62% of Java online submissions for Out of Boundary Paths.
-"""
-
 class Solution {
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        Integer[][][] result = new Integer[m][n][maxMove + 1];
-        return helper (m, n, maxMove, startRow, startColumn, result);
-    }
-    private int helper (int m, int n, int move, int i, int j, Integer[][][] result) {
-        int divider = 1000000007;
-        if (i < 0 || j < 0 || i >= m || j >= n) {
-            return 1;
-        }
-        if (move <= 0) {
+        if (maxMove == 0) {
             return 0;
         }
-        if (result[i][j][move] != null) {
-            return result[i][j][move];
+        int[][] dp = new int[m + 2][n + 2];
+        int[][] dpTwo = new int[m + 2][n + 2];
+
+        for (int i = 1; i <= m; i++) {
+            dp[i][1]++;
+            dp[i][n]++;
         }
-        int answer = 0;
-        answer = answer % divider + helper(m, n, move - 1, i - 1, j, result) % divider;
-        answer = answer % divider + helper(m, n, move - 1, i + 1, j, result) % divider;
-        answer = answer % divider + helper(m, n, move - 1, i, j - 1, result) % divider;
-        answer = answer % divider + helper(m, n, move - 1, i, j + 1, result) % divider;
-        result[i][j][move] = answer % divider;
-        return result[i][j][move];
+        for (int j = 1; j <= n; j++) {
+            dp[1][j]++;
+            dp[m][j]++;
+        }
+
+        int result = dp[startRow + 1][startColumn + 1];
+        for (int d = 1; d < maxMove; d++) {
+            int[][] x = dp;
+            dp = dpTwo;
+            dpTwo = x;
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dp[i][j] = (int)(((long)dpTwo[i - 1][j] + dpTwo[i + 1][j] + dpTwo[i][j - 1] + dpTwo[i][j + 1]) % 1000000007L);
+                }
+            }
+            result = (result + dp[startRow + 1][startColumn + 1]) % 1000000007;
+        }
+        return result;
     }
 }
