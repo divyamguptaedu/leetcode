31f3b4d2c232e1c7cfc0be72d5cfca7d9dcf0520
Divyam Gupta <dgupta92@gatech.edu>
2023-02-04 23:41:10 -0500

    Add 633.

--- medium/633. Sum of Square Numbers/633_judgeSquareSum.java
+++ medium/633. Sum of Square Numbers/633_judgeSquareSum.java
@@ -1,25 +1,20 @@
-"""
-Performance:
-Runtime: 7 ms, faster than 50.65% of Java online submissions for Sum of Square Numbers.
-Memory Usage: 41 MB, less than 50.85% of Java online submissions for Sum of Square Numbers.
-"""
-
 class Solution {
     public boolean judgeSquareSum(int c) {
-          long temp = c;
-          long left = 0 ;
-          long right = (long) Math.sqrt(c);
-          long sum = 0 ;
-          while (left <= right) {
-              sum = left * left + right * right;
-              if (sum == temp) {
+        long root = (long) Math.sqrt(c);
+        long low = 0;
+        long high = root;
+
+        while (low <= high) {
+            long sum = (low * low) + (high * high);
+            if (sum == (long) c) {
                 return true;
-              } else if (sum > temp) {
-                right--;
-              } else if (sum < temp) {
-                left++;
-              }
-          }
-          return false ;
+            }
+            if (sum > (long) c) {
+                high--;
+            } else {
+                low++;
+            }
+        }
+        return false;
     }
 }
