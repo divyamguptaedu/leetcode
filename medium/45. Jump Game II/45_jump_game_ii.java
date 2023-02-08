cd936a0f6ee807f2effc8240c7ea0c58bd36901f
Divyam Gupta <dgupta92@gatech.edu>
2023-02-07 23:33:33 -0500

    Add 45.

--- medium/45. Jump Game II/45_jump_game_ii.java
+++ medium/45. Jump Game II/45_jump_game_ii.java
@@ -1,26 +1,23 @@
-"""
-Performance:
-Runtime: 1 ms, faster than 99.31% of Java online submissions for Jump Game II.
-Memory Usage: 40 MB, less than 26.40% of Java online submissions for Jump Game II.
-"""
-
 class Solution {
+    public int jump(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int n = nums.length;
+        int maxJump = 0;
+        int positionAfterMaxJump = 0;
+        int jumps = 0;
 
-	// computed the minimum number of steps;
-	public int jump(int[] nums) {
-	    int end = 0;
-	    int result = 0;
-	    int i = 0;
-	    while (i < nums.length && end < nums.length - 1) {
-	        int max = 0;
-	        while (i <= end) {
-	            max = Math.max(max, nums[i] + i);
-	            i++;
-	        }
-	        end = max;
-	        result++;
-	    }
-	    return result;
-	}
-	
+        for (int i = 0; i < n - 1; i++) {
+            maxJump = Math.max(maxJump, i + nums[i]);
+            if (positionAfterMaxJump == i) {
+                jumps++;
+                positionAfterMaxJump = maxJump;
+            }
+            if (positionAfterMaxJump >= n -1) {
+                return jumps;
+            }
+        }
+        return jumps;
+    }
 }
