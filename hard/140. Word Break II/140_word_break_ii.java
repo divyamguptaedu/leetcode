6e1b35129aab8f9b084e29a67ca41a266ea5329c
Divyam Gupta <dgupta92@gatech.edu>
2023-02-08 21:08:10 -0500

    Add 140

--- hard/140. Word Break II/140_word_break_ii.java
+++ hard/140. Word Break II/140_word_break_ii.java
@@ -1,39 +1,32 @@
-"
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Word Break II.
-Memory Usage: 37.6 MB, less than 54.39% of Java online submissions for Word Break II.
-"
-
 class Solution {
-    public List<String> wordBreak(String wordToBreak, List<String> wordDict) {        
-        return helper(wordToBreak, wordDict, new HashMap<>());
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        HashSet set = new HashSet();
+        boolean[] dp =  new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 0; i < wordDict.size(); i++) {
+            set.add(wordDict.get(i));
+        }
+        List result = new ArrayList();
+        recursion("", result, 0, s, dp, set);
+        return result;
     }
     
-    public List<String> helper(String wordToBreak, List<String> wordDict, Map<String, List<String>> wordBrokenDict){
-        if (wordBrokenDict.containsKey(wordToBreak)) {
-            return wordBrokenDict.get(wordToBreak);
+    private void recursion(String path, List<String> result, int index, String s, boolean[] dp, Set set) {
+        if (index == s.length()) {
+            result.add(path);
+            return;
         }
-        
-        List<String> solutions = new ArrayList<>();
-        for (String word: wordDict) {
-            if (!wordToBreak.startsWith(word)) {
-                continue;
-            }
-            
-            if (wordToBreak.length() == word.length()) {
-                solutions.add(word);
-                continue;
-            }
-            
-            List<String> subSolutions = helper(wordToBreak.substring(word.length()), wordDict, wordBrokenDict);
-            for(String subSolution: subSolutions) {
-                StringBuilder strBuilder = new StringBuilder();
-                strBuilder.append(word).append(' ').append(subSolution);
-                solutions.add(strBuilder.toString());
+        for (int j = index; j <= s.length(); j++) {
+            if (dp[index] && set.contains(s.substring(index, j))) {
+                dp[j] = true;
+                String temp = path;
+                path += s.substring(index, j);
+                if (j != s.length()) {
+                    path += " ";
+                }
+                recursion(path, result, j, s, dp, set);
+                path = temp;
             }
         }
-        
-        wordBrokenDict.put(wordToBreak, solutions);
-        return solutions;
     }
 }
