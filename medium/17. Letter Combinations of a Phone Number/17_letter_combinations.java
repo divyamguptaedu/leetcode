f2db7a7585b262c6753f95a0b833a5b586f78f67
Divyam Gupta <dgupta92@gatech.edu>
2023-02-13 22:37:48 -0500

    Add 17.

--- medium/17. Letter Combinations of a Phone Number/17_letter_combinations.java
+++ medium/17. Letter Combinations of a Phone Number/17_letter_combinations.java
@@ -1,49 +1,49 @@
-"""
-Performance:
-Runtime: 4 ms, faster than 43.87% of Java online submissions for Letter Combinations of a Phone Number.
-Memory Usage: 39.5 MB, less than 14.34% of Java online submissions for Letter Combinations of a Phone Number.
-"""
-
 class Solution {
+	Map<Character, Character[]> digitToCharacter = new HashMap<>();
+	{
+	    digitToCharacter.put('2', new Character[]{'a', 'b', 'c'});
+	    digitToCharacter.put('3', new Character[]{'d', 'e', 'f'});
+	    digitToCharacter.put('4', new Character[]{'g', 'h', 'i'});
+	    digitToCharacter.put('5', new Character[]{'j', 'k', 'l'});
+	    digitToCharacter.put('6', new Character[]{'m', 'n', 'o'});
+	    digitToCharacter.put('7', new Character[]{'p', 'q', 'r', 's'});
+	    digitToCharacter.put('8', new Character[]{'t', 'u', 'v'});
+	    digitToCharacter.put('9', new Character[]{'w', 'x', 'y', 'z'});
+	}
 
-    public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if (digits.length() > 0) {
-        	allCombinations(digits, "", result);
-        }
-        return result;
-    }
+	public List<String> letterCombinations(String digits) {
+	    List<String> ans = new ArrayList<>();
+	    if (digits.isEmpty()) {
+	        return ans;
+	    } 
+	    int n = digits.length();
+	    int numberOfCombinations = calculateNumberOfCombinations(digits);
+	    for (int conbinationIndex = 0; conbinationIndex < numberOfCombinations; conbinationIndex++) {
+	        String combination = "";
+	        int temp = conbinationIndex;
+	        for (int digitIndex = n - 1; digitIndex >= 0; digitIndex--) {
+	            if (digits.charAt(digitIndex) == '7' || digits.charAt(digitIndex) == '9') {
+	                combination = digitToCharacter.get(digits.charAt(digitIndex))[temp % 4] + combination;
+	                temp /= 4;
+	            } else {
+	                combination = digitToCharacter.get(digits.charAt(digitIndex))[temp % 3] + combination;
+	                temp /= 3;
+	            }
+	        }
+	        ans.add(combination);
+	    }
+	    return ans;
+	}
 
-
-    private void allCombinations(String digits, String answer, List<String> result) {
-    	if (digits.length() == 0) {
-    		result.add(answer);
- 			return;
-    	}
-
-    	char number = digits.charAt(0);
-    	String temp = digits.substring(1);
-    	String code = digitCode(number);
-
-        // get all combinations;
-    	for (int i = 0; i < code.length(); i++) {
-    		allCombinations(temp, answer + code.charAt(i), result);
-    	}
-    }
-
-
-    // returns the string associated with that number of the phone;
-    private String digitCode(char digit) {
-    	switch (digit) {
-    		case '2' : return "abc";
-    		case '3' : return "def";
-    		case '4' : return "ghi";
-    		case '5' : return "jkl";
-    		case '6' : return "mno";
-    		case '7' : return "pqrs";
-    		case '8' : return "tuv";
-    		case '9' : return "wxyz";
-    	}
-        return "";
-    }
+	public int calculateNumberOfCombinations(String digits) {
+	    int ans = 1;
+	    for (int i = 0; i < digits.length(); i++) {
+	        if (digits.charAt(i) == '7' || digits.charAt(i) == '9') {
+	            ans *= 4;
+	        } else {
+	            ans *= 3;
+	        }
+	    }
+	    return ans;
+	}
 }
