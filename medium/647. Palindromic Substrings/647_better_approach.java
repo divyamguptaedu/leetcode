8795e203ef673551764eff9d8b72bf441899fa73
Divyam Gupta <dgupta92@gatech.edu>
2023-02-15 23:32:06 -0500

    Add 647.

--- medium/647. Palindromic Substrings/647_better_approach.java
+++ medium/647. Palindromic Substrings/647_better_approach.java
@@ -1,34 +1,20 @@
-"""
-Performance:
-isPalindrome function now uses recursion, instead of the pointer approach used initially.
-128/130 test cases passed with this approach. Need to optimize solution for longer strings like "s" * 1000.
-"""
-
 class Solution {
     public int countSubstrings(String s) {
-    	int count = 0;
-    	for (int i = 0; i < s.length(); i++) {
-    		for (int j = i + 1; j < s.length() + 1; j++) {
-                // get the substring and check if it's a palindrome
-    			if (isPalindrome(s.substring(i, j))) {
-    				count++;
-    			}
-    		}
-    	}
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += palindromeCount(s, i, i);
+            count += palindromeCount(s, i, i + 1);
+        }
         return count;
     }
 
-    // checks if the passed in string is a palindrome using recursion.
-    private boolean isPalindrome(String s) {
-
-        if (s.length() < 2) {
-            return true;
+    private int palindromeCount(String s, int left, int right) {
+        int numberOfPalindromes = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            numberOfPalindromes++;
+            left--;
+            right++;
         }
-        if (s.charAt(0) != s.charAt(s.length() - 1)) {
-            return false;
-        }
-
-        return isPalindrome(s.substring(1, s.length() - 1));
+        return numberOfPalindromes;
     }
-    
 }
