011886b1f6baad2a23cc91cc0fd290b9d84cee95
Divyam Gupta <dgupta92@gatech.edu>
2023-02-28 22:39:06 -0500

    Add 72.

--- hard/72. Edit Distance/72_edit_distance.java
+++ hard/72. Edit Distance/72_edit_distance.java
@@ -1,45 +1,28 @@
-"""
-Performance: 
-Runtime: 45 ms, faster than 27.65% of Java online submissions for Sliding Window Maximum.
-Memory Usage: 132.9 MB, less than 5.20% of Java online submissions for Sliding Window Maximum.
-"""
-
-public class Solution {
-
-    int[][] store;
-    
+class Solution {
     public int minDistance(String word1, String word2) {
-		store = new int[word1.length()][word2.length()];
-        return helper(word1, word2, 0, 0);
-    }
-    
-    private int helper(String word1, String word2, int index1, int index2) {
-        // check equality; 
-        if (index1 == word1.length()) {
-        	return word2.length() - index2;
-        }
-        if (index2 == word2.length()) {
-        	return word1.length() - index1;
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
         }
 
-        // use storage for optimization;
-		if (store[index1][index2] > 0) {
-			return store[index1][index2];
-		}
-        int result;
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
 
-        if (word1.charAt(index1) == word2.charAt(index2)) {
-            result = helper(word1, word2, index1 + 1, index2 + 1);
-        } else {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
+                }
+            }
+        }
 
-            // choose minimum of result and result after recursion on new index;
-            result = 1 + helper(word1, word2, index1 + 1, index2 + 1);
-            result = Math.min(result, 1 + helper(word1, word2, index1 + 1, index2));
-            result = Math.min(result, 1 + helper(word1, word2, index1, index2 + 1));
-        }  
-
-		store[index1][index2] = result;
-
-		return result;
+        return dp[m][n];
     }
 }
