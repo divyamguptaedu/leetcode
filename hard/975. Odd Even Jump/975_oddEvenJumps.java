906376c5881f6529f68bc977d70bbb8680d49639
Divyam Gupta <dgupta92@gatech.edu>
2023-03-13 23:37:30 -0400

    Add 975.

---
+++ hard/975. Odd Even Jump/975_oddEvenJumps.java
@@ -0,0 +1,32 @@
+class Solution {    
+    public int oddEvenJumps(int[] arr) {
+        int jumpsPossible = 1;
+        int len = arr.length;
+        
+        boolean[] isOddJumpPossible =  new boolean[len];
+        boolean[] isEvenJumpPossible =  new boolean[len];
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        
+        isOddJumpPossible[len-1] = true;
+        isEvenJumpPossible[len-1] = true;
+        map.put(arr[len-1], len-1);
+        
+        for (int i = len - 2; i >= 0; i--) {
+            Map.Entry<Integer, Integer> elementIfOddJump = map.ceilingEntry(arr[i]);
+            Map.Entry<Integer, Integer> elementIfEvenJump = map.floorEntry(arr[i]);
+            
+            if (elementIfOddJump != null) {
+                isOddJumpPossible[i] = isEvenJumpPossible[elementIfOddJump.getValue()]; 
+            }
+            if (elementIfEvenJump != null) {
+                isEvenJumpPossible[i] = isOddJumpPossible[elementIfEvenJump.getValue()];
+            }
+            if (isOddJumpPossible[i]) {
+                jumpsPossible++;
+            }
+            map.put(arr[i], i);
+        }
+        return jumpsPossible;
+    }
+}
