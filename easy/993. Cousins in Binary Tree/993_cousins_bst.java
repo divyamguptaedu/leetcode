73ffee16b35919a1a001126f47cf3246560edff7
Divyam Gupta <dgupta92@gatech.edu>
2024-02-01 21:53:52 -0800

    Add 993.

--- easy/993. Cousins in Binary Tree/993_cousins_bst.java
+++ easy/993. Cousins in Binary Tree/993_cousins_bst.java
@@ -56,3 +56,52 @@ class Solution {
         return false;
     }
 }
+
+//another solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        int xDepth = findDepth(root, x, 1);
+        int yDepth = findDepth(root, y, 1);
+        return !isSiblings(root, x, y) && (xDepth == yDepth);
+    }
+    public static boolean isSiblings(TreeNode root, int x, int y){
+        if (root == null || (root.left == null && root.right == null)){
+            return false;
+        }
+        if (root.left != null && root.right != null){
+            if (((root.left.val == x || root.right.val == x) && (root.left.val == y || root.right.val == y))){
+                return true;
+            }
+        } 
+        return isSiblings(root.left, x, y) || isSiblings(root.right, x, y);
+    }
+
+    public static int findDepth(TreeNode root, int x, int depth){
+        if (root == null || (root.left == null && root.right == null && root.val != x)){
+            return -1;
+        }
+        if (root.val == x){
+            return depth;
+        }
+        int leftCheck = findDepth(root.left, x, depth + 1);
+        int rightCheck = findDepth(root.right, x, depth + 1);
+        return leftCheck !=  -1 ? leftCheck : rightCheck;
+    }
+}
