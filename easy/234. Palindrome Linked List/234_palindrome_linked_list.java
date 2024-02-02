cb5d3bf0584323d08dfba1ec31711b7c5ec3eb79
Divyam Gupta <dgupta92@gatech.edu>
2024-02-01 21:53:37 -0800

    Add 234.

--- easy/234. Palindrome Linked List/234_palindrome_linked_list.java
+++ easy/234. Palindrome Linked List/234_palindrome_linked_list.java
@@ -38,3 +38,51 @@ class Solution {
         return true;
     }
 }
+
+//another solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode middle = head;
+        ListNode end = head;
+        while (end != null && end.next != null) {
+            middle = middle.next;
+            end = end.next.next;
+        } 
+        ListNode temp = reverseList(middle);
+        while (temp != null && head != null) {
+            if (temp.val != head.val) {
+                return false;
+            }
+            temp = temp.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    public ListNode reverseList(ListNode head) {        
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
+
