f2ce4b7a4a2f7634d69d4357347b50da0c6f4af7
Divyam Gupta <dgupta92@gatech.edu>
2024-02-04 23:45:18 -0800

    Add 637

--- easy/637. Average of Levels in Binary Tree/637_average_bst.java
+++ easy/637. Average of Levels in Binary Tree/637_average_bst.java
@@ -43,3 +43,51 @@ public class Solution {
         return result;
     }
 }
+
+//another solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Double> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
+        }
+
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            Double sum = 0.0;
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(sum / size);
+        }
+        return result;
+    }
+}
