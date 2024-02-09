b33e24ff02c1b4b196d47326e6fd72f7dcdbb8a8
Divyam Gupta <dgupta92@gatech.edu>
2024-02-08 19:26:39 -0800

    Add 110

--- easy/110. Balanced Binary Tree/110_balanced_binary_tree.java
+++ easy/110. Balanced Binary Tree/110_balanced_binary_tree.java
@@ -43,3 +43,45 @@ class Solution {
         return (Math.max(leftHeight, rightHeight) + 1);
     }
 }
+
+//another solution:
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if (Math.abs(left - right) > 1) {
+            return false;
+        }
+        boolean isLeft = isBalanced(root.left);
+        boolean isRight = isBalanced(root.right);
+        return isLeft && isRight;
+    }
+    public static int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        return 1 + Math.max(left, right);
+    }
+
+}
