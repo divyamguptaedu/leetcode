7a30ff47850414efcd19284800da81e334759e7b
Divyam Gupta <dgupta92@gatech.edu>
2024-02-08 19:25:40 -0800

    Add 121

--- easy/121. Best Time to Buy and Sell Stock/121_best_time_stock.java
+++ easy/121. Best Time to Buy and Sell Stock/121_best_time_stock.java
@@ -9,3 +9,23 @@ class Solution {
         return maximum;
     }
 }
+
+//another solution
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minimum = prices[0];
+        int x = 0; 
+        int result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (minimum > prices[i]) {
+                minimum = prices[i];
+            }
+            x = prices[i] - minimum;
+            if (result < x) {
+                result = x;
+            }
+        }
+        return result;
+    }
+}
