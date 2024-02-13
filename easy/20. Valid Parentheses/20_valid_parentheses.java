17e88487824c9b78f5f7aed42e9ae835f7322c8d
Divyam Gupta <dgupta92@gatech.edu>
2024-02-12 21:16:28 -0800

    Update 20

--- easy/20. Valid Parentheses/20_valid_parentheses.java
+++ easy/20. Valid Parentheses/20_valid_parentheses.java
@@ -32,3 +32,23 @@ class Solution {
         
     }
 }
+
+//another solution
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(map.get(c));
+            } else if (stack.empty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.empty();    
+    }
+}
