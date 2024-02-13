f0bf67cf32b8e61711aace8ea9b49fc1ec8f8caa
Divyam Gupta <dgupta92@gatech.edu>
2024-02-12 21:16:57 -0800

    Add 1570

--- medium/1570. Dot Product of Two Sparse Vector/1570_dot_product.java
+++ medium/1570. Dot Product of Two Sparse Vector/1570_dot_product.java
@@ -25,3 +25,59 @@ class SparseVector {
 // SparseVector v1 = new SparseVector(nums1);
 // SparseVector v2 = new SparseVector(nums2);
 // int ans = v1.dotProduct(v2);
+
+//another solution
+
+class SparseVector {
+
+    List<Pair<Integer, Integer>> data = new ArrayList<>();
+
+    SparseVector(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            data.add(new Pair(i, nums[i]));
+        }
+    }
+    
+    public int dotProduct(SparseVector vec) {
+        if (data.size() < vec.data.size()) {
+            return helper(this.data, vec.data);
+        } else {
+            return helper(vec.data, this.data);
+        }
+    }
+
+    public int helper(List<Pair<Integer, Integer>> sparse, List<Pair<Integer, Integer>> notSparse) {
+        int result = 0;
+        for (Pair<Integer, Integer> pair : sparse) {
+            int x = search(notSparse, pair.getKey());
+            if (x != -1) {
+                result += pair.getValue() * x;
+            }
+        }
+        return result;
+    }
+
+    public int search(List<Pair<Integer, Integer>> data, int i) {
+        int left = 0;
+        int right = data.size() -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (data.get(mid).getKey() == i) {
+                return data.get(mid).getValue();
+            } else if (data.get(mid).getKey() < i) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
