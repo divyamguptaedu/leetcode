0ce9f188b43e863f8550655f296ac247f39b09cd
Divyam Gupta <dgupta92@gatech.edu>
2024-02-17 22:10:15 -0800

    Add 1650

--- medium/1650. Lowest Common Ancestor of a Binary Tree III/1650_lca.java
+++ medium/1650. Lowest Common Ancestor of a Binary Tree III/1650_lca.java
@@ -33,3 +33,53 @@ class Solution {
         return temp;
     }
 }
+
+//another solution
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        HashSet<Node> set = new HashSet<>();
+        set.add(p);
+        if (set.contains(q)) {
+            return p;
+        }
+        set.add(q);
+        while (p != null && q != null) {
+            p = p.parent;
+            q = q.parent;
+            if (set.contains(p)) {
+                return p;
+            } else {
+                set.add(p);
+            }
+            if(set.contains(q)) {
+                return q;
+            } else {
+                set.add(q);
+            }
+        }
+        while (p != null) {
+            p = p.parent;
+            if (set.contains(p)) {
+                return p;
+            }
+        }
+        while (q != null) {
+            q = q.parent;
+            if (set.contains(q)) {
+                return q;
+            }
+        }
+        return null;
+    }
+}
