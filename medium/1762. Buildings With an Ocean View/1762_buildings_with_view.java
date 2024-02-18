b4106ba2288cc7d6f595918c383b3b4a19d0f69d
Divyam Gupta <dgupta92@gatech.edu>
2024-02-17 22:10:28 -0800

    Add 1762

--- medium/1762. Buildings With an Ocean View/1762_buildings_with_view.java
+++ medium/1762. Buildings With an Ocean View/1762_buildings_with_view.java
@@ -3,31 +3,25 @@ Performance:
 Runtime: 1 ms, faster than 100.00% of Java online submissions for Buildings With an Ocean View.
 Memory Usage: 55.2 MB, less than 45.83% of Java online submissions for Buildings With an Ocean View.
 "
-
 class Solution {
     public int[] findBuildings(int[] heights) {
-        boolean[] viewExists = new boolean[heights.length];
-        int[] result;
-        int resultLength = 0;
-        int maxHeight = 0;
-        
-        for (int i = heights.length - 1; i >= 0; i--) {
-            if (heights[i] > maxHeight) {
-                viewExists[i] = true;
-                resultLength++;
-                maxHeight = heights[i];
+        List<Integer> buildings = new ArrayList<>();
+        int highest = heights.length - 1;
+        buildings.add(highest);
+        for (int i = highest - 1; i >= 0; i--) {
+            if (heights[i] <= heights[highest]) {
+                continue;
             }
+            buildings.add(i);
+            highest = i;
         }
-        
-        result = new int[resultLength];
-        int index = 0;
-        
-        for (int i = 0; i < viewExists.length; i++) {
-            if (viewExists[i]) {
-                result[index] = i;
-                index++;
-            }
+
+        int size = buildings.size();
+        int[] result = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            result[i] = buildings.get(size - i - 1);
         }
+
         return result;
     }
 }
