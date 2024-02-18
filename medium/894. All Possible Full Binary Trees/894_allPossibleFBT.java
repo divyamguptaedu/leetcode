0d6a1fb9fb048143d84b5573828ced772a1d9cfa
Divyam Gupta <dgupta92@gatech.edu>
2024-02-17 22:10:37 -0800

    Add 894

--- medium/894. All Possible Full Binary Trees/894_allPossibleFBT.java
+++ medium/894. All Possible Full Binary Trees/894_allPossibleFBT.java
@@ -1,4 +1,4 @@
-"
+x"
 Performance:
 Runtime: 6 ms, faster than 88.25% of Java online submissions for All Possible Full Binary Trees.
 Memory Usage: 59.4 MB, less than 36.03% of Java online submissions for All Possible Full Binary Trees.
@@ -40,6 +40,53 @@ class Solution {
         if (n == 1) {
             result.add(new TreeNode(0));
         }
+        return result;
+    }
+}
+
+//another solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        List<TreeNode> result = new ArrayList<>();
+        if (n == 0 || n % 2 == 0) {
+            return result;
+        }
+        
+        if (n == 1) {
+            result.add(new TreeNode(0));
+            return result;
+        }
+        
+        for (int i = 1 ; i < n ; i += 2) {
+            int node = n - 1 - i;
+            List<TreeNode> leftTree = allPossibleFBT(i);
+            List<TreeNode> rightTree = allPossibleFBT(node);
+            for (TreeNode left : leftTree) {
+                for (TreeNode right : rightTree) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+        
         return result;
     }
 }
