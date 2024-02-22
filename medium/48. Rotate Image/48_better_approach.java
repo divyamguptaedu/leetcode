1402356597de602e1967bcc3993368e8abc6a292
Divyam Gupta <dgupta92@gatech.edu>
2024-02-21 22:09:27 -0800

    Add 48

--- medium/48. Rotate Image/48_better_approach.java
+++ medium/48. Rotate Image/48_better_approach.java
@@ -12,3 +12,24 @@ class Solution {
         }
     }
 }
+
+//another solution
+
+class Solution {
+    public void rotate(int[][] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int temp = nums[i][j];
+                nums[i][j] = nums[j][i];
+                nums[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length / 2; j++) {
+                int temp = nums[i][j];
+                nums[i][j] = nums[i][nums.length - 1 - j];
+                nums[i][nums.length - 1 - j] = temp;
+            }
+        }
+    }
+}
