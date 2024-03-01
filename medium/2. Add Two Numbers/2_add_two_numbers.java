c3bf32f04aab163c8b9e229c53032b8b54bc8030
Divyam Gupta <dgupta92@gatech.edu>
2024-02-29 19:57:44 -0800

    Add 2

--- medium/2. Add Two Numbers/2_add_two_numbers.java
+++ medium/2. Add Two Numbers/2_add_two_numbers.java
@@ -14,33 +14,38 @@ Memory Usage: 39.4 MB, less than 50.53% of Java online submissions for Add Two N
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new  ListNode();
+        ListNode temp = dummy;
+        int carry = 0;
 
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-	    int carryOver = 0;
-	    ListNode temp = new ListNode(0);
-	   	ListNode result = new ListNode(0);
-	    temp = result;
-
-	    // added and carrie over the remainder;
-	    while (l1 != null || l2 != null || carryOver != 0) {
-
-	        if (l1 != null) {
-	            carryOver += l1.val;
-	            l1 = l1.next;
-	        }
-	        if (l2 != null) {
-	            carryOver += l2.val;
-	            l2 = l2.next;
-	        }
-
-	        temp.next = new ListNode(carry % 10);
-	        carry = carry / 10;
-	        temp = temp.next;
-	        
-	    }
-
-	    return result.next;
-	}
+        while (l1! = null || l2 != null || carry == 1) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            temp.next = node;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
 }
