cb34e5035a785a477d968a77235a9db82dc1d5a0
Divyam Gupta <dgupta92@gatech.edu>
2024-02-29 19:58:11 -0800

    Add 5

--- medium/5. Longest Palindromic Substring/5_longest_palindromic_substring.java
+++ medium/5. Longest Palindromic Substring/5_longest_palindromic_substring.java
@@ -39,3 +39,40 @@ class Solution {
     }
     
 }
+
+//another solution
+
+class Solution {
+    public String longestPalindrome(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            List<Integer> indexes = helperGetIndexes(s.substring(i), s.charAt(i), i);
+            for (Integer index: indexes) {
+                if (isPalindrome(s.substring(i, index + 1))) {
+                    if (result.length() < s.substring(i, index + 1).length()) {
+                        result = s.substring(i, index + 1);
+                    }
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    private boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++ ) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private List<Integer> helperGetIndexes(String s, char letter, int buffer) {
+        List<Integer> indexes = new ArrayList();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == letter) {
+                indexes.add(i + buffer);
+            }
+        }
+        return indexes;
+    }
+}
