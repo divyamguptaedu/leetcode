c17bb7dbda5e4db1244c7a9bd1eaef0d9455e3bb
Divyam Gupta <dgupta92@gatech.edu>
2024-04-07 23:17:17 -0700

    Add 523

--- medium/523. Continuous Subarray Sum/523_continous_subarray_sum.java
+++ medium/523. Continuous Subarray Sum/523_continous_subarray_sum.java
@@ -1,26 +1,21 @@
-"
-Runtime: 46 ms, faster than 5.64% of Java online submissions for Continuous Subarray Sum.
-Memory Usage: 128.2 MB, less than 5.02% of Java online submissions for Continuous Subarray Sum.
-"
-
 class Solution {
-    public boolean checkSubarraySum(int[] nums, int k) {      
-        Map<Integer, Integer> map = new HashMap<>();
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap();
+        map.put(0, -1); //To account for sum to be 0. 
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            sum %= k; 
-            if (sum == 0 && i > 0) {
-                return true;
-            }
-            if (map.containsKey(sum) && i - map.get(sum) > 1) { 
-                return true;
-            }
-            if (!map.containsKey(sum)) {
-                map.put(sum, i); 
-            }
-            
+            int remainder = sum % k;
+            if (map.containsKey(remainder)) {
+                if ((i - map.get(remainder)) > 1) {
+                    return true;
+                } else {
+                    continue;
+                }
+            }   
+            map.put(remainder, i);
         }
+
         return false;
     }
 }
