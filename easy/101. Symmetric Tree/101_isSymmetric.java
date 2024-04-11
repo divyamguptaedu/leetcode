def6eadb8bb234e4395f7feaa3cc3f64551a49ad
Divyam Gupta <dgupta92@gatech.edu>
2024-04-10 21:51:23 -0700

    Add 101

---
+++ easy/101. Symmetric Tree/101_isSymmetric.java
@@ -0,0 +1,33 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root, root);
+    }
+
+    public boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return (left.val == right.val) && helper(left.right, right.left) && helper(left.left, right.right);
+    }
+}
