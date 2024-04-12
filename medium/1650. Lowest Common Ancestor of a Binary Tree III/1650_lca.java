930c1f81fb558e05506a7c774a050fb47ae8ea19
Divyam Gupta <dgupta92@gatech.edu>
2024-04-11 22:32:11 -0700

    Add 1650

--- medium/1650. Lowest Common Ancestor of a Binary Tree III/1650_lca.java
+++ medium/1650. Lowest Common Ancestor of a Binary Tree III/1650_lca.java
@@ -1,9 +1,3 @@
-"
-Performance:
-Runtime: 22 ms, faster than 42.41% of Java online submissions for Lowest Common Ancestor of a Binary Tree III.
-Memory Usage: 40 MB, less than 42.10% of Java online submissions for Lowest Common Ancestor of a Binary Tree III.
-"
-
 /*
 // Definition for a Node.
 class Node {
@@ -13,73 +7,24 @@ class Node {
     public Node parent;
 };
 */
-
+//Time Complexity: O(n)
+//Space Complexity: O(1)
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
-        HashSet<Node> parents = new HashSet<>();
-        Node temp = p;
-        while (temp != null) {
-            parents.add(temp);
-            temp = temp.parent;
-        }
-        temp = q;
-        while (temp != null) {
-            if (parents.contains(temp)) {
-                break;
+        Node pCopy = p;
+        Node qCopy = q;
+        while (pCopy != qCopy) {
+            if (pCopy.parent == null) {
+                pCopy = q;
             } else {
-                temp = temp.parent;
+                pCopy = pCopy.parent;
             }
-        }
-        return temp;
-    }
-}
-
-//another solution
-
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node parent;
-};
-*/
-
-class Solution {
-    public Node lowestCommonAncestor(Node p, Node q) {
-        HashSet<Node> set = new HashSet<>();
-        set.add(p);
-        if (set.contains(q)) {
-            return p;
-        }
-        set.add(q);
-        while (p != null && q != null) {
-            p = p.parent;
-            q = q.parent;
-            if (set.contains(p)) {
-                return p;
+            if (qCopy.parent == null) {
+                qCopy = p;
             } else {
-                set.add(p);
-            }
-            if(set.contains(q)) {
-                return q;
-            } else {
-                set.add(q);
+                qCopy = qCopy.parent;
             }
         }
-        while (p != null) {
-            p = p.parent;
-            if (set.contains(p)) {
-                return p;
-            }
-        }
-        while (q != null) {
-            q = q.parent;
-            if (set.contains(q)) {
-                return q;
-            }
-        }
-        return null;
+        return pCopy;
     }
 }
