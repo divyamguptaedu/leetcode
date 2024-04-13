46fb00a91cf86ceb7901e75b4b5e06b5d82ec9a0
Divyam Gupta <dgupta92@gatech.edu>
2024-04-12 23:13:57 -0700

    Add 129

--- medium/129. Sum Root to Leaf Numbers/129_sum_tree.java
+++ medium/129. Sum Root to Leaf Numbers/129_sum_tree.java
@@ -1,9 +1,3 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Sum Root to Leaf Numbers.
-Memory Usage: 36.6 MB, less than 53.26% of Java online submissions for Sum Root to Leaf Numbers.
-"""
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -19,21 +13,24 @@ Memory Usage: 36.6 MB, less than 53.26% of Java online submissions for Sum Root 
  *     }
  * }
  */
+//Time: O(n)
+//Space: O(h) : h is the height of the tree, recursion stack.
 class Solution {
+    int output = 0;
+    
     public int sumNumbers(TreeNode root) {
-       return helper(root, 0); 
+        preorder(root, 0);
+        return output;
     }
-    
-    private int helper(TreeNode node, int path) {
-        if (node == null) {
-            return 0;
+
+    public void preorder(TreeNode node, int numberSoFar) {
+        if (node != null) {
+            numberSoFar = numberSoFar * 10 + node.val;
+            if (node.left == null && node.right == null) { //add to the result if no childs present
+                output += numberSoFar;
+            }
+            preorder(node.left, numberSoFar);
+            preorder(node.right, numberSoFar) ;
         }
-        path = (path * 10) + node.val;
-        
-        if (node.left == null && node.right == null) {
-            return path;
-        }
-    
-        return helper(node.left, path) + helper(node.right, path);
-    }   
+    }
 }
