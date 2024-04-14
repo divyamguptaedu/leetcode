984cdef74afd9b0cb54fc1fe5ffe4fa6db5f795b
Divyam Gupta <dgupta92@gatech.edu>
2024-04-13 20:58:55 -0700

    Add 270

---
+++ easy/270. Closest Binary Search Tree Value/270_closestValue.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ //Time: O(h) h is the height of the tree
+ //Space: O(1)
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+        int currentValue = root.val;
+        int closestValue = root.val;
+        while (root != null) {
+            currentValue = root.val;
+            if (Math.abs(currentValue - target) < Math.abs(closestValue - target)) {
+                closestValue = currentValue;
+            }
+            if ((Math.abs(currentValue - target) == Math.abs(closestValue - target) && currentValue < closestValue)) {
+                closestValue = currentValue;
+            }
+            if (target < root.val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return closestValue;
+    }
+}
