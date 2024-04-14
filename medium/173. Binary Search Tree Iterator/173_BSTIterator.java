ec9003c40768d2bca1a4195279959b8beeb85c99
Divyam Gupta <dgupta92@gatech.edu>
2024-04-13 20:58:29 -0700

    Add 173

---
+++ medium/173. Binary Search Tree Iterator/173_BSTIterator.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+//Time: O(1) for the hasNext and next. O(n) for initialization.
+//Space: O(n)
+class BSTIterator {
+    ArrayList<Integer> list;
+    int index;
+
+    public BSTIterator(TreeNode root) {
+        list = new ArrayList<Integer>();
+        index = -1;
+        inorder(root);
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+
+    public int next() {
+        index++;
+        return list.get(index);
+    }
+
+    public boolean hasNext() {
+        return index + 1 < list.size();
+    }
+}
+
+
+
+
+
+
