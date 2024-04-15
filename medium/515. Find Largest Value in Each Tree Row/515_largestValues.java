a121c01c485a92f51a4f45ea6c54b22ee4239861
Divyam Gupta <dgupta92@gatech.edu>
2024-04-14 22:24:10 -0700

    Add 515

--- medium/515. Find Largest Value in Each Tree Row/515_largestValues.java
+++ medium/515. Find Largest Value in Each Tree Row/515_largestValues.java
@@ -1,9 +1,3 @@
-"
-Performance:
-Runtime: 2 ms, faster than 82.34% of Java online submissions for Find Largest Value in Each Tree Row.
-Memory Usage: 42.2 MB, less than 98.82% of Java online submissions for Find Largest Value in Each Tree Row.
-"
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -19,32 +13,36 @@ Memory Usage: 42.2 MB, less than 98.82% of Java online submissions for Find Larg
  *     }
  * }
  */
+
+ //Time: O(n)
+ //Space: O(n)
 class Solution {
-    List<Integer> result = new ArrayList<>();
     public List<Integer> largestValues(TreeNode root) {
         if (root == null) {
-            return result;
+            return new ArrayList<Integer>();
         }
+
+        List<Integer> result = new ArrayList<Integer>();
         Queue<TreeNode> queue = new LinkedList<>();
-        levelOrder(root, queue);
-        return result;
-    }
-    public void levelOrder(TreeNode root, Queue<TreeNode> queue) {
         queue.add(root);
+        
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            int maximum = Integer.MIN_VALUE;
-            for (int i = 0; i < size; i++) {
-                if (queue.peek().left != null) {
-                    queue.add(queue.peek().left);
+            int currentLength = queue.size();
+            int maxInThisRow = Integer.MIN_VALUE;
+            
+            for (int i = 0; i < currentLength; i++) {
+                TreeNode node = queue.remove();
+                maxInThisRow = Math.max(maxInThisRow, node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
                 }
-                if (queue.peek().right != null) {
-                    queue.add(queue.peek().right);
+                if (node.right != null) {
+                    queue.add(node.right);
                 }
-                maximum = Math.max(maximum, queue.remove().val);
             }
-            result.add(maximum);
+            result.add(maxInThisRow);
         }
         
+        return result;
     }
 }
