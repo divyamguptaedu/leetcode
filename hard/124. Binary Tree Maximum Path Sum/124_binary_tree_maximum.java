53ce8b2cd064bd1c0a2f00bfb8d26f3371b8bf29
Divyam Gupta <dgupta92@gatech.edu>
2024-04-15 23:24:55 -0700

    Add 124

--- hard/124. Binary Tree Maximum Path Sum/124_binary_tree_maximum.java
+++ hard/124. Binary Tree Maximum Path Sum/124_binary_tree_maximum.java
@@ -1,8 +1,3 @@
-"""
-Performance:
-Runtime: 1 ms, faster than 30.05% of Java online submissions for Binary Tree Maximum Path Sum.
-Memory Usage: 45.2 MB, less than 8.76% of Java online submissions for Binary Tree Maximum Path Sum.
-"""
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -18,42 +13,25 @@ Memory Usage: 45.2 MB, less than 8.76% of Java online submissions for Binary Tre
  *     }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//Time: O(n)
+//Space: O(n)
 class Solution {
+    int maxSum;
     public int maxPathSum(TreeNode root) {
-        int[] maxSum = {Integer.MIN_VALUE};
-        maxPathSum(root, maxSum);
-        return maxSum[0];
+        maxSum = Integer.MIN_VALUE;
+        helper(root);
+        return maxSum;
     }
-
-    public int maxPathSum(TreeNode root, int[] maxSum) {
+    private int helper(TreeNode root) {
         if (root == null) {
             return 0;
         }
+        int leftSum = helper(root.left);
+        leftSum = leftSum > 0 ? leftSum : 0;
+        int rightSum = helper(root.right);
+        rightSum = rightSum > 0 ? rightSum : 0;
 
-        int leftSum = Math.max(0, maxPathSum(root.left, maxSum));
-        int rightSum = Math.max(0, maxPathSum(root.right, maxSum));
-
-        maxSum[0] = Math.max(maxSum[0], leftSum + rightSum + root.val);
-
-        return Math.max(leftSum, rightSum) + root.val;
+        maxSum = Math.max(maxSum, leftSum + rightSum + root.val);
+        return Math.max(leftSum + root.val, rightSum + root.val);
     }
 }
-
-
-
-
