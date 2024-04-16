3c7cf9b166997d19c344406366b70f8287fc3b55
Divyam Gupta <dgupta92@gatech.edu>
2024-04-15 23:25:15 -0700

    Add 637

--- easy/637. Average of Levels in Binary Tree/637_average_bst.java
+++ easy/637. Average of Levels in Binary Tree/637_average_bst.java
@@ -20,7 +20,49 @@ Memory Usage: 40.9 MB, less than 80.69% of Java online submissions for Average o
  * }
  */
 public class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//Time: O(n)
+//Space: O(m) m is the maximum nodes in a level
+class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Double sum = 0.0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(sum / size);
+        }
+        return result;
+    }
+}
         List<Double> result = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
