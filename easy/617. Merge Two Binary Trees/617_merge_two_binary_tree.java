a386426068c6568a9e148409983a708da4e5b9f4
Divyam Gupta <dgupta92@gatech.edu>
2024-04-15 23:25:05 -0700

    Add 617

--- easy/617. Merge Two Binary Trees/617_merge_two_binary_tree.java
+++ easy/617. Merge Two Binary Trees/617_merge_two_binary_tree.java
@@ -1,9 +1,3 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Two Binary Trees.
-Memory Usage: 38.9 MB, less than 87.76% of Java online submissions for Merge Two Binary Trees.
-"""
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -19,25 +13,22 @@ Memory Usage: 38.9 MB, less than 87.76% of Java online submissions for Merge Two
  *     }
  * }
  */
+//Time: O(n)
+//Space: O(n)
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        // base case when there is a null on both the trees.
+        // Base Cases
         if (root1 == null && root2 == null) {
             return null;
-        } 
-        // base case when root1 is null.
+        }
         if (root1 == null) {
             return root2;
         }
-        // base case when root2 is null.
         if (root2 == null) {
             return root1;
         }
-
-        // update root1 by adding values.
-        root1.val = root1.val + root2.val;
-
-        // perform recursion on left root1 node and right root1 node.
+        //Main Logic
+        root1.val += root2.val;
         root1.left = mergeTrees(root1.left, root2.left);
         root1.right = mergeTrees(root1.right, root2.right);
         return root1;
