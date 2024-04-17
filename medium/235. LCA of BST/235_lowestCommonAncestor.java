8589422e0fdf0c7e9894f8261873bdbcfa18eb3a
Divyam Gupta <dgupta92@gatech.edu>
2024-04-16 19:50:26 -0700

    Add 235

--- medium/235. LCA of BST/235_lowestCommonAncestor.java
+++ medium/235. LCA of BST/235_lowestCommonAncestor.java
@@ -7,15 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
+//Time: O(n)
+//Space: O(1)
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int valueOfP = p.val;
+        int valueOfQ = q.val;
         TreeNode node = root;
         while (node != null) {
-            if (node.val > p.val && node.val > q.val) {
-                node = node.left;
-            } else if (node.val < p.val && node.val < q.val) {
+            int valueOfNode = node.val;
+            if (valueOfP > valueOfNode && valueOfQ > valueOfNode) {
                 node = node.right;
+            } else if (valueOfP < valueOfNode && valueOfQ < valueOfNode) {
+                node = node.left;
             } else {
                 return node;
             }
