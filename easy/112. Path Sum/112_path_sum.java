91776a9cf0f4ee7d0a2ea639d0256637aa6e7ef0
Divyam Gupta <dgupta92@gatech.edu>
2024-04-16 19:51:12 -0700

    Add 112

--- easy/112. Path Sum/112_path_sum.java
+++ easy/112. Path Sum/112_path_sum.java
@@ -1,8 +1,3 @@
-/*
-Runtime: 1 ms, faster than 46.73% of Java online submissions for Path Sum.
-Memory Usage: 43.7 MB, less than 48.38% of Java online submissions for Path Sum.
-*/
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -18,14 +13,19 @@ Memory Usage: 43.7 MB, less than 48.38% of Java online submissions for Path Sum.
  *     }
  * }
  */
+ //Time: O(n)
+ //Space: O(n)
 class Solution {
-    public boolean hasPathSum(TreeNode node, int tar) {
-        if (node == null) {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
             return false;
         }
-        if (node.left == null && node.right == null && tar - node.val == 0) {
-            return true;
+
+        sum -= root.val;
+        if ((root.left == null) && (root.right == null)) { //leaf node
+            return (sum == 0);
         }
-        return hasPathSum(node.left, tar - node.val) || hasPathSum(node.right, tar - node.val);
+
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 }
