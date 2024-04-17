2cb6410fa33f3435d35e981d7a449f194d84d9c2
Divyam Gupta <dgupta92@gatech.edu>
2024-04-16 19:51:02 -0700

    Add 111

--- easy/111. Minimum Depth of Binary Tree/111_minimum_depth.java
+++ easy/111. Minimum Depth of Binary Tree/111_minimum_depth.java
@@ -1,9 +1,4 @@
 /**
-Runtime: 1 ms, faster than 95.05% of Java online submissions for Minimum Depth of Binary Tree.
-Memory Usage: 43.8 MB, less than 67.58% of Java online submissions for Minimum Depth of Binary Tree.
-**/
-
-/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -18,37 +13,8 @@ Memory Usage: 43.8 MB, less than 67.58% of Java online submissions for Minimum D
  *     }
  * }
  */
-class Solution {
-    public int minDepth(TreeNode root) {
-        if (root == null){
-            return 0;
-        }
-        int leftNode = minDepth(root.leftNode);
-        int rightNode = minDepth(root.rightNode);
-        if (leftNode == 0 || rightNode == 0) {
-			return 1 + leftNode + rightNode;
-		}
-        return Math.min(leftNode, rightNode) + 1;
-    }
-}
-
-//another solution
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+ //Time: O(n)
+ //Space: O(n)
 class Solution {
     public int minDepth(TreeNode root) {
         if (root == null) {
@@ -56,6 +22,11 @@ class Solution {
         }
         int left = minDepth(root.left);
         int right = minDepth(root.right);
-        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+        
+        if (left == 0 || right == 0) {
+            return left + right + 1;
+        } else {
+            return Math.min(left, right) + 1;
+        }
     }
 }
