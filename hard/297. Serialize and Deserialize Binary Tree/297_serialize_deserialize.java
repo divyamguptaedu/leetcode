63729c9cdb21a9be6f680044e5d13cb58cf3ce08
Divyam Gupta <dgupta92@gatech.edu>
2024-04-17 23:33:28 -0700

    Add 297

--- hard/297. Serialize and Deserialize Binary Tree/297_serialize_deserialize.java
+++ hard/297. Serialize and Deserialize Binary Tree/297_serialize_deserialize.java
@@ -1,9 +1,3 @@
-"""
-Performance:
-Runtime: 10 ms, faster than 69.11% of Java online submissions for Serialize and Deserialize Binary Tree.
-Memory Usage: 40.6 MB, less than 82.28% of Java online submissions for Serialize and Deserialize Binary Tree.
-"""
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,110 +7,42 @@ Memory Usage: 40.6 MB, less than 82.28% of Java online submissions for Serialize
  *     TreeNode(int x) { val = x; }
  * }
  */
+ //Time: O(n)
+ //Space: O(n)
 public class Codec {
-
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        StringBuilder string = new StringBuilder();
-        serializeHelper(root, string);
-        String result = string.toString();
+        return serializerHelper(root, "");
+    }
+
+    public String serializerHelper(TreeNode root, String result) { //preorder traversal approach
+        if (root == null) {
+            result += "null,";
+        } else {
+            result += result.valueOf(root.val) + ","; //convert int to string
+            result = serializerHelper(root.left, result);
+            result = serializerHelper(root.right, result);
+        }
         return result;
     }
 
-    private void serializeHelper(TreeNode root, StringBuilder string) {
-    	if (root == null) {
-    		string.append("*" + " ");
-    		return;
-    	}
-    	string.append(root.val + " ");
-    	serializeHelper(root.left, string);
-    	serializeHelper(root.right, string);
-    }
-
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] array = data.split(" ");
-        int[] index = new int[1];
-        TreeNode root = deserializeHelper(array, index);
-        return root;
+        String[] stringNodesArray = data.split(",");
+        List<String> input = new LinkedList<String>(Arrays.asList(stringNodesArray));
+        return deserializerHelper(input);
     }
 
-    private TreeNode deserializeHelper(String[] array, int[] index) {
-    	if (index[0] > array.length || array[index[0]].equals("*")) {
-    		index[0]++;
-    		return null;
-    	}
-    	int i = index[0]++;
-    	int nodeValue = Integer.parseInt(array[i]);
-    	TreeNode root = new TreeNode(nodeValue);
-    	root.left = deserializeHelper(array, index);
-    	root.right = deserializeHelper(array, index);
-    	return root;
-    }
-}
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser = new Codec();
-// Codec deser = new Codec();
-// TreeNode ans = deser.deserialize(ser.serialize(root));
-
-// another way
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Codec {
-
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        if (root == null) {
-            return "";
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        StringBuilder result = new StringBuilder();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            if (node == null) {
-                result.append("n ");
-                continue;
-            }
-            result.append(node.val + " ");
-            queue.add(node.left);
-            queue.add(node.right);
-        }
-        return result.toString();
-    }
-
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        if (data == "") {
+    public TreeNode deserializerHelper(List<String> input) {
+        if (input.get(0).equals("null")) {
+            input.remove(0);
             return null;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        String[] values = data.split(" ");
-        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-        queue.add(root);
-        for (int i = 1; i < values.length; i++) {
-            TreeNode parent = queue.poll();
-            if (!values[i].equals("n")) {
-                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
-                parent.left = left;
-                queue.add(left);
-            }
-            i++;
-            if (!values[i].equals("n")) {
-                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
-                parent.right = right;
-                queue.add(right);
-            }
-        }
+
+        TreeNode root = new TreeNode(Integer.valueOf(input.get(0))); //convert string to int
+        input.remove(0);
+        root.left = deserializerHelper(input);
+        root.right = deserializerHelper(input);
         return root;
     }
 }
@@ -125,5 +51,3 @@ public class Codec {
 // Codec ser = new Codec();
 // Codec deser = new Codec();
 // TreeNode ans = deser.deserialize(ser.serialize(root));
-
-
