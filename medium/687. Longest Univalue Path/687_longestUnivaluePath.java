c469d10b914a0e0fbd39389e7eb8aaf72ea866da
Divyam Gupta <dgupta92@gatech.edu>
2024-04-17 23:33:39 -0700

    Add 687

--- medium/687. Longest Univalue Path/687_longestUnivaluePath.java
+++ medium/687. Longest Univalue Path/687_longestUnivaluePath.java
@@ -1,9 +1,3 @@
-"
-Performance:
-Runtime: 11 ms, faster than 86.69% of Java online submissions for Longest Univalue Path.
-Memory Usage: 71.4 MB, less than 95.22% of Java online submissions for Longest Univalue Path.
-"
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -19,37 +13,26 @@ Memory Usage: 71.4 MB, less than 95.22% of Java online submissions for Longest U
  *     }
  * }
  */
-
+//Time: O(n)
+//Space: O(n)
 class Solution {
-    int maximum = 0;
+    int result = 0;
     public int longestUnivaluePath(TreeNode root) {
-        helper(root, Integer.MIN_VALUE);
-        return maximum > 0 ? maximum - 1 : 0;
+        helper(root, -1);
+        return result;
     }
-    
-    private int[] helper(TreeNode root, int temp) {
-        if (root == null) {
-            return new int[]{Integer.MAX_VALUE, 0};
+    int helper(TreeNode currentNode, int parent) {
+        if (currentNode == null) {
+            return 0;
         }
-        int current = root.val;
-        int[] left = helper(root.left, current);
-        int[] right = helper(root.right, current);
-        
-        if (left[0] == current && right[0] == current) {
-            maximum = Math.max(maximum, left[1] + right[1] + 1); 
-        } else if (left[0] == current) {
-            maximum = Math.max(maximum, left[1] + 1);
-            right[1] = 0;
-        } else if (right[0] == current) {
-            maximum = Math.max(maximum, right[1] + 1);
-            left[1] = 0;
+        int left = helper(currentNode.left, currentNode.val);
+        int right = helper(currentNode.right, currentNode.val);
+        result = Math.max(result, left + right);
+
+        if (currentNode.val == parent) {
+            return Math.max(left, right) + 1;
         } else {
-            left[1] = 0;
-            right[1] = 0;
+            return 0;
         }
-        
-        int result = Math.max(left[1], right[1]) + 1;
-        
-        return new int[]{current, result};
     }
 }
