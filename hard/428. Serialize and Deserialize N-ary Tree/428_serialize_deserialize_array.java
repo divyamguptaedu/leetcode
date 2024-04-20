31c0691e53da5092ab0c3410cb1101f17f955597
Divyam Gupta <dgupta92@gatech.edu>
2024-04-19 23:02:39 -0700

    Add 428

--- hard/428. Serialize and Deserialize N-ary Tree/428_serialize_deserialize_array.java
+++ hard/428. Serialize and Deserialize N-ary Tree/428_serialize_deserialize_array.java
@@ -1,9 +1,3 @@
-"""
-Performance:
-Runtime: 13 ms, faster than 28.77% of Java online submissions for Serialize and Deserialize N-ary Tree.
-Memory Usage: 40.7 MB, less than 67.55% of Java online submissions for Serialize and Deserialize N-ary Tree.
-"""
-
 /*
 // Definition for a Node.
 class Node {
@@ -22,77 +16,52 @@ class Node {
     }
 };
 */
-
+//Time: O(n)
+//Space: O(n)
 class Codec {
-
+    int index = 0;
+    // Encodes a tree to a single string.
     public String serialize(Node root) {
-        if(root == null) {
+        if (root == null) {
             return "";
         }
-        Queue<Node> queue = new LinkedList<>();
-        Node end = new Node(), child = new Node();
-        queue.add(root); 
-        queue.add(end);
-        StringBuilder stringBuilder = new StringBuilder();
-        while(!queue.isEmpty()){
-            Node node = queue.poll();
-            if(node == end){
-                stringBuilder.append("/");
-                if(!queue.isEmpty())
-                    queue.add(end);
-            }
-            else if(node == child){
-                stringBuilder.append("*");
-            }else {
-                stringBuilder.append(node.val + ",");
-                for(Node c : node.children)
-                    queue.add(c);
-                if(queue.peek()!= end)
-                    queue.add(child);
-            }
-        }
-        return stringBuilder.toString();
+        StringBuilder sb = new StringBuilder();
+        helperSerialize(root, sb);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
     
+    public void helperSerialize(Node curr, StringBuilder sb) {
+        if (curr == null) {
+            return;
+        }
+        sb.append(curr.val + ",");
+        sb.append(curr.children.size() + ",");
+        for (Node child: curr.children) {
+            helperSerialize(child, sb);
+        }
+    }
+	
+    // Decodes your encoded data to tree.
     public Node deserialize(String data) {
-        if (data.isEmpty()) {
+        if (data.length() == 0) {
             return null;
         }
-        int start = data.indexOf(",");
-        Node root = new Node(Integer.parseInt(data.substring(0, start)), new ArrayList<Node>());
-        helper (root, data, start);
-        return root;
+        String[] stringArrayOfNodes = data.split(",");
+        return helperDeserialize(stringArrayOfNodes);
     }
-
-    public void helper(Node root, String s,int start){
-        LinkedList<Node> llOne = new LinkedList<Node>(), llTwo = new LinkedList<Node>();
-        llOne.add(root);
-        llTwo.add(root);
-        Node rootNode = root;
-        for (int i = start; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == ',') {
-                continue;
-            }
-            if (c == '/') {
-                llTwo = llOne;
-                llOne = new LinkedList();
-                rootNode = llTwo.poll();
-            } else {
-                if(c == '*')
-                    rootNode = llTwo.poll();
-                else{
-                    int j = i+1;
-                    while (j<s.length() && s.charAt(j) != ',') {
-                        j++;
-                    }
-                    Node child = new Node(Integer.parseInt(s.substring(i, j)), new ArrayList());
-                    i = j;
-                llOne.add(child);
-                rootNode.children.add(child);
-                }
-            }
+    
+    public Node helperDeserialize(String[] input) {
+        if (index >= input.length) {
+            return null;
         }
+        int value = Integer.valueOf(input[index++]);
+        int size = Integer.valueOf(input[index++]);
+        Node currentNode = new Node(value, new ArrayList<>());
+        for (int i = 0; i < size; i++) {
+            currentNode.children.add(helperDeserialize(input));
+        }
+        return currentNode;
     }
 }
 
