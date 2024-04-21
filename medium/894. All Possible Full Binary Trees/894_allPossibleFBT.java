0b4846927a829f3f07143643ab447516b8ba7bf1
Divyam Gupta <dgupta92@gatech.edu>
2024-04-20 22:19:09 -0700

    Add 894

--- medium/894. All Possible Full Binary Trees/894_allPossibleFBT.java
+++ medium/894. All Possible Full Binary Trees/894_allPossibleFBT.java
@@ -1,9 +1,3 @@
-x"
-Performance:
-Runtime: 6 ms, faster than 88.25% of Java online submissions for All Possible Full Binary Trees.
-Memory Usage: 59.4 MB, less than 36.03% of Java online submissions for All Possible Full Binary Trees.
-"
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -19,48 +13,8 @@ Memory Usage: 59.4 MB, less than 36.03% of Java online submissions for All Possi
  *     }
  * }
  */
-class Solution {
-    public List<TreeNode> allPossibleFBT(int n) {
-        if (n % 2 == 0)  {
-            return new ArrayList<TreeNode>();
-        }
-        List<TreeNode> result = new ArrayList<>();
-        for (int i = 1; i < n; i += 2) { 
-            List<TreeNode> left = allPossibleFBT(i);
-            List<TreeNode> right = allPossibleFBT(n - i - 1);
-            for (TreeNode leftNode : left) {
-                for (TreeNode rightNode : right) {
-                    TreeNode root = new TreeNode(0);
-                    root.left = leftNode;
-                    root.right = rightNode;
-                    result.add(root);
-                }
-            }
-        }
-        if (n == 1) {
-            result.add(new TreeNode(0));
-        }
-        return result;
-    }
-}
-
-//another solution
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+ //Time: O(2^(n/2))
+ //Space: O(n.(2^(n/2)))
 class Solution {
     public List<TreeNode> allPossibleFBT(int n) {
         List<TreeNode> result = new ArrayList<>();
@@ -74,7 +28,7 @@ class Solution {
         }
         
         for (int i = 1 ; i < n ; i += 2) {
-            int node = n - 1 - i;
+            int node = n - 1 - i; //so that left and right can sum up to n - 1
             List<TreeNode> leftTree = allPossibleFBT(i);
             List<TreeNode> rightTree = allPossibleFBT(node);
             for (TreeNode left : leftTree) {
