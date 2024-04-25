5f4ade18d3ba84a1ceedb042c8b71f3c0452c12c
Divyam Gupta <dgupta92@gatech.edu>
2024-04-24 22:12:34 -0700

    Add 695

--- medium/695. Max Area of Island/695_maxAreaOfIsland.java
+++ medium/695. Max Area of Island/695_maxAreaOfIsland.java
@@ -1,67 +1,27 @@
-"
-Performance:
-Runtime: 3 ms, faster than 83.21% of Java online submissions for Max Area of Island.
-Memory Usage: 47.7 MB, less than 33.45% of Java online submissions for Max Area of Island.
-"
-
+//The task involves finding the area of each connected shape in a grid and then determining the maximum area among them. 
+//To achieve this, we explore each land square and recursively visit all squares connected to it in four directions. 
+//We keep track of visited squares to avoid counting them more than once and ensure each shape is counted only once.
+//Time: O(rc)
+//Space: O(rc)
 class Solution {
-    int islands = 0;
-    int maximum = 0;
+    int[][] grid;
+    boolean[][] seen;
     public int maxAreaOfIsland(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    islands = 0;
-                    helper(i, j, grid);
-                    if (islands>maximum) {
-                        maximum = islands;
-                    }
-                }
+        this.grid = grid;
+        seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c));
             }
         }
-        return maximum;
+        return ans;
     }
-    
-    public void helper (int row, int col, int[][] grid) {
-        if (row >= 0 && col >= 0 && row < grid.length && col < grid[0].length && grid[row][col] == 1) {
-            grid[row][col] = 2;
-            helper(row - 1, col, grid);
-            helper(row, col - 1, grid);
-            helper(row + 1, col, grid);
-            helper(row, col + 1, grid);
-            islands++;
-        }
-    }
-
-}
-
-//another solution
-
-class Solution {
-    public int maxAreaOfIsland(int[][] grid) {
-        int max = 0;
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                if (grid[row][col] == 1) {
-                    int area = calculate(grid, row, col, 0);
-                    if (area > max) {
-                        max = area;
-                    }
-                }
-            }
-        }
-
-        return max;
-    }
-
-    public int calculate(int[][] grid, int row, int col, int area) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0) {
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || seen[r][c] || grid[r][c] == 0) {
             return 0;
         }
-        grid[row][col] = 0;
-        return  calculate(grid, row + 1, col, area) +
-                calculate(grid, row - 1, col, area) + 
-                calculate(grid, row, col + 1, area) +
-                calculate(grid, row, col - 1, area) + 1;
+        seen[r][c] = true;
+        return (1 + area(r+1, c) + area(r-1, c) + area(r, c-1) + area(r, c+1));
     }
 }
