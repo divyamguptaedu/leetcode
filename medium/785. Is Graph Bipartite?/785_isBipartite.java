9cccd40241baf6f0f884624bc561c7893bc9eae8
Divyam Gupta <dgupta92@gatech.edu>
2024-04-24 22:12:43 -0700

    Add 785

--- medium/785. Is Graph Bipartite?/785_isBipartite.java
+++ medium/785. Is Graph Bipartite?/785_isBipartite.java
@@ -1,66 +1,35 @@
-"
-Performance:
-Runtime: 1 ms, faster than 88.15% of Java online submissions for Is Graph Bipartite?.
-Memory Usage: 54.3 MB, less than 18.85% of Java online submissions for Is Graph Bipartite?.
-"
+//Coloring each node in a graph involves using a depth-first search starting from uncolored nodes. 
+//Nodes are colored with alternating colors, and a stack is used to track nodes to visit. 
+//If a neighbor has the same color as the current node, the coloring is not possible.
+//Time: O(n+e)
+//Space: O(n)
 
 class Solution {
-    public boolean isBipartite(int[][] graph) {
-        int[] visited = new int[graph.length];
-        Arrays.fill(visited, -1);
-        for (int i = 0; i < graph.length; i++) {
-            if (visited[i] == -1) 
-                if (!helper(i, graph, visited)) {
-                    return false;
-                }
-        }
-        return true;
-    }
-    
-    private boolean helper(int node, int[][] graph, int[] visited) {
-        if (visited[node] == -1) {
-            visited[node] = 1;
-        }
-        for (int i : graph[node]) {
-            if (visited[i] == -1) {
-                visited[i] = 1 - visited[node];
-                if (!helper(i, graph,visited)) {
-                    return false;
-                }
-            }
-            else if (visited[i] == visited[node]) {
-                return false;
-            }
-        }
-        return true;
-    }
-} 
-
-// other way
-
-class Solution {
-    private boolean isSameColored(int node, int color, int[] vis, int[][] graph) {
-        vis[node] = color;
-        for (int it : graph[node]) {
-            if (vis[it] == 0) {
-                if (isSameColored(it, -color, vis, graph) == true)
-                    return true;
-            }
-            else if (vis[it] == color)
-                return true;
-        }
-        return false;
-        
-    }
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int[] vis = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                if (isSameColored(i, 1, vis, graph) == true)
-                    return false;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+
+        for (int start = 0; start < n; ++start) {
+            if (color[start] == -1) {
+                Stack<Integer> stack = new Stack();
+                stack.push(start);
+                color[start] = 0;
+
+                while (!stack.empty()) {
+                    Integer node = stack.pop();
+                    for (int nei: graph[node]) {
+                        if (color[nei] == -1) {
+                            stack.push(nei);
+                            color[nei] = color[node] ^ 1;
+                        } else if (color[nei] == color[node]) {
+                            return false;
+                        }
+                    }
+                }
             }
         }
+
         return true;
     }
 }
