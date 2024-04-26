0bebf19f472502427c522b4936b62fd881ea7e12
Divyam Gupta <dgupta92@gatech.edu>
2024-04-25 22:37:55 -0700

    Add 529

---
+++ medium/529. Minesweeper/529_minesweeper.java
@@ -0,0 +1,42 @@
+//Time Complexity: O(8 * M * N) = O(M * N)
+//Space Complexity: O(M * N)
+
+class Solution {
+    private static final int[][] directions = { { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 },  { -1, 1 } };
+
+    public char[][] updateBoard(char[][] board, int[] click) {
+        if (board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        revealBoard(board, click[0], click[1]);
+        return board;
+    }
+
+    private void revealBoard(char[][] board, int x, int y) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] != 'E') {
+            return;
+        }
+        int mines = getMinesCount(board, x, y);
+        if (mines != 0) {
+            board[x][y] = (char) (mines + '0');
+            return;
+        }
+        board[x][y] = 'B';
+        for (int[] d : directions) {
+            revealBoard(board, x + d[0], y + d[1]);
+        }
+    }
+
+    private int getMinesCount(char[][] board, int x, int y) {
+        int mines = 0;
+        for (int[] d : directions) {
+            int r = x + d[0];
+            int c = y + d[1];
+            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length && board[r][c] == 'M') {
+                mines++;
+            }
+        }
+        return mines;
+    }
+}
