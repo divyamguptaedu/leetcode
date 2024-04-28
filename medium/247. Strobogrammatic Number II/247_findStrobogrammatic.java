3cb326354d98948b2e9e7f5408b6035899346085
Divyam Gupta <dgupta92@gatech.edu>
2024-04-28 14:30:12 -0700

    Add 247

--- medium/247. Strobogrammatic Number II/247_findStrobogrammatic.java
+++ medium/247. Strobogrammatic Number II/247_findStrobogrammatic.java
@@ -1,9 +1,15 @@
-//Time: O(N 5^(N/2 + 1))
+//For n = 1, the result is 0, 1, 8
+//For n = 2, the result is 11, 88, 69, 96
+//For n = 3, the result is 101, 111, 181, 808, 818, 888, .... basically we add numbers from 1 on the outside of numbers from n = 2, not the zero pair.
+//So, we can use recursion to solve this.
+//For every previous string, and for every pair in the data, we add the string in between the pair key and value.
+
+//Time: O(N 5^(N/2 + 1)) because we have 5 pairs in the data, in each recursion, we iterate over all strings and append 5 pairs to each. 
 //Space: O(N 5^(N/2))
 
 class Solution {
-    public char[][] data = {{'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
-    
+    public char[][] data = { { '0', '0' }, { '1', '1' }, { '6', '9' }, { '8', '8' }, { '9', '6' } };
+
     public List<String> findStrobogrammatic(int n) {
         return helper(n, n);
     }
@@ -12,14 +18,14 @@ class Solution {
         if (n == 0) {
             return new ArrayList<>(List.of(""));
         }
-        
+
         if (n == 1) {
             return new ArrayList<>(List.of("0", "1", "8"));
         }
-        
+
         List<String> previous = helper(n - 2, finalLength);
         List<String> current = new ArrayList<>();
-        
+
         for (String p : previous) {
             for (char[] pair : data) {
                 if (pair[0] != '0' || n != finalLength) {
@@ -27,7 +33,7 @@ class Solution {
                 }
             }
         }
-        
+
         return current;
     }
 }
