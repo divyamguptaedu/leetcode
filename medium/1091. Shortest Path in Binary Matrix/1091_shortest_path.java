299917aef770aa4b631e305eb674a3bcfceec62d
Divyam Gupta <dgupta92@gatech.edu>
2024-04-27 21:56:36 -0700

    Add 1091

--- medium/1091. Shortest Path in Binary Matrix/1091_shortest_path.java
+++ medium/1091. Shortest Path in Binary Matrix/1091_shortest_path.java
@@ -1,39 +1,53 @@
-"
-Performance:
-Runtime: 14 ms, faster than 70.16% of Java online submissions for Shortest Path in Binary Matrix.
-Memory Usage: 40.5 MB, less than 32.13% of Java online submissions for Shortest Path in Binary Matrix.
-"
+//Since its a shortest path problem, we can solve it using DFS. So, first we can define the directions array with all the 8 possible directions.
+//Then, we can first check for the edge case when the start and end are not set to 0, in that case we return -1.
+//For the BFS, we initialize the queue, set the starting point to 1, and add the starting point to the queue.
+//In this BFS, we check if the current cell is the last cell, if yes, return distance.
+//If not, then we get all the neighbors of this cell which have a 0 and add them to the queue and increase their distance.
+
+//Time: O(n) where n is the total number of cells in the grid.
+//Space: O(n)
 
 class Solution {
+    private static final int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        
-        if (grid == null || grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] != 0) {
+        if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) {
             return -1;
         }
-        
-        int[][] directions = {{1,0}, {-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
-        
-        int m = grid.length; //square matrix m == n
-        Queue<Pair<Integer,Integer>> queue = new LinkedList();
-        if (m == 1) {
-            return 1;
-        }
-        
-        queue.add(new Pair(0,0));
+        Queue<int[]> queue = new ArrayDeque<>();
         grid[0][0] = 1;
-        while(!queue.isEmpty()){
-            Pair<Integer,Integer> pos = queue.poll();
-            for(int[] dir : directions){
-                int newPosX = pos.getKey() + dir[0];
-                int newPosY = pos.getValue() + dir[1];
-                if(newPosX >= m || newPosX < 0 || newPosY >= m || newPosY < 0 || grid[newPosX][newPosY] >= 1)
-                    continue;
-                if(newPosX == m - 1 && newPosX == newPosY)
-                    return grid[pos.getKey()][pos.getValue()] + 1;
-                grid[newPosX][newPosY] =  grid[pos.getKey()][pos.getValue()] + 1; 
-                queue.offer(new Pair(newPosX,newPosY));
+        queue.add(new int[]{0, 0});
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.remove();
+            int row = cell[0];
+            int col = cell[1];
+            int distance = grid[row][col];
+            if (row == grid.length - 1 && col == grid[0].length - 1) {
+                return distance;
+            }
+            for (int[] neighbour : getNeighbours(row, col, grid)) {
+                int neighbourRow = neighbour[0];
+                int neighbourCol = neighbour[1];
+                queue.add(new int[]{neighbourRow, neighbourCol});
+                grid[neighbourRow][neighbourCol] = distance + 1;
             }
         }
+
         return -1;
     }
+    
+    private List<int[]> getNeighbours(int row, int col, int[][] grid) {
+        List<int[]> neighbours = new ArrayList<>();
+        for (int i = 0; i < directions.length; i++) {
+            int newRow = row + directions[i][0];
+            int newCol = col + directions[i][1];
+            if (newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length || grid[newRow][newCol] != 0) {
+                continue;    
+            }
+            neighbours.add(new int[]{newRow, newCol});
+        }
+        return neighbours; 
+    }
+    
 }
