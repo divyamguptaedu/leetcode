692f0bcbcbcf2330d0d2c455c85e02eb7f4b5f4f
Divyam Gupta <dgupta92@gatech.edu>
2024-04-27 21:55:14 -0700

    Add 79

--- medium/79. Word Search/79_word_search.java
+++ medium/79. Word Search/79_word_search.java
@@ -1,12 +1,29 @@
+//Did a four way dfs on the grid interating over every cell. 
+//In the dfs function, we check for all the bad cases first like the row and col number, 
+//and if the char present at that cell is the char at the current index in the word, 
+//basically is it the letter we are looking for, if yes, we mark it as visited, 
+//and kick off the four way dfs by increasing the index by 1. 
+//If not, we set the cell back to its original letter and return false. 
+//For the time complexity, in the worst case, we kick off the word at the very last index, 
+//so O(N) to reach there where N is the number of cells in the grid, 
+//and since at that point we would have already explored one of the directions, 
+//we will have three directions left, so O(3^L) where L is the length of the word. 
+
+//Time: O(N.3^L) where N is the #cells and L is the length of the word
+//Space: O(L)
 class Solution {
-    boolean[][] visited;
+    private char[][] board;
+    private int ROWS;
+    private int COLS;
+
     public boolean exist(char[][] board, String word) {
-        int row = board.length;
-        int col = board[0].length;
-        visited = new boolean[row][col];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if ((board[i][j] == word.charAt(0)) && foundWord(i, j, 0, board, word)) {
+        this.board = board;
+        this.ROWS = board.length;
+        this.COLS = board[0].length;
+
+        for (int row = 0; row < this.ROWS; ++row) {
+            for (int col = 0; col < this.COLS; ++col) {
+                if (this.backtrack(row, col, word, 0)) {
                     return true;
                 }
             }
@@ -14,19 +31,22 @@ class Solution {
         return false;
     }
 
-    public boolean foundWord(int i, int j, int index, char[][] board, String word) {
-        if (index == word.length()) {
+    protected boolean backtrack(int row, int col, String word, int index) {
+        if (index >= word.length()) {
             return true;
         }
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || word.charAt(index) != board[i][j] || visited[i][j]) {
+        if (row < 0 || row == this.ROWS || col < 0 || col == this.COLS || this.board[row][col] != word.charAt(index)) { //all bad cases
             return false;
         }
-
-        visited[i][j] = true;
-        if (foundWord(i+1, j, index + 1, board, word) || foundWord(i, j+1, index + 1, board, word) || foundWord(i-1, j, index + 1, board, word) || foundWord(i, j-1, index + 1, board, word)) {
-            return true;
-        } 
-        visited[i][j] = false;
+        this.board[row][col] = '#'; //mark as visited for the upcoming four way dfs
+        int[] rowOffsets = {0, 1, 0, -1};
+        int[] colOffsets = {1, 0, -1, 0};
+        for (int d = 0; d < 4; ++d) {
+            if (this.backtrack(row + rowOffsets[d], col + colOffsets[d], word, index + 1)) { //four way dfs
+                return true;
+            }
+        }
+        this.board[row][col] = word.charAt(index); //backtrack, set it back to the original
         return false;
     }
 }
