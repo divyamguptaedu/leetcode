0e46021642b6037e7dd22462389937b47de610b7
Divyam Gupta <dgupta92@gatech.edu>
2024-04-29 22:58:12 -0700

    Add 219

--- easy/219. Contains Duplicate II/219_contains_duplicates_ii.java
+++ easy/219. Contains Duplicate II/219_contains_duplicates_ii.java
@@ -1,20 +1,13 @@
-"
-Performance:
-Runtime: 60 ms, faster than 14.97% of Java online submissions for Contains Duplicate II.
-Memory Usage: 94.5 MB, less than 25.45% of Java online submissions for Contains Duplicate II.
-"
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer,Integer> valueMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (valueMap.containsKey(nums[i]) && i - valueMap.get(nums[i]) <= k) {
-                return true;
-            } else {
-                valueMap.put(nums[i],i);
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
             }
         }
         return false;
-
     }
 }
