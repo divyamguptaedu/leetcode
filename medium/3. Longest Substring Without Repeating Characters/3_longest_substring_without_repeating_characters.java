3d14442c89272d38e8cfcf20f80d9888cc20b5c1
Divyam Gupta <dgupta92@gatech.edu>
2024-04-29 22:58:41 -0700

    Add 3

--- medium/3. Longest Substring Without Repeating Characters/3_longest_substring_without_repeating_characters.java
+++ medium/3. Longest Substring Without Repeating Characters/3_longest_substring_without_repeating_characters.java
@@ -1,52 +1,21 @@
-"""
-Performance:
-Runtime: 7 ms, faster than 40.44% of Java online submissions for Longest Substring Without Repeating Characters.
-Memory Usage: 39.7 MB, less than 24.74% of Java online submissions for Longest Substring Without Repeating Characters.
-"""
-
-class Solution {
-	public int lengthOfLongestSubstring(String s) {
-		
-	    int i = 0;
-	    int j = 0;
-	    int max = 0;
-	    Set<Character> set = new HashSet<>();
-	    
-	    // removes the first element of the set each time when a repeating string is found;
-	    while (j < s.length()) {
-	        if (!set.contains(s.charAt(j))) {
-	            set.add(s.charAt(j++));
-	            max = Math.max(max, set.size());
-	        } else {
-	            set.remove(s.charAt(i++));
-	        }
-	    }
-	    
-	    return max;
-	}
-}
-
-//another solution
-
-class Solution {
+public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() < 2) {
-            return s.length();
-        }
-        int x = 0;
-        int length = 0;
-        int counter = 0;
-        for (int i = 1; i < s.length(); i++) {
-            for (int j = x; j < i; j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    x = j + 1;
-                }
+        Integer[] chars = new Integer[128];
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            Integer index = chars[r];
+            if (index != null && index >= left && index < right) {
+                left = index + 1;
             }
-            counter = i - x + 1;
-            if (counter > length) {
-                length = counter;
-            }
+            res = Math.max(res, right - left + 1);
+            chars[r] = right;
+            right++;
         }
-        return length;
+
+        return res;
     }
 }
