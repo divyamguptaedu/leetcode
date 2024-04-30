d2fade2ccc6520d187f9f735296bb86a6d0ac0d5
Divyam Gupta <dgupta92@gatech.edu>
2024-04-29 22:59:05 -0700

    Add 76

--- hard/76. Minimum Window Substring/76_minimum_window_sliding.java
+++ hard/76. Minimum Window Substring/76_minimum_window_sliding.java
@@ -1,57 +1,62 @@
-"""
-Performance:
-Runtime: 3 ms, faster than 96.20% of Java online submissions for Minimum Window Substring.
-Memory Usage: 39.2 MB, less than 77.70% of Java online submissions for Minimum Window Substring
-"""
-
 class Solution {
-	public String minWindow(String s, String t) {
-	    if (s.length() == 0 || t.length() == 0 || s.length() < t.length()) return "";
-	    
-	    int[] tmap = new int[256]; //store the count of each character in t
-	    boolean[] tbmap = new boolean[256]; //store the appearance of each character in t
-	    
-	    for (int i = 0; i < t.length(); i++){
-	        tmap[t.charAt(i)]++;
-	        tbmap[t.charAt(i)] = true;
-	    }
-	    
-	    int i = 0, j = 0; //i and j stores the start and end index of substring
-	    int found  = 0; //found stores how many letters in t has been found in s
-	    int len = Integer.MAX_VALUE;
-	    String res = "";
-	    
-	    while(j < s.length()){
-	        char cj = s.charAt(j);
-	        if(found < t.length()){  // we haven't found all the letters in t
-	            if(tbmap[cj]){
-	                if(tmap[cj] > 0) {   //the times cj appear in s is smaller than the times appears in t
-	                    found++;
-	                }
-	                tmap[cj]--;
-	            }
-	            j++;
-	        }
-	        
-	        while(found == t.length()){ 
-	            char ci = s.charAt(i);
-	            if(!tbmap[ci]) i++;  //ci in s is not in t
-	            else if (tmap[ci] < 0){  //ci appears more times in s than t
-	                tmap[ci]++;
-	                i++;
-	            }else{
-	                if(j-i < len){
-	                    res = s.substring(i, j);
-	                    len = j - i;
-	                }
-	                found--;
-	                tmap[ci]++;
-	                i++;
-	            }
-	        }
-	        
-	    }
-	    
-	    return res;
-	}
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
+        }
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
+        }
+        int required = dictT.size();
+
+        // Filter all the characters from s into a new list along with their index.
+        // The filtering criteria is that the character should be present in t.
+        List<Pair<Integer, Character>> filteredS = new ArrayList<Pair<Integer, Character>>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (dictT.containsKey(c)) {
+                filteredS.add(new Pair<Integer, Character>(i, c));
+            }
+        }
+
+        int l = 0, r = 0, formed = 0;
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+        int[] ans = { -1, 0, 0 };
+
+        // Look for the characters only in the filtered list instead of entire s.
+        // This helps to reduce our search.
+        // Hence, we follow the sliding window approach on as small list.
+        while (r < filteredS.size()) {
+            char c = filteredS.get(r).getValue();
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try and contract the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = filteredS.get(l).getValue();
+
+                // Save the smallest window until now.
+                int end = filteredS.get(r).getKey();
+                int start = filteredS.get(l).getKey();
+                if (ans[0] == -1 || end - start + 1 < ans[0]) {
+                    ans[0] = end - start + 1;
+                    ans[1] = start;
+                    ans[2] = end;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
 }
