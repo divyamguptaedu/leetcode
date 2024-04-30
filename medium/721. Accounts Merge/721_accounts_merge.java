ecb171a284c711e28470572f440bfcdb9f89a5da
Divyam Gupta <dgupta92@gatech.edu>
2024-04-29 22:58:32 -0700

    Add 721

--- medium/721. Accounts Merge/721_accounts_merge.java
+++ medium/721. Accounts Merge/721_accounts_merge.java
@@ -1,56 +1,53 @@
 //Created the adj list, traversed over the accounts to check if the first email was already visited, 
 //if not, performed DFS. During DFS, stored the emails in an array, marked as visited. 
 //In the end, sorted the emails, added account name at start, returned the answer list.
-//Time: O(NK log NK): N is the number of account, K is the max length of an account
-//Space: O(NK)
+//Time: O(NM log(NM)) where N is the number of people, and M is the maximum number of accounts they have.
+//Space: O(NM)
 class Solution {
+    HashMap<String, List<String>> adjList = new HashMap<>();
     Set<String> visited = new HashSet<>();
-    HashMap<String,List<String>> adjList = new HashMap<>();
 
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        // create the graph the from accounts
-        for (List<String> account : accounts){
-            String firstEmail = account.get(1);
+        for (List<String> account : accounts) {
+            String firstEmail = account.get(1); //can take any email, the point is to connect them all.
             int accountSize = account.size();
-            for (int index = 2;index < accountSize;index++){
+            for (int index = 2; index < accountSize; index++) { //connect all the accounts
                 String email = account.get(index);
-                if (!adjList.containsKey(firstEmail)){
-                    adjList.put(firstEmail,new ArrayList<String>());
+                if (!adjList.containsKey(firstEmail)) {
+                    adjList.put(firstEmail, new ArrayList<String>());
                 }
                 adjList.get(firstEmail).add(email);
-                if (!adjList.containsKey(email)){
-                    adjList.put(email, new ArrayList<String>());
+                if (!adjList.containsKey(email)) {
+                    adjList.put(email, new ArrayList<>());
                 }
                 adjList.get(email).add(firstEmail);
             }
         }
-        List<List<String>> res = new ArrayList<>();
-        for (List<String> account : accounts){
+        List<List<String>> result = new ArrayList<>();
+        for (List<String> account : accounts) {
             String name = account.get(0);
             String firstEmail = account.get(1);
             if (!visited.contains(firstEmail)) {
-                ArrayList<String> mergeAccount = new ArrayList<>();
-                mergeAccount.add(name);
-                DFS(mergeAccount,firstEmail);
-                Collections.sort(mergeAccount.subList(1,mergeAccount.size()));
-                res.add(mergeAccount);
+                ArrayList<String> mergedAccount = new ArrayList<>();
+                mergedAccount.add(name);
+                DFS(mergedAccount, firstEmail);
+                Collections.sort(mergedAccount.subList(1, mergedAccount.size()));
+                result.add(mergedAccount);
             }
-            
         }
-        return res;
+
+        return result;
     }
 
-    public void DFS(List<String>mergeAccount, String email) {
+    private void DFS(List<String> mergedAccount, String email) {
         visited.add(email);
-        mergeAccount.add(email);
-        
+        mergedAccount.add(email);
         if (!adjList.containsKey(email)) {
             return;
         }
-        
-        for (String neighbour: adjList.get(email)) {
-            if (!visited.contains(neighbour)) {
-                DFS(mergeAccount,neighbour);
+        for (String otherEmail : adjList.get(email)) {
+            if (!visited.contains(otherEmail)) {
+                DFS(mergedAccount, otherEmail);
             }
         }
     }
