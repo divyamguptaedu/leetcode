0350aae95f80214446f8fa1504e3db824964c08a
Divyam Gupta <dgupta92@gatech.edu>
2024-05-02 16:44:45 -0400

    Add 33

--- medium/33. Search in Rotated Sorted Array/33_search_in_rotated_sorted_array.java
+++ medium/33. Search in Rotated Sorted Array/33_search_in_rotated_sorted_array.java
@@ -1,32 +1,39 @@
-""" 
-Performance: 
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Search in Rotated Sorted Array.
-Memory Usage: 39.6 MB, less than 6.10% of Java online submissions for Search in Rotated Sorted Array.
-"""
-
 class Solution {
     public int search(int[] nums, int target) {
-        // binary search algorithm.
-        int begin = 0;
-        int end = nums.length - 1;
-        while (begin <= end) {
-        	int middle = (begin + end) / 2;
-        	if (nums[middle] == target) {
-        		return middle;
-        	}
-        	if (nums[begin] <= nums[middle]) {
-        		if (target >= nums[begin] && target < nums[middle]) {
-        			end = middle - 1;
-        		} else {
-        			begin = middle + 1;
-        		}
-        	} else {
-        		if (target > nums[middle] && target <= nums[end]) {
-        			begin = middle + 1;
-        		} else {
-        			end = middle - 1;
-        		}
-        	}
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        // Find the index of the pivot element (the smallest element)
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return shiftedBinarySearch(nums, left, target);
+    }
+
+    // Shift elements in a circular manner, with the pivot element at index 0.
+    // Then perform a regular binary search
+    private int shiftedBinarySearch(int[] nums, int pivot, int target) {
+        int n = nums.length;
+        int shift = n - pivot;
+        int left = (pivot + shift) % n;
+        int right = (pivot - 1 + shift) % n;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[(mid - shift + n) % n] == target) {
+                return (mid - shift + n) % n;
+            } else if (nums[(mid - shift + n) % n] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
         return -1;
     }
