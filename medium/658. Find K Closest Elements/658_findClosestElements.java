9f5c56ed9f253ac8b3c4854408e4ff90155d26b0
Divyam Gupta <dgupta92@gatech.edu>
2024-05-02 16:43:34 -0400

    Add 648

--- medium/658. Find K Closest Elements/658_findClosestElements.java
+++ medium/658. Find K Closest Elements/658_findClosestElements.java
@@ -1,13 +1,20 @@
+//We can use binary search to find the best window for this problem. 
+//We initialize left to be 0 and right to be len(arr) - k as we will keep the pointer to be the leftmost point of the window.
+//We perform binary search to check check if moving the window to left is better or right is better. The smaller element wins.
+//At the end, we have the leftmost index for the final answer. Return the subarray.
+
+//Time: O(log(n-k)+k) Finding the bound takes log(n-k), building the result takes O(k).
+//Space: O(1)
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         // Initialize binary search bounds
         int left = 0;
-        int right = arr.length - k;
+        int right = arr.length - k; //because we consider the pointer to be the leftmost element in the window
         
         // Binary search against the criteria described
         while (left < right) {
             int mid = (left + right) / 2;
-            if (x - arr[mid] > arr[mid + k] - x) {
+            if (x - arr[mid] > arr[mid + k] - x) { //what's better? Moving the window to left or right?
                 left = mid + 1;
             } else {
                 right = mid;
