043e7dded368dde1593a3eaa10e4de00f9749e54
Divyam Gupta <dgupta92@gatech.edu>
2024-05-02 16:45:07 -0400

    Add 153

--- medium/153. Find Minimum in Rotated Sorted Array/153_find_max_rotated_sorted_array.java
+++ medium/153. Find Minimum in Rotated Sorted Array/153_find_max_rotated_sorted_array.java
@@ -1,24 +1,52 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Find Minimum in Rotated Sorted Array.
-Memory Usage: 38.7 MB, less than 36.47% of Java online submissions for Find Minimum in Rotated Sorted Array.
-"""
-
 class Solution {
     public int findMin(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start < end) {
-            if (nums[start] < nums[end]) {
-                return nums[start];       
+        // If the list has just one element then return that element.
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        // initializing left and right pointers.
+        int left = 0, right = nums.length - 1;
+
+        // if the last element is greater than the first element then there is no
+        // rotation.
+        // e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
+        // Hence the smallest element is first element. A[0]
+        if (nums[right] > nums[0]) {
+            return nums[0];
+        }
+
+        // Binary search way
+        while (right >= left) {
+            // Find the mid element
+            int mid = left + (right - left) / 2;
+
+            // if the mid element is greater than its next element then mid+1 element is the
+            // smallest
+            // This point would be the point of change. From higher to lower value.
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
             }
-            int middle = start + (end - start) / 2;
-            if (nums[middle] < nums[start]) {
-                end = middle;
+
+            // if the mid element is lesser than its previous element then mid element is
+            // the smallest
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+
+            // if the mid elements value is greater than the 0th element this means
+            // the least value is still somewhere to the right as we are still dealing with
+            // elements
+            // greater than nums[0]
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
             } else {
-                start = middle + 1;
+                // if nums[0] is greater than the mid value then this means the smallest value
+                // is somewhere to
+                // the left
+                right = mid - 1;
             }
         }
-        return nums[start];
+        return Integer.MAX_VALUE;
     }
 }
