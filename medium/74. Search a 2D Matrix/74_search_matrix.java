7d3a1ebe1e052efe22c43e3cef764dad2aaec5db
Divyam Gupta <dgupta92@gatech.edu>
2024-05-02 16:44:27 -0400

    Add 74

--- medium/74. Search a 2D Matrix/74_search_matrix.java
+++ medium/74. Search a 2D Matrix/74_search_matrix.java
@@ -1,23 +1,19 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Search a 2D Matrix.
-Memory Usage: 38.9 MB, less than 36.08% of Java online submissions for Search a 2D Matrix.
-
-"""
-
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int index = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            if (target <= matrix[i][matrix[0].length - 1]) {
-                index = i;
-                break;
-            }
-        }
-        
-        for (int i = matrix[0].length-1; i >= 0; i--) {
-            if (target == matrix[index][i]) {
-                return true;
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+
+        // binary search
+        int left = 0, right = m * n - 1;
+        int pivotIdx, pivotElement;
+        while (left <= right) {
+            pivotIdx = (left + right) / 2;
+            pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+            if (target == pivotElement) return true;
+            else {
+                if (target < pivotElement) right = pivotIdx - 1;
+                else left = pivotIdx + 1;
             }
         }
         return false;
