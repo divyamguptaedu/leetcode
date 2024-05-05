9e91aec140389f51c1fee797338d23f4a6adbf67
Divyam Gupta <dgupta92@gatech.edu>
2024-05-04 23:56:14 -0400

    Add 300

--- medium/300. Longest Increasing Subsequence/300_longest_increasing_subsequence.java
+++ medium/300. Longest Increasing Subsequence/300_longest_increasing_subsequence.java
@@ -1,55 +1,39 @@
-"""
-Performance:
-Runtime: 2 ms, faster than 99.06% of Java online submissions for Longest Increasing Subsequence.
-Memory Usage: 38.8 MB, less than 43.34% of Java online submissions for Longest Increasing Subsequence.
-"""
-
-public class Solution {
-	
+class Solution {
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int[] temp = new int[nums.length];
-        temp[0] = nums[0];
-        int length = 0;
+        ArrayList<Integer> sub = new ArrayList<>();
+        sub.add(nums[0]);
+
         for (int i = 1; i < nums.length; i++) {
-            // use binary search to find position;
-            int position = binarySearch(temp,length,nums[i]);
-            if (nums[i] < temp[position]) temp[position] = nums[i];
-            if (position > length) {
-                length = position;
-                temp[length] = nums[i];
+            int num = nums[i];
+            if (num > sub.get(sub.size() - 1)) {
+                sub.add(num);
+            } else {
+                int j = binarySearch(sub, num);
+                sub.set(j, num);
             }
         }
-        return length + 1;
+
+        return sub.size();
     }
 
-    // binary search, operates in logn time.
-    private int binarySearch(int[] temp, int length, int val) {
+    private int binarySearch(ArrayList<Integer> sub, int num) {
         int left = 0;
-        int right = length;
-        while(left + 1 < right) {
-            // find middle and choose side;
-            int mid = left + (right - left) / 2;
-            if (temp[mid] == val) {
+        int right = sub.size() - 1;
+        int mid = (left + right) / 2;
+
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (sub.get(mid) == num) {
                 return mid;
+            }
+
+            if (sub.get(mid) < num) {
+                left = mid + 1;
             } else {
-                if (temp[mid] < val) {
-                    left = mid;
-                } else {
-                    right = mid;
-                }
+                right = mid;
             }
         }
-        if (temp[right] < val) {
-        	return length + 1;
-        }
-        else if (temp[left] >= val) {
-        	return left;
-        }
-        else {
-        	return right;
-        }
+
+        return left;
     }
 }
