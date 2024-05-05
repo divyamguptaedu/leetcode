59feced6bb4639eaf7f27ffb913c6870a6b2b375
Divyam Gupta <dgupta92@gatech.edu>
2024-05-04 23:57:00 -0400

    ADD 167

--- medium/167. Two Sum II - Input Array Is Sorted/167_twoSum.java
+++ medium/167. Two Sum II - Input Array Is Sorted/167_twoSum.java
@@ -1,20 +1,19 @@
-"
-Performance:
-Runtime: 2 ms, faster than 71.55% of Java online submissions for Two Sum II - Input Array Is Sorted.
-Memory Usage: 50.3 MB, less than 22.48% of Java online submissions for Two Sum II - Input Array Is Sorted.
-"
-
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int first = 0;
-        int last = numbers.length-1;
-        while (numbers[first] + numbers[last] != target) {
-            if (numbers[first] + numbers[last] > target) { 
-                last--;
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+
+            if (sum == target) {
+                return new int[] { low + 1, high + 1 };
+            } else if (sum < target) {
+                ++low;
             } else {
-                first++;
+                --high;
             }
         }
-        return new int[]{first + 1, last + 1};
+        // In case there is no solution, return {-1, -1}.
+        return new int[] { -1, -1 };
     }
 }
