ce025a446ebc764c571d28f9e47a867f77fb793e
Divyam Gupta <dgupta92@gatech.edu>
2024-05-05 20:56:59 -0400

    Add 825

--- medium/825. Friends Of Appropriate Ages/825_numFriendRequests.java
+++ medium/825. Friends Of Appropriate Ages/825_numFriendRequests.java
@@ -1,28 +1,40 @@
 class Solution {
     public int numFriendRequests(int[] ages) {
+        int requestCount = 0;
+        // sort the array to enable binary search
         Arrays.sort(ages);
-        int n = ages.length;
-        int answer = 0;
-        int[] count = new int[121];
-        for (int i = 0; i < n; i++) {
-            int low = 0;
-            int high = i - 1;
-            int result = -1;
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (0.5 * ages[i] + 7.0 >= ages[mid]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                    result = mid;
-                }
+        for (int i = 0; i < ages.length; i++) {
+            // exclusive minimum age based on the provided formula
+            int minAge = ages[i] / 2 + 7;
+            if (minAge < ages[i]) {
+                // find the index of the first element greater than minAge
+                int leftIdx = binarySearch(ages, 0, ages.length, minAge);
+                // find the index of the last element equal to ages[i] - there
+                // is at least 1 such element (ages[i]) but there may be more
+                int rightIdx = binarySearch(ages, 0, ages.length, ages[i]) - 1;
+                // user i can send a friend request to every user in the
+                // [leftIdx, rightIdx] range except itself - the number of
+                // users in the range is calculated as rightIdx - leftIdx + 1,
+                // but we subtract 1 to exclude user i
+                requestCount += rightIdx - leftIdx;
             }
-            if (result != -1) {
-                answer += (i - result); 
-                answer += count[ages[i]];
-            }
-            count[ages[i]]++;
         }
-        return answer;
+        return requestCount;
+    }
+
+    // binary search to find the first age value that is greater than the
+    // provided target in O(log n) time
+    private int binarySearch(int[] ages, int l, int r, int target) {
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (ages[mid] > target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        // l is the index of the smallest age value for which the condition
+        // ages[mid] > target evaluates as true
+        return l;
     }
 }
