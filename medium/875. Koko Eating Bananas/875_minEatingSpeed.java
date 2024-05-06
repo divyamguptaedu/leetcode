1fd52231aea75f0725e3be6ab4e3226397751521
Divyam Gupta <dgupta92@gatech.edu>
2024-05-05 20:56:51 -0400

    Add 875

--- medium/875. Koko Eating Bananas/875_minEatingSpeed.java
+++ medium/875. Koko Eating Bananas/875_minEatingSpeed.java
@@ -1,29 +1,33 @@
-"
-Performance:
-Runtime: 54 ms, faster than 93.14% of Java online submissions for Koko Eating Bananas.
-Memory Usage: 54.3 MB, less than 25.16% of Java online submissions for Koko Eating Bananas.
-
-"
-
 class Solution {
-    public int minEatingSpeed(int[] arr, int h) {
-        int right = Integer.MIN_VALUE;
-        for (int element : arr) {
-            right = Math.max(right, element);
+    public int minEatingSpeed(int[] piles, int h) {
+        // Initalize the left and right boundaries
+        int left = 1, right = 1;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
         }
-        int left = 1;
-        while (left < right) { 
+
+        while (left < right) {
+            // Get the middle index between left and right boundary indexes.
+            // hourSpent stands for the total hour Koko spends.
             int middle = (left + right) / 2;
-            int current = 0;
-            for (int element : arr) {
-                current += Math.ceil((element * 1.0) / middle);
+            int hourSpent = 0;
+
+            // Iterate over the piles and calculate hourSpent.
+            // We increase the hourSpent by ceil(pile / middle)
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / middle);
             }
-            if (current > h) {
-                left = middle + 1;
-            } else {
+
+            // Check if middle is a workable speed, and cut the search space by half.
+            if (hourSpent <= h) {
                 right = middle;
+            } else {
+                left = middle + 1;
             }
         }
-        return left;
+
+        // Once the left and right boundaries coincide, we find the target value,
+        // that is, the minimum workable eating speed.
+        return right;
     }
 }
