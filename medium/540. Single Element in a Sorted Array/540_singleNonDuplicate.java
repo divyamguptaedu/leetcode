036af8588224761540f0428650d73e964eacd336
Divyam Gupta <dgupta92@gatech.edu>
2024-05-05 20:57:16 -0400

    Add 540

--- medium/540. Single Element in a Sorted Array/540_singleNonDuplicate.java
+++ medium/540. Single Element in a Sorted Array/540_singleNonDuplicate.java
@@ -1,21 +1,18 @@
-"
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Single Element in a Sorted Array.
-Memory Usage: 60.7 MB, less than 35.72% of Java online submissions for Single Element in a Sorted Array.
-"
-public class Solution {
+class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int size = nums.length;
         int low = 0;
-        int high = size / 2;
+        int high = nums.length - 1;
         while (low < high) {
-            int temp = (low + high) / 2;
-            if (nums[2 * temp] != nums[2 * temp + 1]) {
-                high = temp;
+            int mid = (low + high) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
             } else {
-                low = temp + 1;
+                high = mid;
             }
         }
-        return nums[2 * low];
+        return nums[low];
     }
 }
