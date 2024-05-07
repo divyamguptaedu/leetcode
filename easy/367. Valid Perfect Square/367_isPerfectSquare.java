1d6a04fc4ba703668a080de72c6e8985b30eabaf
Divyam Gupta <dgupta92@gatech.edu>
2024-05-06 23:23:07 -0700

    Add 367

--- easy/367. Valid Perfect Square/367_isPerfectSquare.java
+++ easy/367. Valid Perfect Square/367_isPerfectSquare.java
@@ -1,20 +1,28 @@
+//We can just solve this question using a binary search approach. We start from left == 2 and right == num/2.
+//Then we just calculate mid, check if mid*mid == num, if yes, return true.
+//If not, then check mid*mid > num, then right = mid - 1.
+//Else left = mid + 1.
+//Time: O(logn)
+//Space: O(1)
 class Solution {
     public boolean isPerfectSquare(int num) {
         if (num < 2) {
             return true;
         }
 
-        long left = 2, right = num / 2, x, guessSquared;
+        long left = 2;
+        long right = num / 2;
+        long guessSquared;
         while (left <= right) {
-            x = left + (right - left) / 2;
-            guessSquared = x * x;
+            long mid = (left + right) / 2;
+            guessSquared = mid * mid;
             if (guessSquared == num) {
                 return true;
             }
             if (guessSquared > num) {
-                right = x - 1;
+                right = mid - 1;
             } else {
-                left = x + 1;
+                left = mid + 1;
             }
         }
         return false;
