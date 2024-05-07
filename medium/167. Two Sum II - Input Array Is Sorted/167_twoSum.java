b27191b5caddda974e79f792b9424fbdc5ecaff7
Divyam Gupta <dgupta92@gatech.edu>
2024-05-06 23:22:43 -0700

    Add 167

--- medium/167. Two Sum II - Input Array Is Sorted/167_twoSum.java
+++ medium/167. Two Sum II - Input Array Is Sorted/167_twoSum.java
@@ -1,19 +1,26 @@
+//We can solve this question using a two pointer approach. We can have low pointer at 0 and high pointer at end.
+//Now, we can do while low < high, get the sum of the two numbers at pointers
+//If sum == target, return
+//If sum < target, increment the low pointer
+//If sum > target, decrement the high pointer
+//If not found, we can return -1, -1.
+
+//Time: O(n)
+//Space: O(1)
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int low = 0;
         int high = numbers.length - 1;
         while (low < high) {
             int sum = numbers[low] + numbers[high];
-
             if (sum == target) {
-                return new int[] { low + 1, high + 1 };
+                return new int[] { low + 1, high + 1 }; //because the array is 1 indexed, not zero.
             } else if (sum < target) {
-                ++low;
+                low++;
             } else {
-                --high;
+                high--;
             }
         }
-        // In case there is no solution, return {-1, -1}.
         return new int[] { -1, -1 };
     }
 }
