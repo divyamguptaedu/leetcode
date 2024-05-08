39318b340c79a0f01907f601fd0395d9cb96efea
Divyam Gupta <dgupta92@gatech.edu>
2024-05-07 22:15:47 -0700

    Add 1901

--- medium/1901. Find a Peak Element II/1901_getMaxElement.java
+++ medium/1901. Find a Peak Element II/1901_getMaxElement.java
@@ -1,21 +1,12 @@
+//To solve this question, we can use a binary search approach. Using binary search, we will chose the row, but in the row, finding the max element will be 
+//an O(n) operation. So, we first get the mid col, then get the max element in the mid col, which means we would have already checked the top and bottom, 
+//we would just need to check the left and right after getting the max element in that col. If we find a peak element, we return.
+//If not, we chose the side which has the higher number.
+
+//Time: O(nlogm) where n and m are the dimensions
+//Space: O(1)
 public class Solution {
-    // find row index of max element and col index is given by mid
-    public int getMaxElement(int[][] matrix, int mid) {
-        int index = -1;
-        int maxi = Integer.MIN_VALUE;
-
-        for (int row = 0; row < matrix.length; row++) {
-            int elm = matrix[row][mid];
-
-            if (elm > maxi) {
-                maxi = Math.max(maxi, elm);
-                index = row;
-            }
-        }
-        return index;
-    }
-
-    public int[] findPeakGrid(int[][] matrix) {
+        public int[] findPeakGrid(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
 
@@ -23,16 +14,14 @@ public class Solution {
         int end = m - 1;
 
         while (start <= end) {
-            int mid = start + (end - start) / 2;
+            int mid = (start + end) / 2;
 
             // we find maximum because we have to check top, bottom, left, right if we use maximum then our element is definitely
             // greater than top and bottom. so we need to check only left and right i.e. our problem is reduced to find peak in 1d
-
             int row = getMaxElement(matrix, mid);
             int left = -1;
             int right = -1;
 
-            // handling edge cases
             if (mid - 1 >= 0) {
                 left = matrix[row][mid - 1];
             }
@@ -58,5 +47,21 @@ public class Solution {
         }
 
         return new int[]{-1, -1};
+    }
+
+    // find row index of max element and col index is given by mid
+    public int getMaxElement(int[][] matrix, int mid) {
+        int index = -1;
+        int maxi = Integer.MIN_VALUE;
+
+        for (int row = 0; row < matrix.length; row++) {
+            int elm = matrix[row][mid];
+
+            if (elm > maxi) {
+                maxi = Math.max(maxi, elm);
+                index = row;
+            }
+        }
+        return index;
     }
 }
