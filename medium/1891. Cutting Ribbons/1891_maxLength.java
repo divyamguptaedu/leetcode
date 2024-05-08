3930d47020f4262bb0638ab82415a7bf272d4c4a
Divyam Gupta <dgupta92@gatech.edu>
2024-05-07 22:15:31 -0700

    Add 1891

--- medium/1891. Cutting Ribbons/1891_maxLength.java
+++ medium/1891. Cutting Ribbons/1891_maxLength.java
@@ -1,3 +1,9 @@
+//We can solve this question using binary search approach. The minimum length of the cut can be 1 and the max can be the max number in ribbons. 
+//Once we have the range, we get the mid value, use it as a proposed cut value. We check how man segments can we make with this mid value,
+//if it is >= k then we save it, and chose the right half. Otherwise, left half. We update the result if we find a higher number.
+
+//Time: O(nlogn)
+//Space: O(1)
 class Solution {
     public int maxLength(int[] ribbons, int k) {      
         int n = ribbons.length;
@@ -11,7 +17,7 @@ class Solution {
         int ribbonSegmentCutMaxLength = 0;
 
         while (end >= start) {
-            int mid = start + (end - start) / 2;
+            int mid = (start + end) / 2;
             int ribbonSegmnetCut = 0;
             for (int ribbon : ribbons){
                 ribbonSegmnetCut += (ribbon / mid);
