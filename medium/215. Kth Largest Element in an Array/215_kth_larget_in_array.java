98f8d98f8e3e2104d9b4fe1ad7bb045a747bdc46
Divyam Gupta <dgupta92@gatech.edu>
2024-05-08 21:34:59 -0700

    Add 215

--- medium/215. Kth Largest Element in an Array/215_kth_larget_in_array.java
+++ medium/215. Kth Largest Element in an Array/215_kth_larget_in_array.java
@@ -1,24 +1,19 @@
-"""
-Performance:
-Runtime: 4 ms, faster than 62.81% of Java online submissions for Kth Largest Element in an Array.
-Memory Usage: 39.1 MB, less than 74.98% of Java online submissions for Kth Largest Element in an Array.
-"""
+//To solve this question, we can just add all the elements to a pq, and make sure that we are removing from the heap
+//as soon as the heap size is > k, so that we are only keeping the top k elements in the heap. 
+//Once the loop ends, we just return the heap.peek element, which is the kth largest element in the array.
 
+//Time: O(nlogk)
+//Space: O(k)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-
-        // add all n in nums to a max heap.
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
-        for (int n : nums) {
-        	pq.add(n);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int num: nums) {
+            heap.add(num);
+            if (heap.size() > k) {
+                heap.remove();
+            }
         }
-
-        // remove top k - 1;
-        for (int i = 1; i < k; i++) {
-        	pq.poll();
-        }
-
-        // return kth;
-        return pq.poll();
+        
+        return heap.peek();
     }
 }
