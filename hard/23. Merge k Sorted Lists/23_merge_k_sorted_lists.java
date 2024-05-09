e77f1f5c82e9615cc8fc33c1031d757c10a404f0
Divyam Gupta <dgupta92@gatech.edu>
2024-05-08 21:35:53 -0700

    Add 23

--- hard/23. Merge k Sorted Lists/23_merge_k_sorted_lists.java
+++ hard/23. Merge k Sorted Lists/23_merge_k_sorted_lists.java
@@ -1,3 +1,10 @@
+//To solve this question, we will just initialize a priority queue with a comparator which places the smaller
+//element first. We will add all the nodes in the pq and will add their children as well if present.
+//Then we one by one poll the pq and initialize the next creating a merged sorted list.
+
+//Time: O(NlogK) where N is the total number of nodes in the final list, and k is the number of lists.
+//Space: O(k) for the heap. Heap takes less space than N is most situations.
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,24 +17,34 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (ListNode list : lists) {
-            while (list != null) {
-                pq.add(list.val);
-                list = list.next;
+        ListNode head = new ListNode(0);
+        ListNode point = head;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(
+            new Comparator<ListNode>() {
+                @Override
+                public int compare(ListNode o1, ListNode o2) {
+                    if (o1.val > o2.val) {
+                        return 1; //place o1 later
+                    } else if (o1.val == o2.val) {
+                        return 0; //order doesn't matter
+                    } else {
+                        return -1; //place o1 first
+                    }
+                }
+            }
+        );
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.add(node);
             }
         }
-        
-        ListNode ans = new ListNode(0);
-        if (pq.size() == 0) {
-            return null;
-        } else {
-            ListNode temp = ans;
-            while (pq.size() != 0) {
-                temp.next = new ListNode(pq.poll());
-                temp = temp.next;
+        while (!queue.isEmpty()) {
+            point.next = queue.poll();
+            point = point.next;
+            if (point.next != null) {
+                queue.add(point.next);
             }
         }
-        return ans.next;   
+        return head.next;
     }
 }
