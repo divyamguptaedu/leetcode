54369cbe45776d43066e41c73643a635b0c8b1e9
Divyam Gupta <dgupta92@gatech.edu>
2024-05-08 21:35:05 -0700

    Add 451

--- medium/451. Sort Characters By Frequency/451_frequencySort.java
+++ medium/451. Sort Characters By Frequency/451_frequencySort.java
@@ -1,71 +1,45 @@
-"""
-Performance:
-Runtime: 13 ms, faster than 88.21% of Java online submissions for Sort Characters By Frequency.
-Memory Usage: 46.6 MB, less than 74.80% of Java online submissions for Sort Characters By Frequency.
-"""
+//To solve this question, one approach is to first make a frequency map and then sort the keys based on the values, and then make a string.
+//This approach will be nlogn time, however, we can do this question in n time, using bucketsort instead of any other sort. 
+//So, we will make the frequency map, get the max frequency, and then make a list of list of chars representing buckets. 
+//For every key in the map, we will add that key to the bucket of its frequency. This is essentially sorting in n time.
+//Then we will iterate the buckets list of list starting from the end, and build our result string and return.
 
+//Time: O(n)
+//Space: O(n)
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Node> valueMap = new HashMap<>();
-        char[] temp = s.toCharArray();
-        for (char c : temp) {
-            if (valueMap.containsKey(c)) {
-                valueMap.get(c).frequency++;
-            } else {
-                valueMap.put(c, new Node(c, 1));
-            }
-        }
-        PriorityQueue<Node> heap = new PriorityQueue<>((x, y) -> y.frequency - x.frequency);
-        for (Node n : valueMap.values()) {
-            heap.add(n);
-        }
-        StringBuilder result = new StringBuilder();
-        while (!heap.isEmpty()) {
-            Node node = heap.poll();
-            while (node.frequency-- > 0) {
-                result.append(node.letter);
-            }
-        }
-        return result.toString();
-    }
-}
 
-class Node {
-    char letter;
-    int frequency;
-    Node(char letter, int frequency) {
-        this.letter = letter;
-        this.frequency = frequency;
-    }
-}
-
-//another solution
-
-class Solution {
-    public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (Character c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        
-        List<Character>[] buckets = new ArrayList[s.length() + 1];
-        for (Character c : map.keySet()) {
-            if (buckets[map.get(c)] == null) {
-                buckets[map.get(c)] = new ArrayList<>();       
-            }
-            buckets[map.get(c)].add(c);
+        if (s == null || s.isEmpty()) {
+            return s;
         }
 
-        StringBuilder result = new StringBuilder();
-        for (int i = buckets.length - 1; i >= 0; i--) {
-            if (buckets[i] != null) {
-                for (Character c : buckets[i]) {
-                    for (int j = 0; j < i; j++) {
-                        result.append(c);
-                    }
+        // Count up the occurances.
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        int maximumFrequency = Collections.max(counts.values()); //get the max
+
+        // Make the list of buckets and apply bucket sort.
+        List<List<Character>> buckets = new ArrayList<>();
+        for (int i = 0; i <= maximumFrequency; i++) {
+            buckets.add(new ArrayList<Character>());
+        }
+        for (Character key : counts.keySet()) {
+            int freq = counts.get(key);
+            buckets.get(freq).add(key);
+        }
+
+        // Build up the string.
+        StringBuilder sb = new StringBuilder();
+        for (int i = buckets.size() - 1; i >= 1; i--) {
+            for (Character c : buckets.get(i)) {
+                for (int j = 0; j < i; j++) {
+                    sb.append(c);
                 }
             }
-        } 
-        return result.toString();   
+        }
+        return sb.toString();
     }
 }
