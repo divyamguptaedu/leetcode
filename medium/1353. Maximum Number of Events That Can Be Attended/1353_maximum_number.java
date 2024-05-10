2d1071b12cb3976ed54fdf3405b547ea7ac37e55
Divyam Gupta <dgupta92@gatech.edu>
2024-05-09 21:05:08 -0700

    Add 1353

--- medium/1353. Maximum Number of Events That Can Be Attended/1353_maximum_number.java
+++ medium/1353. Maximum Number of Events That Can Be Attended/1353_maximum_number.java
@@ -1,30 +1,46 @@
-"
-Performance:
-Runtime: 66 ms, faster than 58.70% of Java online submissions for Maximum Number of Events That Can Be Attended.
-Memory Usage: 84.6 MB, less than 44.14% of Java online submissions for Maximum Number of Events That Can Be Attended.
-"
-
+// Sort + PriorityQueue Solution
+// 1. Sort events by start day
+// 2. Store end days of in progress events in min heap
+// 3. Join the earliest ending in progress evetns from the earliest start event to the latest start evetn.
+//    1) Get current date
+//    2) Add just started events at current day in the min heap
+//    3) Join the earliest ending event
+//    4) Remove already ended events
+// 4. Do the loop until we explore all the events and the min heap is empty.
+// Time complexity: O(NlogN)
+// Space complexity: O(N)
 class Solution {
     public int maxEvents(int[][] events) {
-        int count = 0;
-        int i = 0;
-        Arrays.sort(events,(e1,e2) -> Integer.compare(e1[0],e2[0])); //Sort the events by the start time and in case of tie by the end time in ascending order.
-
+        if (events == null || events.length == 0) {
+            return 0;
+        }
+        final int N = events.length;
+        // Sort events by start day.
+        Arrays.sort(events, (e1, e2) -> Integer.compare(e1[0], e2[0]));
+        // Store end days of in progress events in min heap.
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for (int day = 1; day <= 100000; day++) {
-           while (i < events.length && events[i][0]  == day) { //adding the events starting that day
-               pq.add(events[i][1]);
-               i++;
-           }             
-           while (!pq.isEmpty() && pq.peek()<day) {//removing the events already over by that day
-               pq.poll();
-           }
-           if (!pq.isEmpty()) {// attending the event that day and removing it
-              pq.poll();
-              count++;
+        // Join the earliest ending in progress evetns from the earliest start event to
+        // the latest start event.
+        int i = 0, day = 0, res = 0;
+        while (i < N || !pq.isEmpty()) {
+            // Get current date.
+            if (pq.isEmpty()) {
+                day = events[i][0];
+            }
+            // Add just started events at current day in the min heap.
+            while (i < N && day == events[i][0]) {
+                pq.add(events[i][1]);
+                i++;
+            }
+            // Join the earliest ending event.
+            pq.poll();
+            res++;
+            day++;
+            // Remove already ended events.
+            while (!pq.isEmpty() && day > pq.peek()) {
+                pq.poll();
             }
         }
-        return count;
+        return res;
     }
 }
