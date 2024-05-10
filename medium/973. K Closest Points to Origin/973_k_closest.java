f42a9a7b9cf007e6e625063f28411db0710bfb6e
Divyam Gupta <dgupta92@gatech.edu>
2024-05-09 21:05:48 -0700

    Add 973

--- medium/973. K Closest Points to Origin/973_k_closest.java
+++ medium/973. K Closest Points to Origin/973_k_closest.java
@@ -1,20 +1,29 @@
-/*
-sort by distance
-    sqrt(x^2 + y^2)
-return top k
-*/
+//We can define a distance function to calculate the distance and create a modified pq which compares
+//based on the distance of the points and sorts in descending order.
+//We just put all the points in the pq and then just poll k top points to return.
 
+//Time: O(NlogN)
+//Space: O(N)
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] result = new int[k][2];
-        Arrays.sort(points, (a, b) -> {           
-            int distanceA = (a[0]*a[0]+a[1]*a[1]);
-            int distanceB = (a[0]*b[0]+b[1]*b[1]);
-            return distanceB - distanceA;
-        });
-        for (int i = 0; i < k; i++) {
-            result[i] = points[i];
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (p1, p2) -> getDistance(p1).compareTo(getDistance(p2)));
+
+        for (int i = 0; i < points.length; i++) {
+            pq.add(points[i]);
         }
+
+        int[][] result = new int[k][2];
+
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll();
+        }
+
         return result;
+    }
+
+    private Double getDistance(int[] point) {
+        return Math.sqrt((point[0] * point[0]) + (point[1] * point[1]));
     }
 }
