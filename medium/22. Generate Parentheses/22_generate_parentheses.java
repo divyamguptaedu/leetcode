8f808135cd02e620127cbaa074495eeed69511c3
Divyam Gupta <dgupta92@gatech.edu>
2024-05-10 22:16:57 -0700

    Add 22

--- medium/22. Generate Parentheses/22_generate_parentheses.java
+++ medium/22. Generate Parentheses/22_generate_parentheses.java
@@ -1,61 +1,32 @@
-"""
-Performance:
-Runtime: 1 ms, faster than 72.29% of Java online submissions for Generate Parentheses.
-Memory Usage: 39 MB, less than 69.62% of Java online submissions for Generate Parentheses.
-"""
-
+//Time: O(4^n/sqrt(n))
+//Space: O(n)
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<String>();
-        String answer = "";
-        int start = 0;
-        int end = 0;
-        result = helper(start, end, n, answer, result);
-        return result;
-    }
-    
-    private List<String> helper(int start, int end, int n, String answer, List<String> result) {
-        if (answer.length() == (n + n)) {
-            result.add(answer);
-            return result;
-        }
-        String newString;
-        int newIndex;
-        // using recursion to add "(" n times.
-        if (start < n) {
-            newString = answer + "(";
-            newIndex = start + 1;
-            helper(newIndex, end, n, newString, result);
-        }
-        // using recursion to add ")" n times.
-        if (end < start) {
-            newString = answer + ")";
-            newIndex = end + 1;
-            helper(start, newIndex, n, newString, result);
-        }
-        return result;
-    }
-}
+        List<String> answer = new ArrayList<>();
+        backtracking(answer, new StringBuilder(), 0, 0, n);
 
-//another solution
-
-class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        helper(result, 0, 0, n, "");
-        return result;
+        return answer;
     }
 
-    public void helper(List<String> result, int close, int open, int n, String ans) {
-        if (close == n && open == n) {
-            result.add(ans);
+    private void backtracking(
+            List<String> answer,
+            StringBuilder curString,
+            int leftCount,
+            int rightCount,
+            int n) {
+        if (curString.length() == 2 * n) {
+            answer.add(curString.toString());
             return;
         }
-        if (open < n) {
-            helper(result, close, open + 1, n, ans + "(");
+        if (leftCount < n) {
+            curString.append("(");
+            backtracking(answer, curString, leftCount + 1, rightCount, n);
+            curString.deleteCharAt(curString.length() - 1);
         }
-        if (close < open) {
-            helper(result, close + 1, open, n, ans + ")");
+        if (leftCount > rightCount) {
+            curString.append(")");
+            backtracking(answer, curString, leftCount, rightCount + 1, n);
+            curString.deleteCharAt(curString.length() - 1);
         }
     }
 }
