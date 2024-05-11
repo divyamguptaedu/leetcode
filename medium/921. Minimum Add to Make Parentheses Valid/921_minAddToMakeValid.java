b953b1019b5413b3f0531876ac38c4d246057f24
Divyam Gupta <dgupta92@gatech.edu>
2024-05-10 22:17:47 -0700

    Add 921

--- medium/921. Minimum Add to Make Parentheses Valid/921_minAddToMakeValid.java
+++ medium/921. Minimum Add to Make Parentheses Valid/921_minAddToMakeValid.java
@@ -1,46 +1,17 @@
-"
-Performance:
-Runtime: 1 ms, faster than 89.16% of Java online submissions for Minimum Add to Make Parentheses Valid.
-Memory Usage: 41.7 MB, less than 71.90% of Java online submissions for Minimum Add to Make Parentheses Valid.
-"
-
+//Time: O(n)
+//Space: O(1)
 class Solution {
-    public int minAddToMakeValid(String s) {
-        int count = 0;
-        int error = 0;
-        for (char letter : s.toCharArray()) {
-            if (letter == '(')
-                count++;
-            else {
-                if (count == 0) {
-                    error++;
-                } else {
-                    count--;
-                }
+    public int minAddToMakeValid(String S) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            bal += S.charAt(i) == '(' ? 1 : -1;
+            // It is guaranteed bal >= -1
+            if (bal == -1) {
+                ans++;
+                bal++;
             }
         }
-        return error + count;
-    }
-}
 
-//another solution
-
-class Solution {
-    public int minAddToMakeValid(String s) {
-        int left = 0;
-        int right = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                left++;
-            } else {
-                if (left > 0) {
-                    left--;
-                } else {
-                    right++;
-                }
-            }
-        }
-        return left + right;
+        return ans + bal;
     }
 }
