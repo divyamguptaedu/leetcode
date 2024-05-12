0df98b2e7a5f88896c88f00c2fcb454df1cc9ed3
Divyam Gupta <dgupta92@gatech.edu>
2024-05-11 21:07:12 -0700

    Add 64

--- medium/64. Minimum Path Sum/64_minimum_path_sum.java
+++ medium/64. Minimum Path Sum/64_minimum_path_sum.java
@@ -1,27 +1,16 @@
-class Solution {
+public class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                dp[0][0] = grid[0][0];
-            } else {
-                dp[0][i] = dp[0][i-1] + grid[0][i];
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[0].length - 1; j >= 0; j--) {
+                if (i == grid.length - 1 && j != grid[0].length - 1)
+                    grid[i][j] = grid[i][j] + grid[i][j + 1];
+                else if (j == grid[0].length - 1 && i != grid.length - 1)
+                    grid[i][j] = grid[i][j] + grid[i + 1][j];
+                else if (j != grid[0].length - 1 && i != grid.length - 1)
+                    grid[i][j] = grid[i][j] +
+                            Math.min(grid[i + 1][j], grid[i][j + 1]);
             }
         }
-        for (int i = 0; i < m; i++) {
-            if (i == 0) {
-                dp[0][0] = grid[0][0];
-            } else {
-                dp[i][0] = dp[i-1][0] + grid[i][0];           
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(grid[i][j] + dp[i][j-1], grid[i][j] + dp[i-1][j]);
-            }
-        }
-        return dp[m - 1][n - 1];
+        return grid[0][0];
     }
 }
