8fb93ce2a2e9baec20411a738c36c683077c96f7
Divyam Gupta <dgupta92@gatech.edu>
2024-05-12 19:33:02 -0700

    Add 435

--- medium/435. Non-overlapping Intervals/435_eraseOverlapIntervals.java
+++ medium/435. Non-overlapping Intervals/435_eraseOverlapIntervals.java
@@ -1,25 +1,27 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         int n = intervals.length;
+
         if (n == 0) {
             return 0;
         }
+
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        int i = 0;
-        int j = 1;
-        int temp = 0;
+
+        int i = 0, j = 1, nonOverlappingIntervals = 0;
+
         while (j < n) {
             if (intervals[i][1] > intervals[j][0]) {
                 if (intervals[i][1] > intervals[j][1]) {
                     i = j;
                 }
-                temp++;
+                nonOverlappingIntervals++;
             } else {
                 i = j;
             }
             j++;
         }
-        
-        return temp;
+
+        return nonOverlappingIntervals;
     }
 }
