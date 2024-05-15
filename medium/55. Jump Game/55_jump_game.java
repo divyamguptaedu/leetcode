90a09948b44bbd3a9092654c7e5036b00fbfc997
Divyam Gupta <dgupta92@gatech.edu>
2024-05-14 22:00:15 -0700

    Add 55

--- medium/55. Jump Game/55_jump_game.java
+++ medium/55. Jump Game/55_jump_game.java
@@ -1,23 +1,11 @@
-"""
-Performance:
-Runtime: 2 ms, faster than 72.04% of Java online submissions for Jump Game.
-Memory Usage: 40.3 MB, less than 24.58% of Java online submissions for Jump Game.
-"""
-
-class Solution {
-
-	public boolean canJump(int[] nums) {
-		// keep jumping.
-	    int maximum = 0;
-	    for (int i = 0; i < nums.length; i++) {
-	    	// return false if you are beyond i;
-	        if (i > maximum) {
-	        	return false;
-	        }
-	        maximum = Math.max(maximum, i + nums[i]);
-	    }
-	    // else return true;
-	    return true;
-	}
-	
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
 }
