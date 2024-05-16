e717cea95b36e14cab242952ce444ba82c3bba7d
Divyam Gupta <dgupta92@gatech.edu>
2024-05-15 23:48:42 -0700

    Add 5

--- medium/5. Longest Palindromic Substring/5_longest_palindromic_substring.java
+++ medium/5. Longest Palindromic Substring/5_longest_palindromic_substring.java
@@ -1,78 +1,28 @@
-"""
-Performance:
-Runtime: 31 ms, faster than 50.50% of Java online submissions for Longest Palindromic Substring.
-Memory Usage: 39.7 MB, less than 37.90% of Java online submissions for Longest Palindromic Substring.
-"""
-
-class Solution {
-
+public class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() == 0) {
-        	return null;
-        }
-        if (s.length() == 1) {
-        	return s;
+        if (s.length() <= 1) {
+            return s;
         }
 
+        int maxLen = 1;
+        int start = 0;
+        int end = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
 
-        String longest = s.subString(0, 1);
-        for (int i = 0; i < s.length(); i++) {
-        	String temp = helper(s, i, i);
-        	if (temp.length() > longest.length()) {
-        		longest = temp;
-        	}
-        	temp = helper(s, i, i + 1);
-        	if (temp.length() > longest.length()) {
-        		longest = temp;
-        	}
-        }
-        return longest;
-    }
-
-    // helper to get the maximum length palindrome;
-    private String helper(String s, int i, int j) {
-    	while (i >= 0 && j <= s.length() - 1 && s.charAt(i) == s.charAt(j)) {
-    		i--;
-    		j++;
-    	}
-    	return s.subString(i + 1, j);
-    }
-    
-}
-
-//another solution
-
-class Solution {
-    public String longestPalindrome(String s) {
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            List<Integer> indexes = helperGetIndexes(s.substring(i), s.charAt(i), i);
-            for (Integer index: indexes) {
-                if (isPalindrome(s.substring(i, index + 1))) {
-                    if (result.length() < s.substring(i, index + 1).length()) {
-                        result = s.substring(i, index + 1);
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
+                        end = i;
                     }
-                    break;
                 }
             }
         }
-        return result;
-    }
-    private boolean isPalindrome(String s) {
-        for (int i = 0; i < s.length() / 2; i++ ) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private List<Integer> helperGetIndexes(String s, char letter, int buffer) {
-        List<Integer> indexes = new ArrayList();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == letter) {
-                indexes.add(i + buffer);
-            }
-        }
-        return indexes;
+
+        return s.substring(start, end + 1);
     }
 }
