839ca044a39e2ca9d0c8a4034188c6c6ae1fab4f
Divyam Gupta <dgupta92@gatech.edu>
2024-05-15 23:48:35 -0700

    Add 1312

--- hard/1312. Minimum Insertion Steps to Make a String Palindrome/1312_minimum_insertion.java
+++ hard/1312. Minimum Insertion Steps to Make a String Palindrome/1312_minimum_insertion.java
@@ -1,34 +1,30 @@
-"""
-Performance:
-Runtime: 17 ms, faster than 68.73% of Java online submissions for Minimum Insertion Steps to Make a String Palindrome.
-Memory Usage: 39.9 MB, less than 92.71% of Java online submissions for Minimum Insertion Steps to Make a String Palindrome.
-"""
-
 class Solution {
-	public int minInsertions(String s) {
-        if (s == null || s.isEmpty()) {
-        	return 0;
-        }
+    public int minInsertions(String s) {
         int n = s.length();
-        int[][] array = new int[n][n];
-        for (int i = 0; i < n; i++) {
-        	array[i][i] = 1;
+        int[][] dp = new int[n][n]; // dp to store if cases with current start and end character have been already
+                                    // handled
+        char[] ar = s.toCharArray();
+        return helper(ar, 0, n - 1, dp);
+    }
+
+    public int helper(char[] ar, int start, int end, int[][] dp) {
+        if (dp[start][end] != 0)
+            return dp[start][end]; // if handled this case return val
+
+        // iterating until to outer characters dont match
+        while (ar[start] == ar[end] && start < end) {
+            start++;
+            end--;
         }
-        for (int k = 2; k <= n; k++) {
-            for (int i = 0; i < n - k + 1; i++) {
-                int j = i + k - 1;
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (k == 2) {
-                    	array[i][j] = 2;
-                    } else {
-                    	array[i][j] = array[i + 1][j - 1] + 2;
-                    }
-                } else {
-                    array[i][j] = Math.max(array[i][j - 1], array[i + 1][j]);
-                }
-            }
-        }
-        
-        return n - array[0][n - 1];
+        // if every character in the current range match return 0
+        if (start >= end)
+            return 0;
+
+        // incrementing start pointer means we have inserted character equal to char at
+        // start in the position of end
+        // incrementing end pointer means we have inserted character equal to char at
+        // end in the position of start
+        int val = Math.min(helper(ar, start + 1, end, dp), helper(ar, start, end - 1, dp));
+        return dp[start][end] = val + 1; // adding 1
     }
 }
