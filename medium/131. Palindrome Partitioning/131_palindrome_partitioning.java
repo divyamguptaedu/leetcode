266130b421b03113e0ad1e30d46ad61dffa82b5c
Divyam Gupta <dgupta92@gatech.edu>
2024-05-18 18:09:29 -0700

    Add 131

--- medium/131. Palindrome Partitioning/131_palindrome_partitioning.java
+++ medium/131. Palindrome Partitioning/131_palindrome_partitioning.java
@@ -1,36 +1,37 @@
-"""
-Performance:
-Runtime: 20 ms, faster than 33.69% of Java online submissions for Palindrome Partitioning.
-Memory Usage: 192.6 MB, less than 45.10% of Java online submissions for Palindrome Partitioning.
-"""
+//I used a backtracking approach to find all possible palindrome partitions of the string. 
+//Starting from the beginning of the string, I iteratively generated all possible substrings. 
+//For each substring, I checked if it was a palindrome using a helper function. 
+//If it was, I added the substring to a current list and recursively explored further by advancing the starting index. 
+//Once I reached the end of the string, I added the current list of substrings to the result. 
+//After exploring each possibility, I backtracked by removing the last added substring to explore other potential partitions.
 
+//Time: O(2^n * n)
+//Space: O(n)
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        List<String> temp = new ArrayList<>();
-        palindromePartition(s, 0, temp, result);
+        List<List<String>> result = new ArrayList<List<String>>();
+        backtrack(0, result, new ArrayList<String>(), s);
         return result;
     }
-    
-    public void palindromePartition(String s, int index, List<String> temp, List<List<String>> result){
-        if (index == s.length()) {
-            result.add(new ArrayList<>(temp));
-            return;
-        }
-        for (int i = index; i < s.length(); ++i) {
-            if (isPalindrome(s, index, i)){
-                temp.add(s.substring(index, i + 1));
-                palindromePartition(s, i + 1, temp, result);
-                temp.remove(temp.size() - 1);
+
+    private void backtrack(int start, List<List<String>> result, List<String> currentList, String s) {
+        if (start >= s.length())
+            result.add(new ArrayList<String>(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                // add current substring in the currentList
+                currentList.add(s.substring(start, end + 1));
+                backtrack(end + 1, result, currentList, s);
+                // backtrack and remove the current substring from currentList
+                currentList.remove(currentList.size() - 1);
             }
         }
     }
-    
-    public boolean isPalindrome(String s, int start, int end){
-        while (start < end){
-            if (s.charAt(start++) != s.charAt(end--)) {
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--))
                 return false;
-            }
         }
         return true;
     }
