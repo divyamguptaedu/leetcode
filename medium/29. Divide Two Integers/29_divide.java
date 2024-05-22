be29dec2034ae18abcd7b56baf28c364a17326d7
Divyam Gupta <dgupta92@gatech.edu>
2024-05-21 22:54:17 -0700

    Add 29

--- medium/29. Divide Two Integers/29_divide.java
+++ medium/29. Divide Two Integers/29_divide.java
@@ -1,28 +1,73 @@
-class Solution {
-    public int divide(int dividend, int divisor) {
-        int i;
-        int j;
-        int j1;
+//To divide two integers without using multiplication, division, and mod operator, 
+//I employed bit manipulation and simulated the division process. 
+//Firstly, I handled overflow cases where the dividend is Integer.MIN_VALUE and 
+//the divisor is -1 or 1. Then, I converted both numbers to negatives and counted the 
+//number of negative signs. After that, I found the largest doubling of the divisor that 
+//could fit into the dividend in the negative 32-bit integer range. 
+//Starting from the biggest bit, I shifted the divisor to the right until I couldn't shift 
+//it further, updating the quotient accordingly. 
+//Finally, I adjusted the sign of the quotient based on the count of negative signs.
 
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
-       
-        if ((divisor < 0 && dividend > 0) || (divisor > 0 && dividend < 0)) {
-            j = dividend/divisor;
-            return j;
+//Time: O(log n)
+//Space: O(1)
+class Solution {
+    private static int HALF_INT_MIN = -1073741824;
+
+    public int divide(int dividend, int divisor) {
+        // Special cases: overflow.
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
         }
-        if (divisor > 0 && dividend > 0) {
-            j = dividend / divisor;
-            return j;
+        if (dividend == Integer.MIN_VALUE && divisor == 1) {
+            return Integer.MIN_VALUE;
         }
-        if (divisor < 0 && dividend < 0) {   
-            j = dividend / divisor;
-            if (j == min) {
-                return max;
-            } else {
-                return j;
+
+        /* We need to convert both numbers to negatives.
+         * Also, we count the number of negatives signs. */
+        int negatives = 2;
+        if (dividend > 0) {
+            negatives--;
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            negatives--;
+            divisor = -divisor;
+        }
+
+        /* We want to find the largest doubling of the divisor in the negative 32-bit
+         * integer range that could fit into the dividend.
+         * Note if it would cause an overflow by being less than HALF_INT_MIN,
+         * then we just stop as we know double it would not fit into INT_MIN anyway. */
+        int maxBit = 0;
+        while (divisor >= HALF_INT_MIN && divisor + divisor >= dividend) {
+            maxBit += 1;
+            divisor += divisor;
+        }
+
+        int quotient = 0;
+        /* We start from the biggest bit and shift our divisor to the right
+         * until we can't shift it any further */
+        for (int bit = maxBit; bit >= 0; bit--) {
+            /* If the divisor fits into the dividend, then we should set the current
+             * bit to 1. We can do this by subtracting a 1 shifted by the appropriate
+             * number of bits. */
+            if (divisor >= dividend) {
+                quotient -= (1 << bit);
+                /* Remove the current divisor from the dividend, as we've now
+                 * considered this part. */
+                dividend -= divisor;
             }
+            /* Shift the divisor to the right so that it's in the right place
+             * for the next positon we're checking at. */
+            divisor = (divisor + 1) >> 1;
         }
-        return 0;
+
+        /* If there was originally one negative sign, then
+         * the quotient remains negative. Otherwise, switch
+         * it to positive. */
+        if (negatives != 1) {
+            quotient = -quotient;
+        }
+        return quotient;
     }
 }
