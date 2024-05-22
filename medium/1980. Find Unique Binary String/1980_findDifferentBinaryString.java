88caad456d1c7d8a8cbc424e904d5c8d0c0429a0
Divyam Gupta <dgupta92@gatech.edu>
2024-05-21 23:05:21 -0700

    Add 1980

--- medium/1980. Find Unique Binary String/1980_findDifferentBinaryString.java
+++ medium/1980. Find Unique Binary String/1980_findDifferentBinaryString.java
@@ -1,10 +1,20 @@
+//I iterate through the input array of binary strings. 
+//For each string, I access the character at the current index i and append its 
+//complement to the result string. Since the input strings are of length n and 
+//each string is unique, I can guarantee that there will be no repetition in the 
+//resulting binary string. Thus, I obtain a binary string of length n that is 
+//different from any string in the input array. 
+
+//Time: O(n)
+//Space: O(1)
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int length = nums.length;
-        StringBuilder number = new StringBuilder();
-        for (int i = 0; i < length; i ++) {
-            number.append(nums[i].charAt(i) == '1' ? '0' : '1');
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            Character curr = nums[i].charAt(i);
+            ans.append(curr == '0' ? '1' : '0');
         }
-        return number.toString();
+
+        return ans.toString();
     }
 }
