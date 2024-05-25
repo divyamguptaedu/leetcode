95eaf48019111effcc099a9d46a42186f89aa4b4
Divyam Gupta <dgupta92@gatech.edu>
2024-05-24 21:16:11 -0700

    Add 224

--- hard/224. Basic Calculator/224_basic_calculator.java
+++ hard/224. Basic Calculator/224_basic_calculator.java
@@ -1,45 +1,32 @@
-"
-Performance:
-Runtime: 5 ms, faster than 86.27% of Java online submissions for Basic Calculator.
-Memory Usage: 38.7 MB, less than 96.04% of Java online submissions for Basic Calculator.
-"
-
-public int calculate(String s) {
-
-    Stack<Integer> stack = new Stack<Integer>();
-    int result = 0;
-    int number = 0;
-    int sign = 1;
-
-    for (int i = 0; i < s.length(); i++) {
-        char c = s.charAt(i);
-        if (Character.isDigit(c)) {
-            number = 10 * number + (int)(c - '0');
-        } else if (c == '+') {
-            result += sign * number;
-            number = 0;
-            sign = 1;
-        } else if (c == '-') {
-            result += sign * number;
-            number = 0;
-            sign = -1;
-        } else if (c == '(') {
-            //we push the result first, then sign;
-            stack.push(result);
-            stack.push(sign);
-            //reset the sign and result for the value in the parenthesis
-            sign = 1;   
-            result = 0;
-        } else if (c == ')'){
-            result += sign * number;  
-            number = 0;
-            result *= stack.pop();    //stack.pop() is the sign before the parenthesis
-            result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
-            
+class Solution {
+    public int calculate(String s) {
+        int sum = 0;
+        int sign = 1;
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                int val = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    val = val * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                i--;
+                val = val * sign;
+                sign = 1;
+                sum += val;
+            } else if (ch == '(') {
+                st.push(sum);
+                st.push(sign);
+                sum = 0;
+                sign = 1;
+            } else if (ch == ')') {
+                sum *= st.pop();
+                sum += st.pop();
+            } else if (ch == '-') {
+                sign *= -1;
+            }
         }
+        return sum;
     }
-    if (number != 0) {
-        result += sign * number;
-    }
-    return result;
 }
