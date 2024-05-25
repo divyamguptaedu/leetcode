578d0de9189bed59ae82f7ad5d570b563ed08245
Divyam Gupta <dgupta92@gatech.edu>
2024-05-24 21:15:08 -0700

    Add 227

--- medium/227. Basic Calculator II/227_basic_calculator.java
+++ medium/227. Basic Calculator II/227_basic_calculator.java
@@ -1,45 +1,29 @@
-"
-Performance:
-Runtime: 9 ms, faster than 64.35% of Java online submissions for Basic Calculator II.
-Memory Usage: 38.8 MB, less than 88.87% of Java online submissions for Basic Calculator II.
-"
-
 class Solution {
     public int calculate(String s) {
-
-        if (s == null || s.isEmpty()) {
-        	return 0;
-        }
+        if (s == null || s.isEmpty())
+            return 0;
         int length = s.length();
-        Stack<Integer> stack = new Stack<Integer>();
-        int current = 0;
+        int currentNumber = 0, lastNumber = 0, result = 0;
         char operation = '+';
         for (int i = 0; i < length; i++) {
             char currentChar = s.charAt(i);
             if (Character.isDigit(currentChar)) {
-                current = (current * 10) + (currentChar - '0');
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
             }
             if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
-                if (operation == '-') {
-                    stack.push(-current);
-                }
-                else if (operation == '+') {
-                    stack.push(current);
-                }
-                else if (operation == '*') {
-                    stack.push(stack.pop() * current);
-                }
-                else if (operation == '/') {
-                    stack.push(stack.pop() / current);
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
                 }
                 operation = currentChar;
-                current = 0;
+                currentNumber = 0;
             }
         }
-        int result = 0;
-        while (!stack.isEmpty()) {
-            result += stack.pop();
-        }
+        result += lastNumber;
         return result;
     }
 }
