fa4802be467065d6e12e7bf9b9c4661cc97ac5b3
Divyam Gupta <dgupta92@gatech.edu>
2024-05-24 21:15:42 -0700

    Add 670

--- medium/670. Maximum Swap/670_maximumSwap.java
+++ medium/670. Maximum Swap/670_maximumSwap.java
@@ -1,32 +1,19 @@
-"
-Performance:
-Runtime: 1 ms, faster than 84.55% of Java online submissions for Maximum Swap.
-Memory Usage: 38.9 MB, less than 97.78% of Java online submissions for Maximum Swap.
-"
-
 class Solution {
     public int maximumSwap(int num) {
-        char[] result = String.valueOf(num).toCharArray();
-        int[] locations = new int[10];
-        Arrays.fill(locations, -1);
-        for (int i = 0; i < result.length; i++) {
-            locations[result[i] - '0'] = i;
-        }
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 9; j >= 0; j--) {
-                if (j > result[i] - '0' && locations[j] >= 0 && i < locations[j]) {
-                    helper(result, i, locations[j]);
-                    return Integer.valueOf(new String(result));
-                }
+        int x = num;
+        int delta = 0;
+        int hi = x % 10, multiple = 1, prev = 1;
+        x /= 10;
+        while (x > 0) {
+            multiple *= 10;
+            if (x % 10 > hi) {
+                hi = x % 10;
+                prev = multiple;
+            } else if (x % 10 < hi) {
+                delta = (hi - x % 10) * multiple - (hi - x % 10) * prev;
             }
+            x /= 10;
         }
-        return Integer.valueOf(new String(result));
-    }
-    
-    public void helper(char[] array, int x, int y) {
-        char temp = array[x];
-        array[x] = array[y];
-        array[y] = temp;
-        return;
+        return num + delta;
     }
 }
