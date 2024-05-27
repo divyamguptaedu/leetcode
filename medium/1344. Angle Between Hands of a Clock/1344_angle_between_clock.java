bd5d749e3bdf3cc087b329fc27f4f8dc19ef02a1
Divyam Gupta <dgupta92@gatech.edu>
2024-05-26 21:34:54 -0700

    Add 1344

--- medium/1344. Angle Between Hands of a Clock/1344_angle_between_clock.java
+++ medium/1344. Angle Between Hands of a Clock/1344_angle_between_clock.java
@@ -1,16 +1,12 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Angle Between Hands of a Clock.
-Memory Usage: 36.1 MB, less than 75.31% of Java online submissions for Angle Between Hands of a Clock.
-"""
-
 class Solution {
     public double angleClock(int hour, int minutes) {
+        int oneMinAngle = 6;
+        int oneHourAngle = 30;
 
-        double min = 6.0 * minutes;
-        double hr = 30.0 * (hour % 12) + 0.5 * minutes;
-       
-        return Math.min(360.0 - Math.abs(min - hr), Math.abs(min - hr));
-        
+        double minutesAngle = oneMinAngle * minutes;
+        double hourAngle = (hour % 12 + minutes / 60.0) * oneHourAngle;
+
+        double diff = Math.abs(hourAngle - minutesAngle);
+        return Math.min(diff, 360 - diff);
     }
 }
