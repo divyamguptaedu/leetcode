df05eb0d153709b80309a68e638a5d20fb93d97f
Divyam Gupta <dgupta92@gatech.edu>
2024-05-26 21:33:50 -0700

    Add 273

--- hard/273. Integer to English Words/273_integer_english.java
+++ hard/273. Integer to English Words/273_integer_english.java
@@ -1,30 +1,31 @@
-"
-Performance:
-Runtime: 9 ms, faster than 67.42% of Java online submissions for Integer to English Words.
-Memory Usage: 39.3 MB, less than 32.53% of Java online submissions for Integer to English Words.
-"
+class Solution {
+    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+                    "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
-public class Solution {
-    private String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    private String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    
     public String numberToWords(int num) {
-        if (num == 0) {
-        	return "Zero";
-        }
-        return helper(num); 
+        if (num == 0)
+            return "Zero";
+        
+        return helper(num).trim();
     }
     
     private String helper(int num) {
-        String result = new String();
-        if (num < 10) result = belowTen[num];
-        else if (num < 20) result = belowTwenty[num -10];
-        else if (num < 100) result = belowHundred[num/10] + " " + helper(num % 10);
-        else if (num < 1000) result = helper(num/100) + " Hundred " +  helper(num % 100);
-        else if (num < 1000000) result = helper(num/1000) + " Thousand " +  helper(num % 1000);
-        else if (num < 1000000000) result = helper(num/1000000) + " Million " +  helper(num % 1000000);
-        else result = helper(num/1000000000) + " Billion " + helper(num % 1000000000);
-        return result.trim();
+        StringBuilder sb=new StringBuilder();
+        if (num >= 1000000000) {
+            sb.append(helper(num / 1000000000)).append(" Billion ").append( helper(num % 1000000000));
+        } else if  (num >= 1000000) {
+            sb.append(helper(num / 1000000)).append(" Million ").append( helper(num % 1000000));
+        }else if (num >= 1000) {
+            sb.append(helper(num / 1000)).append(" Thousand ").append( helper(num % 1000));
+        }else if (num >= 100) {
+            sb.append(helper(num / 100)).append(" Hundred ").append( helper(num % 100));
+        }else if (num >= 20) {
+            sb.append(tens[num / 10]).append(" ").append( helper(num % 10));
+        } else {
+            sb.append(ones[num]);
+        }
+        return sb.toString().trim();
+        
     }
 }
