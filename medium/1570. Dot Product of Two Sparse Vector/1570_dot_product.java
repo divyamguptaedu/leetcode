7bfa8d5f6af5c6d989b67a8a6128b4efaa9e6803
Divyam Gupta <dgupta92@gatech.edu>
2024-05-28 23:31:22 -0700

    Add 1570

--- medium/1570. Dot Product of Two Sparse Vector/1570_dot_product.java
+++ medium/1570. Dot Product of Two Sparse Vector/1570_dot_product.java
@@ -1,83 +1,30 @@
-"
-Performance:
-Runtime: 1 ms, faster than 100.00% of Java online submissions for Dot Product of Two Sparse Vectors.
-Memory Usage: 53.9 MB, less than 50.96% of Java online submissions for Dot Product of Two Sparse Vectors.
-"
-
-class SparseVector {
-    int[] vector;
-    SparseVector(int[] nums) {
-        this.vector = nums;
-    }
-    
-	// Return the dotProduct of two sparse vectors
-    public int dotProduct(SparseVector vec) {
-        int[] input = vec.vector;
-        int result = 0;
-        for (int i = 0; i < input.length; i++) {
-            result += input[i] * this.vector[i];
-        }
-        return result;
-    }
-}
-
-// Your SparseVector object will be instantiated and called as such:
-// SparseVector v1 = new SparseVector(nums1);
-// SparseVector v2 = new SparseVector(nums2);
-// int ans = v1.dotProduct(v2);
-
-//another solution
-
 class SparseVector {
 
-    List<Pair<Integer, Integer>> data = new ArrayList<>();
+    List<int[]> pairs;
 
     SparseVector(int[] nums) {
+        pairs = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                continue;
+            if (nums[i] != 0) {
+                pairs.add(new int[] { i, nums[i] });
             }
-            data.add(new Pair(i, nums[i]));
-        }
-    }
-    
-    public int dotProduct(SparseVector vec) {
-        if (data.size() < vec.data.size()) {
-            return helper(this.data, vec.data);
-        } else {
-            return helper(vec.data, this.data);
         }
     }
 
-    public int helper(List<Pair<Integer, Integer>> sparse, List<Pair<Integer, Integer>> notSparse) {
-        int result = 0;
-        for (Pair<Integer, Integer> pair : sparse) {
-            int x = search(notSparse, pair.getKey());
-            if (x != -1) {
-                result += pair.getValue() * x;
+    // Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0, p = 0, q = 0;
+        while (p < pairs.size() && q < vec.pairs.size()) {
+            if (pairs.get(p)[0] == vec.pairs.get(q)[0]) {
+                result += pairs.get(p)[1] * vec.pairs.get(q)[1];
+                p++;
+                q++;
+            } else if (pairs.get(p)[0] > vec.pairs.get(q)[0]) {
+                q++;
+            } else {
+                p++;
             }
         }
         return result;
     }
-
-    public int search(List<Pair<Integer, Integer>> data, int i) {
-        int left = 0;
-        int right = data.size() -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (data.get(mid).getKey() == i) {
-                return data.get(mid).getValue();
-            } else if (data.get(mid).getKey() < i) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
-    }
 }
-
-// Your SparseVector object will be instantiated and called as such:
-// SparseVector v1 = new SparseVector(nums1);
-// SparseVector v2 = new SparseVector(nums2);
-// int ans = v1.dotProduct(v2);
