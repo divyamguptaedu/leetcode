54f7c6fbe8a90b898e7150a814aa0326267b2a22
Divyam Gupta <dgupta92@gatech.edu>
2024-05-30 23:06:33 -0700

    Add 708

--- medium/708. Insert into a Sorted Circular Linked List/708_inserted_into_sorted_cll.java
+++ medium/708. Insert into a Sorted Circular Linked List/708_inserted_into_sorted_cll.java
@@ -1,55 +1,36 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node next;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _next) {
-        val = _val;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
-     public Node insert(Node head, int insertVal) {
-            
-            if (head == null) {
-                Node temp = new Node(insertVal);
-                temp.next=temp;
-                return temp;
-            }
-            
-            Node h = head;
-            boolean isInserted = false;
-            
-            while(!isInserted) {
-                Node temp = new Node(insertVal);
-                if ((head.val <= insertVal && head.next.val >= insertVal) ||
-                    (head.val >= insertVal && head.next.val >= insertVal && head.next.val < head.val) ||
-                    (head.val <= insertVal && head.next.val <= insertVal && head.next.val < head.val)) {
-                    Node next = head.next;
-                    head.next = temp;
-                    temp.next = next;  
-                    isInserted = true;
-                }
-                
-                Node = head;
-                head = head.next;
-                
-                if (head == h) {
-                    p.next = temp;
-                    temp.next = head;
-                    isInserted = true;
-                }
-              
-            }
-            return h;
+    public Node insert(Node head, int insertVal) {
+        if (head == null) {
+            Node newNode = new Node(insertVal, null);
+            newNode.next = newNode;
+            return newNode;
         }
+
+        Node prev = head;
+        Node curr = head.next;
+        boolean toInsert = false;
+
+        do {
+            if (prev.val <= insertVal && insertVal <= curr.val) {
+                // Case 1).
+                toInsert = true;
+            } else if (prev.val > curr.val) {
+                // Case 2).
+                if (insertVal >= prev.val || insertVal <= curr.val)
+                    toInsert = true;
+            }
+
+            if (toInsert) {
+                prev.next = new Node(insertVal, curr);
+                return head;
+            }
+
+            prev = curr;
+            curr = curr.next;
+        } while (prev != head);
+
+        // Case 3).
+        prev.next = new Node(insertVal, curr);
+        return head;
+    }
 }
