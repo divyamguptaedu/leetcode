2266039914d1f9ea08e9ec6224c6a80cdc6d1844
Divyam Gupta <dgupta92@gatech.edu>
2024-05-30 23:06:44 -0700

    Add 986

--- medium/986. Interval List Intersections/986_intervalIntersection.java
+++ medium/986. Interval List Intersections/986_intervalIntersection.java
@@ -1,21 +1,24 @@
 class Solution {
-    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        List<int[]> result = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        while (i < firstList.length && j < secondList.length) {
-            int low = Math.max(firstList[i][0], secondList[j][0]);
-            int high = Math.min(firstList[i][1], secondList[j][1]);
-            if (low <= high){
-                result.add(new int[]{low,high});
-            }
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> ans = new ArrayList();
+        int i = 0, j = 0;
 
-            if (firstList[i][1] < secondList[j][1]) {
+        while (i < A.length && j < B.length) {
+            // Let's check if A[i] intersects B[j].
+            // lo - the startpoint of the intersection
+            // hi - the endpoint of the intersection
+            int lo = Math.max(A[i][0], B[j][0]);
+            int hi = Math.min(A[i][1], B[j][1]);
+            if (lo <= hi)
+                ans.add(new int[] { lo, hi });
+
+            // Remove the interval with the smallest endpoint
+            if (A[i][1] < B[j][1])
                 i++;
-            } else {
+            else
                 j++;
-            }
         }
-        return result.toArray(new int[result.size()][]);
+
+        return ans.toArray(new int[ans.size()][]);
     }
 }
