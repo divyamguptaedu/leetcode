78bda02ab5c1a591c15e27a586c2a41a06b01f75
Divyam Gupta <dgupta92@gatech.edu>
2024-05-31 21:24:15 -0700

    Add 138

--- medium/138. Copy List with Random Pointer/138_copy_list_with_random_pointer.java
+++ medium/138. Copy List with Random Pointer/138_copy_list_with_random_pointer.java
@@ -1,43 +1,65 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Copy List with Random Pointer.
-Memory Usage: 42.5 MB, less than 6.04% of Java online submissions for Copy List with Random Pointer.
-"""
-
 /*
 // Definition for a Node.
 class Node {
-    int val;
-    Node next;
-    Node random;
+    public int val;
+    public Node next;
+    public Node random;
 
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
     }
-}
+};
 */
-
-class Solution {
+public class Solution {
     public Node copyRandomList(Node head) {
         if (head == null) {
             return null;
         }
-        HashMap<Node, Node> valueMap = new HashMap<>();
-        Node temp = head;
-        // create new nodes and add to hashmap;
-        while (temp != null) {
-            valueMap.put(temp, new Node(temp.val));
-            temp = temp.next;
+
+        // Creating a new weaved list of original and copied nodes.
+        Node ptr = head;
+        while (ptr != null) {
+            // Cloned node
+            Node newNode = new Node(ptr.val);
+
+            // Inserting the cloned node just next to the original node.
+            // If A->B->C is the original linked list,
+            // Linked list after weaving cloned nodes would be A->A'->B->B'->C->C'
+            newNode.next = ptr.next;
+            ptr.next = newNode;
+            ptr = newNode.next;
         }
-        temp = head;
-        // track down the next and random;
-        while (temp != null) {
-            valueMap.get(temp).next = valueMap.get(temp.next);
-            valueMap.get(temp).random = valueMap.get(temp.random);
-            temp = temp.next;
+
+        ptr = head;
+
+        // Now link the random pointers of the new nodes created.
+        // Iterate the newly created list and use the original nodes' random pointers,
+        // to assign references to random pointers for cloned nodes.
+        while (ptr != null) {
+            ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
+            ptr = ptr.next.next;
         }
-        return valueMap.get(head);
+
+        // Unweave the linked list to get back the original linked list and the cloned
+        // list.
+        // i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
+        Node ptr_old_list = head; // A->B->C
+        Node ptr_new_list = head.next; // A'->B'->C'
+        Node head_old = head.next;
+        while (ptr_old_list != null) {
+            ptr_old_list.next = ptr_old_list.next.next;
+            if (ptr_new_list.next != null) {
+                ptr_new_list.next = ptr_new_list.next.next;
+            } else {
+                ptr_new_list.next = null;
+            }
+            ptr_old_list = ptr_old_list.next;
+            ptr_new_list = ptr_new_list.next;
+        }
+        return head_old;
     }
 }
