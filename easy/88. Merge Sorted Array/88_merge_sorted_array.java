7bc085f308cc80f3bc059b74370897aa75676833
Divyam Gupta <dgupta92@gatech.edu>
2024-05-31 21:23:36 -0700

    Add 88

--- easy/88. Merge Sorted Array/88_merge_sorted_array.java
+++ easy/88. Merge Sorted Array/88_merge_sorted_array.java
@@ -1,34 +1,20 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Sorted Array.
-Memory Usage: 43.5 MB, less than 6.24% of Java online submissions for Merge Sorted Array.
-"""
-
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        m--;
-        n--;
-        for (int i = m + n + 1; i >= 0; i--) {
-            if ((n < 0) || (m >= 0 && nums1[m] > nums2[n])) {
-                nums1[i] = nums1[m];
-                m--;
+        // Set p1 and p2 to point to the end of their respective arrays.
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        // And move p backward through the array, each time writing
+        // the smallest value pointed at by p1 or p2.
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) {
+                break;
             }
-            else {
-                nums1[i] = nums2[n];
-                n--;
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
+            } else {
+                nums1[p] = nums2[p2--];
             }
         }
-    }
-}
-
-//Another solution:
-
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m;
-        for (int j = 0; j < n; j++) {
-            nums1[i++] = nums2[j];
-        }
-        Arrays.sort(nums1);
     }
 }
