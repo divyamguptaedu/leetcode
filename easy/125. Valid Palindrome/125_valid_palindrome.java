c5bba61376c9f27d56f0c060a58582f132bb3a4c
Divyam Gupta <dgupta92@gatech.edu>
2024-06-01 22:20:29 -0700

    Add 125

--- easy/125. Valid Palindrome/125_valid_palindrome.java
+++ easy/125. Valid Palindrome/125_valid_palindrome.java
@@ -1,30 +1,19 @@
-/**
-Runtime: 4 ms, faster than 82.24% of Java online submissions for Valid Palindrome.
-Memory Usage: 43.8 MB, less than 50.89% of Java online submissions for Valid Palindrome.
-**/
-
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-            return true;
-        }
-        int start = 0;
-        int end = s.length() - 1;
-        while (start <= end) {
-            char currStart = s.charAt(start);
-            char currEnd = s.charAt(end);
-            if (!Character.isLetterOrDigit(currStart )) {
-                start++;
-            } else if(!Character.isLetterOrDigit(currEnd)) {
-                end--;
-            } else {
-                if (Character.toLowerCase(currStart) != Character.toLowerCase(currEnd)) {
-                    return false;
-                }
-                start++;
-                end--;
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
             }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (
+                Character.toLowerCase(s.charAt(i)) !=
+                Character.toLowerCase(s.charAt(j))
+            ) return false;
         }
+
         return true;
     }
 }
