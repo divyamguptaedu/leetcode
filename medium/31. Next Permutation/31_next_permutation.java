a4a8db169cb348f2b3afdba46e898a2f3eb98998
Divyam Gupta <dgupta92@gatech.edu>
2024-06-01 22:21:31 -0700

    Add 31

--- medium/31. Next Permutation/31_next_permutation.java
+++ medium/31. Next Permutation/31_next_permutation.java
@@ -1,12 +1,4 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Next Permutation.
-Memory Usage: 39.1 MB, less than 57.38% of Java online submissions for Next Permutation.
-"""
-
 public class Solution {
-
-    // found the next big number;
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
         while (i >= 0 && nums[i + 1] <= nums[i]) {
@@ -22,10 +14,8 @@ public class Solution {
         reverse(nums, i + 1);
     }
 
-    // reverse the array;
     private void reverse(int[] nums, int start) {
-        int i = start;
-        int j = nums.length - 1;
+        int i = start, j = nums.length - 1;
         while (i < j) {
             swap(nums, i, j);
             i++;
@@ -33,7 +23,6 @@ public class Solution {
         }
     }
 
-    // swap two indices;
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
