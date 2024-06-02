d4c4e09d45024ee0d2aed3f101e1da564f97ee4d
Divyam Gupta <dgupta92@gatech.edu>
2024-06-01 22:20:48 -0700

    Add 304

--- medium/304. Range Sum Query 2D - Immutable/304_NumMatrix.java
+++ medium/304. Range Sum Query 2D - Immutable/304_NumMatrix.java
@@ -1,28 +1,18 @@
-"
-Performance:
-Runtime: 2323 ms, faster than 5.50% of Java online submissions for Range Sum Query 2D - Immutable.
-Memory Usage: 132.6 MB, less than 26.73% of Java online submissions for Range Sum Query 2D - Immutable.
-"
 class NumMatrix {
-    public int[][] table;
+    private int[][] dp;
+
     public NumMatrix(int[][] matrix) {
-        table = matrix;
-    }
-    
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        int result = 0;
-        for (int i = row1; i <= row2; i++) {
-            for (int j = col1; j <= col2; j++) {
-                result+=table[i][j];
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return;
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                dp[r + 1][c + 1] = dp[r + 1][c] + dp[r][c + 1] + matrix[r][c] - dp[r][c];
             }
         }
-        return result;
+    }
 
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
     }
 }
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
