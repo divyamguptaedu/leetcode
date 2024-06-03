0d488ff1b1927d51aa1b75a6161c694d1cbc09be
Divyam Gupta <dgupta92@gatech.edu>
2024-06-02 19:08:00 -0700

    Add 1047

--- easy/1047. Remove All Adjacent Duplicates In String/1047_remove_duplicates.java
+++ easy/1047. Remove All Adjacent Duplicates In String/1047_remove_duplicates.java
@@ -1,43 +1,25 @@
+//I iterated through the string, maintaining a StringBuilder to mimic stack behavior.
+//Whenever I encountered a character that matches the top of the stack, 
+//I popped the stack, simulating the removal of adjacent duplicates. 
+//Otherwise, I pushed the current character onto the stack. 
+//Finally, I returned the StringBuilder as a string. 
+
+//Time: O(n) where n is the string length
+//Space: O(n-d) where d is the total length for all duplicates
 class Solution {
-  public String removeDuplicates(String S) {
-      
-    HashSet<String> duplicates = new HashSet();
-    StringBuilder duplicate = new StringBuilder();
-    for (char i = 'a'; i <= 'z'; ++i) {
-        duplicate.setLength(0);
-        duplicate.append(i);
-        duplicate.append(i);
-        duplicates.add(duplicate.toString());
-    }
-
-    int prevLength = -1;
-    while (prevLength != S.length()) {
-      prevLength = S.length();
-      for (String d : duplicates) {
-          S = S.replace(d, "");
-      }
-    }
-
-    return S;
-  }
-}
-
-//another solution
-
-class Solution {
-    public String removeDuplicates(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
-                stack.pop();
+    public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder(); // StringBuilder to mimic stack behavior
+        int sbLength = 0; // Length of StringBuilder
+        for (char character : S.toCharArray()) {
+            // Check if the current character matches the top of the stack
+            if (sbLength != 0 && character == sb.charAt(sbLength - 1)) {
+                sb.deleteCharAt(sbLength - 1); // Remove the top of the stack
+                sbLength--;
             } else {
-                stack.push(s.charAt(i));
+                sb.append(character); // Push the current character onto the stack
+                sbLength++; // Update the length of the StringBuilder
             }
         }
-        StringBuilder builder = new StringBuilder("");
-        while (!stack.isEmpty()) {
-            builder.append(stack.pop());
-        }
-        return builder.reverse().toString();
+        return sb.toString(); // Convert StringBuilder to string
     }
 }
