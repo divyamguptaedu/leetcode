2be33efce40f22cd29e2c7c3ea8224f6b282a5c9
Divyam Gupta <dgupta92@gatech.edu>
2024-06-02 19:07:31 -0700

    Add 419

--- medium/419. Battleships in a Board/419_countBattleships.java
+++ medium/419. Battleships in a Board/419_countBattleships.java
@@ -1,29 +1,40 @@
-"
-Performance:
-Runtime: 2 ms, faster than 15.19% of Java online submissions for Battleships in a Board.
-Memory Usage: 44.5 MB, less than 31.39% of Java online submissions for Battleships in a Board.
-"
+//I traversed the board cell by cell, incrementing the count whenever I encountered a 
+//battleship ('X'). Then, using Depth-First Search (DFS), I marked all cells connected to 
+//this battleship, horizontally or vertically, as visited. 
+//This ensured that no adjacent battleships were counted multiple times. 
+//I terminated DFS when reaching out-of-bounds or encountering water ('.'). 
+//This approach effectively counts each distinct battleship only once.
 
+//Time: O(mn)
+//Space: O(1)
 class Solution {
+    int count = 0; // Initialize the count of battleships
+
     public int countBattleships(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-        int result = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'X') {
-                    if (i == 0 && j == 0) {
-                        result++;
-                    } else if (i == 0 && board[i][j - 1] != 'X') {
-                        result++;
-                    } else if (j == 0 && board[i - 1][j] != 'X') {
-                        result++;
-                    } else if (i != 0 && j != 0 && board[i - 1][j] != 'X' && board[i][j - 1] != 'X'){
-                        result++;
-                    }
+        int n = board.length; // Get the number of rows
+        int m = board[0].length; // Get the number of columns
+        // Traverse the board
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'X') { // If battleship found
+                    count++; // Increment count
+                    dfs(board, i, j, n, m); // Explore connected battleships
                 }
             }
         }
-        return result;
+        return count; // Return total count of battleships
+    }
+
+    public void dfs(char[][] board, int i, int j, int n, int m) {
+        // Base cases for DFS termination
+        if (i < 0 || j < 0 || i == n || j == m || board[i][j] == '.') {
+            return;
+        }
+        board[i][j] = '.'; // Mark the cell as visited
+        // Explore adjacent cells
+        dfs(board, i + 1, j, n, m);
+        dfs(board, i, j + 1, n, m);
+        dfs(board, i - 1, j, n, m);
+        dfs(board, i, j - 1, n, m);
     }
 }
