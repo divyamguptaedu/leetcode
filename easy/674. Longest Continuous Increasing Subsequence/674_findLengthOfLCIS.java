2bdef623bde5118915e5a8f2a22c743bf976f4fc
Divyam Gupta <dgupta92@gatech.edu>
2024-06-03 23:25:46 -0700

    Add 674

--- easy/674. Longest Continuous Increasing Subsequence/674_findLengthOfLCIS.java
+++ easy/674. Longest Continuous Increasing Subsequence/674_findLengthOfLCIS.java
@@ -1,18 +1,22 @@
+//I iterated through the array with a pointer 'anchor' marking the start of the current
+//increasing subsequence. As I traversed the array, 
+//I compared each element with the previous one. 
+//If an element was not greater than the previous, 
+//I updated anchor to the current position. 
+//I continuously tracked the maximum length of increasing subsequence encountered so far.
+
+//Time: O(n)
+//Space: O(1)
 class Solution {
     public int findLengthOfLCIS(int[] nums) {
-        int counter = 1;
-        int result = 1;
-        
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i + 1] > nums[i]) {
-                counter = counter + 1;
-            } else {
-                counter = 1; 
+        int ans = 0;
+        int anchor = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > 0 && nums[i - 1] >= nums[i]) {
+                anchor = i;
             }
-            if (result < counter) {
-                result = counter;
-            }
+            ans = Math.max(ans, i - anchor + 1);
         }
-        return result;
+        return ans;
     }
 }
