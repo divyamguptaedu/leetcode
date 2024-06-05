fc6518333d4169c2408e5b1b18bdb88e89a458a9
Divyam Gupta <dgupta92@gatech.edu>
2024-06-04 22:16:53 -0700

    Add 303

--- easy/303. Range Sum Query - Immutable/303_NumArray.java
+++ easy/303. Range Sum Query - Immutable/303_NumArray.java
@@ -1,19 +1,25 @@
+//To solve the problem, I used a prefix sum array. 
+//First, I initialized the prefix sum array with an extra element to handle the sum
+//calculation efficiently. I filled the prefix sum array by adding the current element
+//of the input array to the previous sum. 
+//For the sumRange method, I simply subtracted the prefix sum at the start index from 
+//the prefix sum at the end index plus one.
+
+//Time: Constructor takes n time, sumRange takes constant.
+//Space: O(n)
 class NumArray {
-    int temp[];
+    int[] sum; // Prefix sum array
+
     public NumArray(int[] nums) {
-        temp = new int[nums.length];
+        sum = new int[nums.length + 1]; // Initialize prefix sum array
         for (int i = 0; i < nums.length; i++) {
-            temp[i] = nums[i];
+            sum[i + 1] = sum[i] + nums[i]; // Compute prefix sums
         }
-    }
-    public int sumRange(int left, int right) {
-        int sum = 0;
-        for (int i = left; i <= right; i++) {
-            sum += temp[i];
-        }
-        return sum;
     }
 
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i]; // Calculate range sum using prefix sums
+    }
 }
 
 /**
