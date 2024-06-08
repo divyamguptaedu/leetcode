afd6165d04d5ebf0dabc338507ec4215d3989428
Divyam Gupta <dgupta92@gatech.edu>
2024-06-08 15:49:56 -0700

    Add 92

--- medium/92. Reverse Linked List II/92_reverse_linked_list_ii.java
+++ medium/92. Reverse Linked List II/92_reverse_linked_list_ii.java
@@ -1,61 +1,60 @@
-"""
-Performance: 
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List II.
-Memory Usage: 36.6 MB, less than 73.22% of Java online submissions for Reverse Linked List II.
+//I iterated through the linked list until reaching the left and right positions to be
+//reversed, keeping track of the nodes before and after the sublist to be reversed.
+//Then, I reversed the sublist iteratively by adjusting the next pointers.
+//Finally, I adjusted the connections of the adjacent nodes to integrate the reversed
+//sublist into the overall list structure.
+//If the sublist starts from the beginning of the list, I updated the head node.
+//This approach modifies the list in place without requiring extra space.
 
-"""
+//Time: n
+//Space: constant
 
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (right <= 1 || left < 1) {
-            return head;
+        if (head == null) {
+            return null; // Return null if the list is empty
         }
-        int count = 1;
-        
-        ListNode dummyNode = new ListNode(0);
-        dummyNode.next = head;
-        head = dummyNode;
-        ListNode start = head;
-        ListNode end = null;
-        ListNode temp = head;
-        while (head != null) {
-            if (count == left) {
-                start = head;
-            }
-            if (count == right + 1) {
-                end = head.next;
-                head.next = null;
-            }
-            count++;
-            head = head.next;
-        }
-        if (start != null) {
 
-            ListNode previous = start.next;
-            start.next = reverse(start.next);
-            previous.next = end;
+        ListNode current = head;
+        ListNode previous = null;
+        // Move pointers to the starting position of the sublist to be reversed
+        while (left > 1) {
+            previous = current;
+            current = current.next;
+            left--;
+            right--;
         }
-        return temp.next;
-    }
-    
-     private static ListNode reverse(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+
+        ListNode connection = previous;
+        ListNode tail = current;
+        ListNode nextNode = null;
+        // Reverse the sublist
+        while (right > 0) {
+            nextNode = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextNode;
+            right--;
         }
-        ListNode previous = head.next;
-        ListNode next = reverse(head.next);
-        previous.next = head;
-        head.next = null;
-        return next;
+
+        // Adjust connections
+        if (connection != null) {
+            connection.next = previous;
+        } else {
+            head = previous; // If sublist starts from the beginning, update head
+        }
+
+        tail.next = current;
+        return head;
     }
 }
