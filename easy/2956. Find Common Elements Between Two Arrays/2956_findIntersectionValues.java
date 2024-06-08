ffaa2ba8e6bfea289ad9f6d7600df2989b806c94
Divyam Gupta <dgupta92@gatech.edu>
2024-06-08 15:49:28 -0700

    Add 2956

--- easy/2956. Find Common Elements Between Two Arrays/2956_findIntersectionValues.java
+++ easy/2956. Find Common Elements Between Two Arrays/2956_findIntersectionValues.java
@@ -1,30 +1,36 @@
-public class Solution {
+//check constraints for the array, should be 100. So, array size 101.
+
+//I initialized two arrays to track the presence of elements from each array. 
+//Then, I iterated through each array, marking the presence of elements in their
+//respective tracking arrays. Afterward, I iterated through each array again,
+//counting the occurrences of elements from the opposite array in the tracking arrays.
+//Finally, I returned the counts of occurrences for both arrays.
+//This allowed me to find the number of common elements between the two arrays
+//in both directions.
+
+//Time: n + m
+//Space: constant, fixed 101 elements
+class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        Set<Integer> firstSet = new HashSet<>();
-        Set<Integer> secondSet = new HashSet<>();
-
+        int[] tracker1 = new int[101];
+        int[] tracker2 = new int[101];
+        // Mark presence of elements from nums1
         for (int num : nums1) {
-            firstSet.add(num);
+            tracker1[num] = 1;
         }
-
+        // Mark presence of elements from nums2
         for (int num : nums2) {
-            secondSet.add(num);
+            tracker2[num] = 1;
         }
-
-        int firstCounter = 0;
+        int[] commonCounts = new int[2];
+        // Count occurrences of nums2 elements in nums1
         for (int num : nums1) {
-            if (secondSet.contains(num)) {
-                firstCounter++;
-            }
+            commonCounts[0] += tracker2[num];
         }
-
-        int secondCounter = 0;
+        // Count occurrences of nums1 elements in nums2
         for (int num : nums2) {
-            if (firstSet.contains(num)) {
-                secondCounter++;
-            }
+            commonCounts[1] += tracker1[num];
         }
-
-        return new int[]{firstCounter, secondCounter};
+        return commonCounts;
     }
 }
