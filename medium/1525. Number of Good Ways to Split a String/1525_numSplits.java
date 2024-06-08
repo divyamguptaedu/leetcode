9c2f0bcb91fc0f41a98bb8ba6076b55ee7f45206
Divyam Gupta <dgupta92@gatech.edu>
2024-06-08 15:50:15 -0700

    Add 1525

--- medium/1525. Number of Good Ways to Split a String/1525_numSplits.java
+++ medium/1525. Number of Good Ways to Split a String/1525_numSplits.java
@@ -1,25 +1,49 @@
+//I first counted the unique characters on the string using a frequency array. 
+//Then, I traversed the string while updating the count of unique characters
+//on both sides.
+//If the number of unique characters on the left matched the number on the right
+//at any point, I incremented the result. 
+//This ensured that I found all good splits where the number of distinct characters 
+//on both sides was the same.
+
+//Example Input: aacaba
+//We will make the freq map: a -> 4, c -> 1, b -> 1
+//Then we will take the first char a, decrement the freq, increment the uniqueOnLeft, won't decrement uniqueOnLeft because freq is still >0.
+//In such a way, we will check all possibilities, and make the result.
+
+//Time: n
+//Space: constant because its at max 26.
 class Solution {
     public int numSplits(String s) {
-        HashSet<Character> hs = new HashSet<Character>();
-        int n = s.length();
-        int[] presum = new int[n];
-        int[] postsum = new int[n];
-        for (int i = 0; i < n; i++) {
-            hs.add(s.charAt(i));
-            presum[i] = hs.size();
+        int res = 0;
+
+        int[] freq = new int[26];
+        int uniqueOnLeft = 0;
+        int uniqueOnRight = 0;
+        for (char c : s.toCharArray()) {
+            if (freq[c - 'a'] == 0) {
+                uniqueOnRight++;
+            }
+            freq[c - 'a']++;
         }
-        
-        hs = new HashSet<Character>();
-        for (int i = n - 1; i >= 0; i--) {
-            hs.add(s.charAt(i));
-            postsum[i] = hs.size();
-        }
-        int count = 0;
-        for (int i = 1; i < n; i++) {
-            if (presum[i - 1] == postsum[i]) {
-                count++;
+
+        // Set<Character> set = new HashSet<>();
+        boolean[] uniqueCharsOnLeft = new boolean[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']--;
+            if (!uniqueCharsOnLeft[c - 'a']) {
+                uniqueOnLeft++;
+                uniqueCharsOnLeft[c - 'a'] = true;
+            }
+            if (freq[c - 'a'] == 0) {
+                uniqueOnRight--;
+            }
+
+            if (uniqueOnLeft == uniqueOnRight) {
+                res++;
             }
         }
-        return count;
+
+        return res;
     }
 }
