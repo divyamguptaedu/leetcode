6b62a55bb1a54996c73fb06a4dbef8b89471ecdc
Divyam Gupta <dgupta92@gatech.edu>
2024-06-12 22:04:58 -0700

    Add 289

--- medium/289. Game of Life/289_game_of_life.java
+++ medium/289. Game of Life/289_game_of_life.java
@@ -1,41 +1,69 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Game of Life.
-Memory Usage: 40.6 MB, less than 86.36% of Java online submissions for Game of Life.
-"""
+//I approached the Game of Life problem by iterating through each cell in the
+//board and applying the rules simultaneously to determine the next state of 
+//each cell based on its neighbors. Using a neighbors array, 
+//I checked all eight possible neighbors for each cell, counting live neighbors. 
+//Depending on the current state of the cell and the count of live neighbors, 
+//I updated the cell's state accordingly: marking live cells to die (-1) or 
+//marking dead cells to become alive (2). After computing the next state 
+//for all cells, I finalized the board by converting temporary states (-1 and 2) 
+//back to 0 and 1, respectively.
 
+//Time: MN
+//Space: constant
 class Solution {
     public void gameOfLife(int[][] board) {
-        int[][] result = new int[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                int sides = helper(i, j, board);
-                if (board[i][j] == 1 && sides < 2) 
-                    result[i][j] = 0;
-                if (board[i][j] == 1 && ((sides == 2) || sides == 3))
-                    result[i][j] = 1;
-                if (board[i][j] == 1 && sides > 3) 
-                    result[i][j] = 0;
-                if (board[i][j] == 0 && sides == 3) 
-                    result[i][j] = 1;
+
+        // Neighbors array to find 8 neighboring cells for a given cell
+        int[] neighbors = { 0, 1, -1 };
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Iterate through board cell by cell.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // For each cell count the number of live neighbors.
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // Check the validity of the neighboring cell.
+                            // and whether it was originally a live cell.
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (Math.abs(board[r][c]) == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                // Rule 1 or Rule 3
+                if ((board[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    // -1 signifies the cell is now dead but originally was live.
+                    board[row][col] = -1;
+                }
+                // Rule 4
+                if (board[row][col] == 0 && liveNeighbors == 3) {
+                    // 2 signifies the cell is now live but was originally dead.
+                    board[row][col] = 2;
+                }
             }
         }
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = result[i][j];
+
+        // Get the final representation for the newly updated board.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (board[row][col] > 0) {
+                    board[row][col] = 1;
+                } else {
+                    board[row][col] = 0;
+                }
             }
         }
-    }
-    
-    private int helper(int i, int j, int[][] board) {
-        int result = 0;
-        int[][] temp = {{0,-1},{0,1},{1,0},{1,-1},{1,1},{-1,-1},{-1,1},{-1,0}};
-        for (int[] t : temp) {   
-            int x = i + t[0];
-            int y = j + t[1];
-            if (x >= 0 && y >= 0 && x < board.length && y < board[0].length)
-                result += board[x][y];
-         }
-         return result;
     }
 }
