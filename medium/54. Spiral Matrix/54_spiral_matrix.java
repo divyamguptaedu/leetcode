c5dd44f87e2757c1d2bfe34d3f5b4983af55eb87
Divyam Gupta <dgupta92@gatech.edu>
2024-06-12 22:04:49 -0700

    Add 54

--- medium/54. Spiral Matrix/54_spiral_matrix.java
+++ medium/54. Spiral Matrix/54_spiral_matrix.java
@@ -1,39 +1,52 @@
+//To solve the problem of traversing a matrix in spiral order, 
+//I used a directional approach. I defined four possible movement directions 
+//(right, down, left, up) and tracked the current direction. 
+//I started at the top-left corner of the matrix and marked visited 
+//elements with a special value. While traversing, 
+//I kept changing the direction when hitting the matrix boundaries or a 
+//previously visited cell. I repeated this process until 
+//I covered all the elements in the matrix.
+
+//Time: MN
+//Space: constant
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        int top = 0;
-        int left = 0;
-        int bottom = matrix.length - 1;
-        int right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-            //Top Row
-            for (int i = left ; i <= right ; i++) {
-                result.add(matrix[top][i]);
+        int VISITED = 101;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        // Four directions that we will move: right, down, left, up.
+        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        // Initial direction: moving right.
+        int currentDirection = 0;
+        // The number of times we change the direction.
+        int changeDirection = 0;
+        // Current place that we are at is (row, col).
+        // row is the row index; col is the column index.
+        int row = 0;
+        int col = 0;
+        // Store the first element and mark it as visited.
+        List<Integer> result = new ArrayList<>();
+        result.add(matrix[0][0]);
+        matrix[0][0] = VISITED;
+        while (changeDirection < 2) {
+            while (row + directions[currentDirection][0] >= 0 &&
+                    row + directions[currentDirection][0] < rows &&
+                    col + directions[currentDirection][1] >= 0 &&
+                    col + directions[currentDirection][1] < columns &&
+                    matrix[row + directions[currentDirection][0]][col +
+                            directions[currentDirection][1]] != VISITED) {
+                // Reset this to 0 since we did not break and change the direction.
+                changeDirection = 0;
+                // Calculate the next place that we will move to.
+                row = row + directions[currentDirection][0];
+                col = col + directions[currentDirection][1];
+                result.add(matrix[row][col]);
+                matrix[row][col] = VISITED;
             }
-            top++;
-
-            //Right Column
-            for (int i = top ; i <= bottom; i++) {
-                result.add(matrix[i][right]);
-            }
-            right--;
-
-            //Bottom Row
-            if (top <= bottom) {
-                for (int i = right; i >= left; i --) {
-                    result.add(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            
-            //Left Column
-            if (left <= right) {
-                for (int i = bottom; i >= top; i --) {
-                    result.add(matrix[i][left]);
-                }
-                left++;
-            }
+            // Change our direction.
+            currentDirection = (currentDirection + 1) % 4;
+            // Increment change_direction because we changed our direction.
+            changeDirection++;
         }
         return result;
     }
