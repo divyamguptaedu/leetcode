c0344f6d03297015de38e72c8b016f02181fd227
Divyam Gupta <dgupta92@gatech.edu>
2024-06-13 23:29:59 -0700

    Add 402

--- medium/402. Remove K Digits/402_removeKdigits.java
+++ medium/402. Remove K Digits/402_removeKdigits.java
@@ -1,31 +1,45 @@
-"""
-Performance:
-Runtime: 11 ms, faster than 93.29% of Java online submissions for Remove K Digits.
-Memory Usage: 42.4 MB, less than 98.88% of Java online submissions for Remove K Digits.
-"""
+//To find the smallest possible integer after removing k digits from a given 
+//string representing a non-negative integer, I used a greedy algorithm with a stack. 
+//I iterated over each digit in the string, and for each digit, 
+//I removed the last digit from the stack if it was larger than the current digit 
+//and k was still greater than zero. This ensured that smaller digits were kept 
+//for a smaller resultant number. 
+//After processing all digits, I removed any remaining digits if k was still 
+//greater than zero. Finally, I built the resulting number string while skipping 
+//leading zeros.
 
+//Time: n
+//Space: n
 class Solution {
     public String removeKdigits(String num, int k) {
-        int size = num.length();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0 ; i < size ; i++){
-            while (k > 0 && result.length() > 0 && result.charAt(result.length()-1) > num.charAt(i)) {
-                k--;
-                result.deleteCharAt(result.length()-1);
+        LinkedList<Character> stack = new LinkedList<Character>();
+
+        for (char digit : num.toCharArray()) {
+            while (stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+                stack.removeLast();
+                k -= 1;
             }
-            result.append(num.charAt(i));
+            stack.addLast(digit);
         }
-        while (k > 0 && result.length() > 0) {
-            result.deleteCharAt(result.length() - 1);
-            k--;                
-        }      
-        int index = 0;
-        while (index < result.length() && result.charAt(index) == '0') {
-            index++;
+
+        /* remove the remaining digits from the tail. */
+        for (int i = 0; i < k; ++i) {
+            stack.removeLast();
         }
-        if (index == result.length()) {
+
+        // build the final string, while removing the leading zeros.
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        for (char digit : stack) {
+            if (leadingZero && digit == '0')
+                continue;
+            leadingZero = false;
+            ret.append(digit);
+        }
+
+        /* return the final string */
+        if (ret.length() == 0)
             return "0";
-        }
-        return result.toString().substring(index);
+        return ret.toString();
     }
 }
