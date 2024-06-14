688fe814386c1a8698df0d7616f71ff0f3a5762e
Divyam Gupta <dgupta92@gatech.edu>
2024-06-13 23:29:21 -0700

    Add 203

--- easy/203. Remove Linked List Elements/203_remove.ll.java
+++ easy/203. Remove Linked List Elements/203_remove.ll.java
@@ -1,72 +1,38 @@
-//
-// Runtime: 1 ms, faster than 82.20% of Java online submissions for Remove Linked List Elements.
-// Memory Usage: 39.6 MB, less than 89.45% of Java online submissions for Remove Linked List Elements.
+//To remove nodes from a linked list with a given value, 
+//I created a dummy node to simplify edge cases where the head node needs removal. 
+//I used two pointers, prev and curr, to traverse the list. 
+//Prev tracked the last valid node, while curr checked each node. 
+//If curr.val matched the target value, I bypassed it by setting prev.next to 
+//curr.next. Otherwise, I moved prev to curr. 
+//This way, I maintained a clean, updated linked list without the target values 
+//and returned the list starting from dummy.next.
 
+//Time: N
+//Space: constant
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null) {
-            return null;
-        }
-        while (head.val == val) {
-            head = head.next;
-            if (head == null) {
-                return head;
-            }
-        }
-        ListNode temp1 = head;
-        ListNode temp2 = head.next;
-        while (temp2 != null) {
-            if (temp2.val == val) {
-      
-                temp2 = temp2.next;
-                temp1.next = temp2;
-            } else {
-                temp1 = temp1.next;
-                temp2 = temp2.next;
-            }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
+        ListNode prev = dummy;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val == val)
+                prev.next = curr.next;
+            else
+                prev = curr;
+            curr = curr.next;
         }
-        
-        return head;
-    }
-}
-
-//another solution
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeElements(ListNode head, int val) {
-        ListNode temp = new ListNode(0);
-        temp.next = head;
-        ListNode curr = temp;
-        
-        while (curr.next != null) {
-            if (curr.next.val == val) {
-                curr.next = curr.next.next;
-            } else {
-                curr = curr.next;
-            }
-        }
-        
-        return temp.next;
+        return dummy.next;
     }
 }
