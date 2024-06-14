392aa2f3ac41cc562195fd2f0878b137e1019d3a
Divyam Gupta <dgupta92@gatech.edu>
2024-06-13 23:30:46 -0700

    Add 27

--- easy/27. Remove Element/27_remove_element.java
+++ easy/27. Remove Element/27_remove_element.java
@@ -1,17 +1,26 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Element.
-Memory Usage: 42.2 MB, less than 63.84% of Java online submissions for Remove Element.
-"""
+//To remove all occurrences of a given value from an integer array in-place, 
+//I used a two-pointer approach. I initialized a pointer i to traverse the array 
+//and another pointer n as the effective length of the array. 
+//When nums[i] equaled the target value, I replaced it with the last element 
+//(nums[n-1]) and reduced n by one. If the value was not the target, I incremented i. 
+//This method allowed me to overwrite the elements to be removed and 
+//return the new length of the modified array.
+
+//Time: n
+//Space: constant
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[result] = nums[i];
-                result++;
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                // reduce array size by one
+                n--;
+            } else {
+                i++;
             }
         }
-        return result;
+        return n;
     }
 }
