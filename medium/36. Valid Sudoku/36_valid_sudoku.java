c213b5de4ccb4e14c0fc0b8beb13441f67499ead
Divyam Gupta <dgupta92@gatech.edu>
2024-06-13 23:30:34 -0700

    Add 36

--- medium/36. Valid Sudoku/36_valid_sudoku.java
+++ medium/36. Valid Sudoku/36_valid_sudoku.java
@@ -1,34 +1,52 @@
+//To check if a 9x9 Sudoku board is valid, 
+//I used three arrays to track the presence of numbers in rows, 
+//columns, and 3x3 sub-boxes. For each cell, 
+//I skipped empty cells and calculated a bit position for the current digit. 
+//I checked if the bit was already set in the respective row, column, or sub-box. 
+//If any bit was already set, the board was invalid. 
+//If not, I set the bit in the corresponding row, column, and sub-box. 
+//This ensured no duplicates in rows, columns, or sub-boxes.
+
+//Time: n^2
+//Space" n
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        int N = 9;
 
-        List<Set<Character>> rowValues = new ArrayList<>();
-        List<Set<Character>> colValues = new ArrayList<>();
-        List<Set<Character>> gridValues = new ArrayList<>();
+        // Use a binary number to record previous occurrence
+        int[] rows = new int[N];
+        int[] cols = new int[N];
+        int[] boxes = new int[N];
 
-        for (int i = 0; i < 9; i++) {
-            rowValues.add(new HashSet<>());
-            colValues.add(new HashSet<>());
-            gridValues.add(new HashSet<>());
-        }
-
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                // Check if the position is filled with number
                 if (board[r][c] == '.') {
                     continue;
                 }
-                
-                int gridIndex = (r/3) * 3 + (c/3);
-                char cellValue = board[r][c];
+                int val = board[r][c] - '0';
+                int pos = 1 << (val - 1);
 
-                if (rowValues.get(r).contains(cellValue) || colValues.get(c).contains(cellValue) || gridValues.get(gridIndex).contains(cellValue)) {
+                // Check the row
+                if ((rows[r] & pos) > 0) {
                     return false;
                 }
-                rowValues.get(r).add(cellValue);
-                colValues.get(c).add(cellValue);
-                gridValues.get(gridIndex).add(cellValue);
+                rows[r] |= pos;
+
+                // Check the column
+                if ((cols[c] & pos) > 0) {
+                    return false;
+                }
+                cols[c] |= pos;
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if ((boxes[idx] & pos) > 0) {
+                    return false;
+                }
+                boxes[idx] |= pos;
             }
         }
-
         return true;
     }
 }
