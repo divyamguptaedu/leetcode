105eea91d2616d9406120fe698523c2f1e8481b9
Divyam Gupta <dgupta92@gatech.edu>
2024-06-13 23:30:57 -0700

    Add 126

--- hard/126. Word Ladder II/126_wordLadderII.java
+++ hard/126. Word Ladder II/126_wordLadderII.java
@@ -5,6 +5,56 @@
 //The BFS ensured that the paths were the shortest, and the DFS allowed for 
 //backtracking to collect all possible paths.
 
+/*
+Detailed Processing of the Input Example
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+
+BFS Construction
+Start with beginWord = "hit".
+Explore possible transformations:
+hit -> hot (valid), update reverse graph: hot -> {hit}
+Queue: ["hot"], Word Set: ["dot","dog","lot","log","cog"]
+Next layer with hot:
+hot -> dot (valid), update reverse graph: dot -> {hot}
+hot -> lot (valid), update reverse graph: lot -> {hot}
+Queue: ["dot", "lot"], Word Set: ["dog","log","cog"]
+Next layer with dot and lot:
+dot -> dog (valid), update reverse graph: dog -> {dot}
+lot -> log (valid), update reverse graph: log -> {lot}
+Queue: ["dog", "log"], Word Set: ["cog"]
+Next layer with dog and log:
+dog -> cog (valid), update reverse graph: cog -> {dog}
+log -> cog (valid), update reverse graph: cog -> {log}
+Queue: ["cog"], Word Set: []
+endWord found, stop BFS.
+DFS Path Construction
+Start DFS from cog:
+Path: ["cog"]
+Explore cog -> dog and cog -> log
+For cog -> dog:
+Path: ["cog", "dog"]
+Explore dog -> dot
+Path: ["cog", "dog", "dot"]
+Explore dot -> hot
+Path: ["cog", "dog", "dot", "hot"]
+Explore hot -> hit
+Path: ["cog", "dog", "dot", "hot", "hit"]
+Reverse and add to ans: ["hit", "hot", "dot", "dog", "cog"]
+For cog -> log:
+Path: ["cog", "log"]
+Explore log -> lot
+Path: ["cog", "log", "lot"]
+Explore lot -> hot
+Path: ["cog", "log", "lot", "hot"]
+Explore hot -> hit
+Path: ["cog", "log", "lot", "hot", "hit"]
+Reverse and add to ans: ["hit", "hot", "lot", "log", "cog"]
+Final Output:
+[
+  ["hit", "hot", "dot", "dog", "cog"],
+  ["hit", "hot", "lot", "log", "cog"]
+]
+*/
 //Time: NM^2 where N is the #words, and M is the max len of a word.
 //Space: NM
 class Solution {
@@ -21,7 +71,7 @@ class Solution {
             String word = queue.remove();
             for (String next : wordSet) {
                 if (isLadder(word, next)) { // is ladder words
-                    // construct the reverse graph from endWord
+					// construct the reverse graph from endWord
                     Set<String> reverseLadders = reverse.computeIfAbsent(next, k -> new HashSet<>());
                     reverseLadders.add(word); 
                     if (endWord.equals(next)) {
@@ -40,7 +90,7 @@ class Solution {
         if (!findEnd) return ans; // if can't reach endWord from startWord, then return ans.
         Set<String> path = new LinkedHashSet<>();
         path.add(endWord);
-        // traverse reverse graph from endWord to beginWord
+		// traverse reverse graph from endWord to beginWord
         findPath(endWord, beginWord, reverse, ans, path); 
         return ans;
     }
