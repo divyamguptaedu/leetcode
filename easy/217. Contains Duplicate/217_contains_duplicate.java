d32ee8dd5cd96c912c154e3a13fb85409b7735c3
Divyam Gupta <dgupta92@gatech.edu>
2024-06-13 23:29:51 -0700

    Add 217

--- easy/217. Contains Duplicate/217_contains_duplicate.java
+++ easy/217. Contains Duplicate/217_contains_duplicate.java
@@ -1,16 +1,13 @@
-"
-Performance:
-Runtime: 10 ms, faster than 34.01% of Java online submissions for Contains Duplicate.
-Memory Usage: 54 MB, less than 9.29% of Java online submissions for Contains Duplicate.
-"
-
+//Just added all elements to a hashset, and if we encounter any element in the set, 
+//which already existed, then we return true, if not false.
+//Time: n
+//Space: n
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>(nums.length);
-        for (int x: nums) {
-            if (set.contains(x)) {
+        for (int x : nums) {
+            if (set.contains(x))
                 return true;
-            }
             set.add(x);
         }
         return false;
