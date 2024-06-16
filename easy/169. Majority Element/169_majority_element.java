4d36df640d23bb5c5a215bf34392cb3a1ab7ca49
Divyam Gupta <dgupta92@gatech.edu>
2024-06-15 19:16:50 -0700

    Add 169

--- easy/169. Majority Element/169_majority_element.java
+++ easy/169. Majority Element/169_majority_element.java
@@ -1,52 +1,28 @@
-"""
-Performance:
-Runtime: 10 ms, faster than 22.22% of Java online submissions for Majority Element.
-Memory Usage: 44.2 MB, less than 63.75% of Java online submissions for Majority Element.
-"""
+//I initialized a count to zero and a candidate variable to zero as well. 
+//As I iterated through the array, I checked if the count was zero to assign the current number 
+//as the candidate. If the current number matched the candidate, 
+//I increased the count; otherwise, I decreased it. 
+//The candidate remaining after the loop was the majority element, 
+//as it appeared more than half the time.
 
+//Time: n
+//Space: constant
 class Solution {
     public int majorityElement(int[] nums) {
+        int count = 0; // Initialize count to zero
+        int candidate = 0; // Variable to store potential majority element
 
-        // create hashmap for each distinct int in array and update its frequency.
-        HashMap<Integer, Integer> valueMap = new HashMap<>();
-        int value = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (valueMap.get(nums[i]) == null) {
-                valueMap.put(nums[i], 1);
-                value = 1;
+        for (int num : nums) {
+            if (count == 0) { // If count is zero, select new candidate
+                candidate = num;
+            }
+            if (num == candidate) {
+                count += 1; // Increase count if current element is the candidate
             } else {
-                value = valueMap.get(nums[i]);
-                value++;
-                valueMap.put(nums[i], value);
-            }
-
-            // return int if its frequency fulfills the condition.
-            if (value > (nums.length / 2)) {
-                return nums[i];
+                count -= 1; // Decrease count if current element is not the candidate
             }
         }
-        return value;
-    }
-}
 
-//another solution
-
-class Solution {
-    public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }  
-        }
-        for (int key : map.keySet()) {
-            if (map.get(key) > (n / 2)) {
-                return key;
-            }
-        }
-        return 0;
+        return candidate; // Return the candidate which is the majority element
     }
 }
