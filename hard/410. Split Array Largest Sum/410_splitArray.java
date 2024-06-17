6c12d3931139abb7b759fdd4db64969cbd7bf500
Divyam Gupta <dgupta92@gatech.edu>
2024-06-16 18:57:18 -0700

    Add 410

--- hard/410. Split Array Largest Sum/410_splitArray.java
+++ hard/410. Split Array Largest Sum/410_splitArray.java
@@ -1,6 +1,6 @@
 //I employed a binary search strategy to minimize the largest sum of the split subarrays. 
 //The main idea was to efficiently determine the smallest possible value for the largest sum 
-//such that the array nums can be split into exactly m subarrays.
+//such that the array nums can be split into exactly k subarrays.
 //First, I calculated the boundaries for the binary search: left was initialized to the maximum 
 //element in nums, and right to the sum of all elements in nums. Then, using binary search, 
 //I iteratively adjusted left and right based on whether the current midpoint (maxSumAllowed) 
@@ -9,29 +9,16 @@
 //subarray's sum doesn't exceed maxSumAllowed. Based on this count, 
 //I adjusted the search boundaries to converge on the minimum possible largest sum.
 
+//For the input: nums = [7,2,5,10,8], k = 2
+//left = max element = 10, right = sum = 32
+//In the first iteration, maxSumAllowed = (10+32)/2 = 21, possible to split nums to [7, 2, 5] and [10, 8]
+//Sum for both splits remain under 21, so move the right pointer to 20 now. Second iteration, 
+//maxSumAllowed = 15, will need 3 subarrays, move left to 16. Third iteration, we get 18, possible to split in 2.
+//So, we store it, move the right to 17. But it won't be possible. So, in the end, 18 will be returned.
+
 //Time: N log S where n is the length of nums, s is the sum of integers in the array.
 //Space: constant
 class Solution {
-    private int minimumSubarraysRequired(int[] nums, int maxSumAllowed) {
-        int currentSum = 0;
-        int splitsRequired = 0;
-
-        for (int element : nums) {
-            // Add element only if the sum doesn't exceed maxSumAllowed
-            if (currentSum + element <= maxSumAllowed) {
-                currentSum += element;
-            } else {
-                // If the element addition makes sum more than maxSumAllowed
-                // Increment the splits required and reset sum
-                currentSum = element;
-                splitsRequired++;
-            }
-        }
-
-        // Return the number of subarrays, which is the number of splits + 1
-        return splitsRequired + 1;
-    }
-
     public int splitArray(int[] nums, int m) {
         // Find the sum of all elements and the maximum element
         int sum = 0;
@@ -61,5 +48,25 @@ class Solution {
         }
 
         return minimumLargestSplitSum;
+    }
+
+    private int minimumSubarraysRequired(int[] nums, int maxSumAllowed) {
+        int currentSum = 0;
+        int splitsRequired = 0;
+
+        for (int element : nums) {
+            // Add element only if the sum doesn't exceed maxSumAllowed
+            if (currentSum + element <= maxSumAllowed) {
+                currentSum += element;
+            } else {
+                // If the element addition makes sum more than maxSumAllowed
+                // Increment the splits required and reset sum
+                currentSum = element;
+                splitsRequired++;
+            }
+        }
+
+        // Return the number of subarrays, which is the number of splits + 1
+        return splitsRequired + 1;
     }
 }
