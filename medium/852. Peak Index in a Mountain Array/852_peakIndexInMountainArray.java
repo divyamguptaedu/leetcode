125b98eab4414bd773ece1012473430e4b1333f9
Divyam Gupta <dgupta92@gatech.edu>
2024-06-19 21:11:18 -0700

    Add 852

--- medium/852. Peak Index in a Mountain Array/852_peakIndexInMountainArray.java
+++ medium/852. Peak Index in a Mountain Array/852_peakIndexInMountainArray.java
@@ -1,26 +1,16 @@
-"
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Peak Index in a Mountain Array.
-Memory Usage: 73.5 MB, less than 5.01% of Java online submissions for Peak Index in a Mountain Array.
-"
-
+//Used simple binary search.
+//Time: logn
+//Space: constant
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        return helper(arr, 0, arr.length - 1);
-    }
-    
-    private int helper(int[] arr, int left, int right) {
-        left = Math.max(0, left);
-        right = Math.min(right, arr.length - 1);
-        int middle = left + (right - left) / 2;
-        if (arr[middle] > arr[middle - 1] && arr[middle] > arr[middle + 1]) {
-            return middle;
+        int l = 0, r = arr.length - 1, mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (arr[mid] < arr[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
         }
-        
-        if (arr[middle] < arr[middle + 1]) {
-            return helper(arr, middle, right);
-        } else {
-            return helper(arr, left, middle);
-        }
+        return l;
     }
 }
