af4c161e01f103c6ffd341e48e69373b828639ac
Divyam Gupta <dgupta92@gatech.edu>
2024-06-19 21:12:05 -0700

    Add 463

--- easy/463. Island Perimeter/463_islandPerimeter.java
+++ easy/463. Island Perimeter/463_islandPerimeter.java
@@ -1,30 +1,30 @@
+//I approached the problem of calculating the perimeter of the island in a 2D grid by iterating through each cell. 
+//For each land cell (grid[r][c] == 1), I initially added 4 to the perimeter, assuming it's surrounded on all sides. 
+//Then, I checked adjacent cells to subtract potential shared edges (result -= 2 for each adjacent land cell).
+//Time: mn
+//Space: constant
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {  
-                    return dfs(grid, i, j, visited);
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int result = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    result += 4;
+
+                    if (r > 0 && grid[r - 1][c] == 1) {
+                        result -= 2;
+                    }
+
+                    if (c > 0 && grid[r][c - 1] == 1) {
+                        result -= 2;
+                    }
                 }
             }
         }
-        return 0;
-    }
-    
-    public int dfs(int[][] grid, int x, int y, boolean[][] visited) {
-        if (x < 0 || x > grid.length - 1 || y < 0 || y > grid[0].length - 1) {
-            return 1;
-        }
-        if (grid[x][y] == 0) {
-            return 1;
-        } else if (grid[x][y] == 1 && !visited[x][y]) {
-            visited[x][y] = true;
-            int top = dfs(grid, x, y + 1, visited);
-            int down = dfs(grid, x, y - 1, visited);
-            int right = dfs(grid, x + 1, y, visited);
-            int left = dfs(grid, x - 1, y, visited);
-            return top + down + right + left;        
-        }
-        return 0;
+
+        return result;
     }
 }
