3b9a65594e02a8739fb3cd5f097f35fd4dc9d4a3
Divyam Gupta <dgupta92@gatech.edu>
2024-06-22 18:52:33 -0700

    Add 168

--- easy/168. Excel Sheet Column Title/168_convertToTitle.java
+++ easy/168. Excel Sheet Column Title/168_convertToTitle.java
@@ -1,21 +1,24 @@
-"""
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Excel Sheet Column Title.
-Memory Usage: 39.5 MB, less than 87.39% of Java online submissions for Excel Sheet Column Title.
-"""
+//I employed a iterative approach using a StringBuilder. 
+//Starting from the integer, I decremented it by 1 to handle the 1-based indexing in Excel columns. 
+//Using modulo operation (columnNumber % 26), I derived the character corresponding to the least significant digit of the column title. 
+//This character was then appended to the StringBuilder after converting it to the corresponding character ('A' + remainder). 
+//I then updated columnNumber by integer division (columnNumber / 26) to process the next digit. 
+//Finally, I reversed the StringBuilder to obtain the correct order of characters.
 
+//TC: logn
+//SC: constant
 class Solution {
-    public String convertToTitle(int n) {
-        StringBuilder result = new StringBuilder();
-        while(n > 0 ){  
-            int a = n % 26; 
-            if (a == 0) {       
-                a = 26;
-                n--;
-             }
-            result.append((char)(a + 64));
-            n/=26;
+    public String convertToTitle(int columnNumber) {
+        StringBuilder ans = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+            // Get the last character and append it at the end of the string.
+            ans.append((char) (((columnNumber) % 26) + 'A'));
+            columnNumber = (columnNumber) / 26;
         }
-        return result.reverse().toString();
+
+        // Reverse it, as we appended characters in reverse order.
+        return ans.reverse().toString();
     }
 }
