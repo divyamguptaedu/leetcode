4128db1ecc7a7c6fb0ad72ce29282cfccb91082a
Divyam Gupta <dgupta92@gatech.edu>
2024-06-22 18:52:23 -0700

    Add 231

--- easy/231. Power of Two/231_power_of_two.java
+++ easy/231. Power of Two/231_power_of_two.java
@@ -1,17 +1,14 @@
-/*
-if n is 1, true
-cal for n/2
-
-edge case -> n < 1
-		  -> remainder is not 0
-*/
-
-public boolean isPowerOfTwo(int n) {
-    if (n == 1) {
-    	return true;
+//I utilized a bitwise approach. First, I handled the edge case where n is 0, returning false immediately. 
+//Then, I cast n to a long integer x to handle the case where n is Integer.MIN_VALUE without overflow issues. 
+//The core logic checks if x and x - 1 bitwise AND results in 0, 
+//which only occurs if x is a power of two (i.e., has exactly one bit set to 1).
+//TC: CONSTANT
+//SC: CONSTANT
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0)
+            return false;
+        long x = (long) n;
+        return (x & (x - 1)) == 0;
     }
-    if ((n < 1) || (n % 2 != 0)) {
-    	return false; 
-    }
-    return isPowerOfTwo(n / 2);
 }
