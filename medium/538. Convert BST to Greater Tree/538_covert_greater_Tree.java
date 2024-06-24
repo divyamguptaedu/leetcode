393feaaaefc30518a3deb0af4b36844168c04c28
Divyam Gupta <dgupta92@gatech.edu>
2024-06-23 18:51:12 -0700

    Add 538

--- medium/538. Convert BST to Greater Tree/538_covert_greater_Tree.java
+++ medium/538. Convert BST to Greater Tree/538_covert_greater_Tree.java
@@ -1,28 +1,16 @@
-"
-Performance:
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Convert BST to Greater Tree.
-Memory Usage: 39.3 MB, less than 71.18% of Java online submissions for Convert BST to Greater Tree.
-
-"
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//In solving the problem of converting a Binary Search Tree (BST) into a Greater Tree, 
+//I employed a recursive approach. Starting from the rightmost node, 
+//I traversed the BST in reverse order (right subtree, root, left subtree). 
+//This allowed me to accumulate the sum of all greater keys encountered so far. 
+//For each node, I added this accumulated sum to the node's value, updating the node to reflect its new value. 
+//This approach leverages the properties of BSTs, 
+//ensuring that each node's new value includes the sum of all greater keys as required.
+//Time: n
+//Space: height of the tree
 class Solution {
-    
+
     int sum = 0;
-    
+
     public TreeNode convertBST(TreeNode root) {
         if (root == null) {
             return null;
