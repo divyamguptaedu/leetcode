fb5a543b15eddae88b42ba75173a3f005a177088
Divyam Gupta <dgupta92@gatech.edu>
2024-06-24 18:50:00 -0700

    Add 817

--- medium/817. Linked List Components/817_numComponents.java
+++ medium/817. Linked List Components/817_numComponents.java
@@ -1,41 +1,49 @@
-"
-Performance:
-Runtime: 15 ms, faster than 82.90% of Java online submissions for Linked List Components.
-Memory Usage: 54 MB, less than 60.90% of Java online submissions for Linked List Components.
-"
+//First, I stored the subset elements in a HashSet for quick lookup. 
+//Then, I traversed the linked list, counting segments where the elements are 
+//part of the subset and consecutive. 
+//I incremented the component count each time a sequence ended and reset the counter. 
+//At the end, if a sequence was still ongoing, I included it in the count.
+//Time: n where n is the num nodes in ll
+//Space: m where m is num elements in subset array nums
 
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public int numComponents(ListNode head, int[] nums) {
-        Map<Integer,Integer> valueMap = new HashMap<>();
-        for (int i:nums) {
-            valueMap.put(i,0);
+        // Store subset elements in a set for quick lookup
+        HashSet<Integer> subsetSet = new HashSet<>();
+        for (int num : nums) {
+            subsetSet.add(num);
         }
-        boolean color = false;
-        int count = 0;
-        if (valueMap.containsKey(head.val)) { 
-            color = true;
-        }
-        while (head != null) {
-            if (valueMap.containsKey(head.val)) {
-                if (color) {
-                    count++;
-                }
-                color = false;
-            } else {
-                color = true;
+        
+        int componentCount = 0;
+        int currentSequence = 0;
+        ListNode currentNode = head;
+
+        while (currentNode != null) {
+            // Check if current node's value is in the subset
+            if (subsetSet.contains(currentNode.val)) {
+                currentSequence++;
+            } else if (currentSequence > 0) {
+                // End of a connected component
+                componentCount++;
+                currentSequence = 0;
             }
-            head = head.next;
+            currentNode = currentNode.next;
         }
-        return count;
+        
+        // Check for an ongoing sequence at the end of the list
+        if (currentSequence == 0) {
+            return componentCount;
+        }
+        return componentCount + 1;
     }
 }
