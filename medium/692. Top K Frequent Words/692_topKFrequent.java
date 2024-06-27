034ee3ec96bf3493b00256fdfbb66c91376faa1e
Divyam Gupta <dgupta92@gatech.edu>
2024-06-26 17:54:14 -0700

    Update

--- medium/692. Top K Frequent Words/692_topKFrequent.java
+++ medium/692. Top K Frequent Words/692_topKFrequent.java
@@ -6,17 +6,6 @@
 
 //Time: nlogn
 //Space: n
-class CompareMapEntry implements Comparator<Map.Entry<String, Integer>> {
-    @Override
-    public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
-        if (entry2.getValue().equals(entry1.getValue())) {
-            return entry1.getKey().compareTo(entry2.getKey());
-        } else {
-            return entry2.getValue() - entry1.getValue();
-        }
-    }
-}
-
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         // Create a map to count the frequency of each word
@@ -24,14 +13,28 @@ class Solution {
         for (String word : words) {
             frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
         }
+        
         // Create a priority queue to sort the words by frequency and lexicographical order
-        PriorityQueue<Map.Entry<String, Integer>> sortedWords = new PriorityQueue<>(k, new CompareMapEntry());
+        PriorityQueue<Map.Entry<String, Integer>> sortedWords = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                if (entry2.getValue().equals(entry1.getValue())) {
+                    return entry1.getKey().compareTo(entry2.getKey());
+                } else {
+                    return entry2.getValue() - entry1.getValue();
+                }
+            }
+        });
+        
+        // Add all entries to the priority queue
         sortedWords.addAll(frequencyMap.entrySet());
+        
         // Extract the top k frequent words
         List<String> topKWords = new ArrayList<>();
-        for (int i = 0; i < k && sortedWords.size() > 0; i++) {
+        for (int i = 0; i < k && !sortedWords.isEmpty(); i++) {
             topKWords.add(sortedWords.poll().getKey());
         }
+        
         return topKWords;
     }
 }
