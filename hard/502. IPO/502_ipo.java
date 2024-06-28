4df3e96db3abe79f9c1d91e3d88eaa3bf1c81042
Divyam Gupta <dgupta92@gatech.edu>
2024-06-28 11:11:16 -0700

    Add 502

---
+++ hard/502. IPO/502_ipo.java
@@ -0,0 +1,48 @@
+//I approached the IPO problem by modeling each project with its required capital and 
+//potential profit using a custom Project class that implements Comparable to sort projects based on their 
+//capital requirements. Initially, I sorted all projects by their capital. 
+//Using a max heap (implemented with PriorityQueue and Collections.reverseOrder()), 
+//I dynamically tracked and selected up to k projects that I could afford with my current capital w. 
+//For each eligible project, its profit was added to w, and 
+//this process continued until either k projects were selected or no more projects could be chosen due 
+//to insufficient capital. Finally, w represents the maximum capital achievable after selecting the 
+//optimal set of projects.
+//Time: nlogn
+//Space: n
+class Solution {
+    class Project implements Comparable<Project> {
+        int capital, profit;
+
+        public Project(int capital, int profit) {
+            this.capital = capital;
+            this.profit = profit;
+        }
+
+        public int compareTo(Project project) {
+            return capital - project.capital;
+        }
+    }
+
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        Project[] projects = new Project[n];
+        for (int i = 0; i < n; i++) {
+            projects[i] = new Project(capital[i], profits[i]);
+        }
+        Arrays.sort(projects);
+        // PriorityQueue is a min heap, but we need a max heap, so we use
+        // Collections.reverseOrder()
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>(n, Collections.reverseOrder());
+        int ptr = 0;
+        for (int i = 0; i < k; i++) {
+            while (ptr < n && projects[ptr].capital <= w) {
+                q.add(projects[ptr++].profit);
+            }
+            if (q.isEmpty()) {
+                break;
+            }
+            w += q.poll();
+        }
+        return w;
+    }
+}
