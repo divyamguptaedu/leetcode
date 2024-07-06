91b41dbf14ae3f6e49397ed6c1ae72ef856ee995
Divyam Gupta <dgupta92@gatech.edu>
2024-07-06 15:18:26 -0700

    Add 2416

--- hard/2416. Sum of Prefix Scores of Strings/2416_sumPrefixScores.java
+++ hard/2416. Sum of Prefix Scores of Strings/2416_sumPrefixScores.java
@@ -1,27 +1,22 @@
-//I approached the problem of calculating prefix scores for each string in the array by using a Trie 
-//data structure. Initially, I built a Trie where each node keeps track of how many times its 
-//corresponding prefix appears in the array of words. During insertion of each word into the Trie, 
-//I incremented counters at each node representing the prefix. Then, for each word, 
-//I traversed the Trie again to compute the cumulative count of all prefixes leading up to each character 
-//of the word. This count directly gives the prefix score for that word. Finally, 
-//I stored these scores in an array and returned it as the result.
+//I approached the problem of calculating prefix scores for each string in the array by using a Trie data structure. 
+//Initially, I built a Trie where each node keeps track of how many times its corresponding prefix appears in the array of words. 
+//During insertion of each word into the Trie, 
+//I incremented counters at each node representing the prefix. 
+//Then, for each word, I traversed the Trie again to compute the cumulative count of all prefixes leading up to each character of the word. 
+//This count directly gives the prefix score for that word. 
+//Finally, I stored these scores in an array and returned it as the result.
 //Time: nl where n is num of words and l is avg len of words
 //Space: nl
-class Solution {
-    public int[] sumPrefixScores(String[] words) {
-        if (words.length == 1) {
-            return new int[] { words[0].length() };
-        }
 
-        Trie trie = new Trie();
-        for (String word : words) {
-            trie.addWord(word);
-        }
-        int[] ans = new int[words.length];
-        for (int i = 0; i < words.length; i++) {
-            ans[i] = trie.getPrefixCount(words[i]);
-        }
-        return ans;
+class TrieNode {
+    final static int R = 26;
+
+    int prefixCount;
+    TrieNode[] children;
+
+    TrieNode() {
+        prefixCount = 0;
+        children = new TrieNode[R];
     }
 }
 
@@ -59,14 +54,20 @@ class Trie {
     }
 }
 
-class TrieNode {
-    final static int R = 26;
+class Solution {
+    public int[] sumPrefixScores(String[] words) {
+        if (words.length == 1) {
+            return new int[] { words[0].length() };
+        }
 
-    int prefixCount;
-    TrieNode[] children;
-
-    TrieNode() {
-        prefixCount = 0;
-        children = new TrieNode[R];
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.addWord(word);
+        }
+        int[] ans = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            ans[i] = trie.getPrefixCount(words[i]);
+        }
+        return ans;
     }
 }
