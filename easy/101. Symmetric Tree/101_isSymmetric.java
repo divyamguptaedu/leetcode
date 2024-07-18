1bee44c48e8ebdaa755fdaeba5314cdfef2944fa
Divyam Gupta <dgupta92@gatech.edu>
2024-07-17 22:15:39 -0700

    Add 101

--- easy/101. Symmetric Tree/101_isSymmetric.java
+++ easy/101. Symmetric Tree/101_isSymmetric.java
@@ -1,6 +1,3 @@
-//Used a simple recursive approach by checking if the two nodes at mirroring positions have equal value. 
-//The two nodes considered are derived by taking the 
-//leftTree.left with rightTree.right and leftTree.right with rightTree.left.
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,8 +13,7 @@
  *     }
  * }
  */
-//Time : O(n)
-//Space : O(n)
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         return helper(root, root);
@@ -30,6 +26,8 @@ class Solution {
         if (leftTree == null || rightTree == null) {
             return false;
         }
-        return (leftTree.val == rightTree.val) && helper(leftTree.right, rightTree.left) && helper(leftTree.left, rightTree.right);
+        return (leftTree.val == rightTree.val) && 
+            helper(leftTree.right, rightTree.left) && 
+            helper(leftTree.left, rightTree.right);
     }
 }
