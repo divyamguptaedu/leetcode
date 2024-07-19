756c0a0d563db54693509dbce7a070c0d785f682
Divyam Gupta <dgupta92@gatech.edu>
2024-07-18 22:11:44 -0700

    Add 1644

--- medium/1644. Lowest Common Ancestor of a Binary Tree II/1644_lca.java
+++ medium/1644. Lowest Common Ancestor of a Binary Tree II/1644_lca.java
@@ -1,7 +1,3 @@
-//Used the same approach as 236. Already had the edge cases covered in the earlier code.
-//Used recursion. Made three pointers, left, right, and mid. 
-//They are assigned 1 or 0, 1 means that p or q is found, 0 means not found. 
-//When a node gets two of three pointers as 1, then lca is found.
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -11,35 +7,40 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-//Time: O(n)
-//Space: O(n)
 class Solution {
-    TreeNode lca;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        recurseTree(root, p, q);
-        return lca;
-    }
-    public int recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
-        if (currentNode == null) {
-            return 0;
+        if (root == null) {
+            return null;
         }
-        int left = this.recurseTree(currentNode.left, p, q);
-        int right = this.recurseTree(currentNode.right, p, q);
-        int mid;
-        if (currentNode == p || currentNode == q) {
-            mid = 1;
-        } else {
-            mid = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        Map<TreeNode, TreeNode> ancestors = new HashMap<>();
+        stack.push(root);
+        ancestors.put(root, null);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                ancestors.put(node.left, node);
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                ancestors.put(node.right, node);
+                stack.push(node.right);
+            }
+        }
+        if (!ancestors.containsKey(p) || !ancestors.containsKey(q)) {
+            return null;
+        }
+        HashSet<TreeNode> parents = new HashSet<>();
+        
+        while (p != null) {
+            parents.add(p);
+            p = ancestors.get(p);
         }
 
-        if (mid + left + right >= 2) { //If we have two 1s in the three variables, then we found the lca.
-            lca = currentNode;
+        while (!parents.contains(q)) {
+            q = ancestors.get(q);
         }
-
-        if (mid + left + right > 0) { // Return 1 if any one of the three values is 1. 
-            return 1;
-        } else {
-            return 0;
-        }
+        return q;
     }
 }
