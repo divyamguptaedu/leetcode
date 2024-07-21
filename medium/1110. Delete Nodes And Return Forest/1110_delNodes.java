b98985675f636e871b1351ead3048f186f3ee80a
Divyam Gupta <dgupta92@gatech.edu>
2024-07-20 21:51:16 -0700

    Add 1110

--- medium/1110. Delete Nodes And Return Forest/1110_delNodes.java
+++ medium/1110. Delete Nodes And Return Forest/1110_delNodes.java
@@ -1,12 +1,3 @@
-//Added the nodes to be deleted in a hashSet to make 'contains' an O(1) operation. 
-//Did a DFS traversal of the tree which just checks if the currentNode is in the hashset, 
-//if yes, then I check if there are left and right child nodes present, 
-//if yes, they are added to the root list and the main node is changed to null to make it as deleted. 
-//Finally in the end, the root is checked if it is null, 
-//if not, it is added to the root list as well and returned.
-//Time: O(n)
-//Space: O(n)
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -23,35 +14,46 @@
  * }
  */
 class Solution {
-    List<TreeNode> result = new ArrayList();
-    Set<Integer> set = new HashSet();
-    
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        for (int i : to_delete) { //adding the nodes in to_delete to a set to make the contains operation O(1)
-            set.add(i);
+        List<TreeNode> result = new ArrayList<>();
+        if (root == null) {
+            return result;
         }
-        root = helper(root);
-        if (root != null) {
+
+        Set<Integer> toDelete = new HashSet<>();
+        for (int val : to_delete) {
+            toDelete.add(val);
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                queue.add(node.left);
+                if (toDelete.contains(node.left.val)) {
+                    node.left = null;
+                }
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+                if (toDelete.contains(node.right.val)) {
+                    node.right = null;
+                }
+            }
+            if (toDelete.contains(node.val)) {
+                if (node.left != null) {
+                    result.add(node.left);
+                }
+                if (node.right != null) {
+                    result.add(node.right);
+                }
+            }
+        }
+        if (!toDelete.contains(root.val)) {
             result.add(root);
         }
         return result;
-    }
-    
-    private TreeNode helper(TreeNode node) {
-        if (node == null) {
-            return null;
-        }
-        node.left = helper(node.left);
-        node.right = helper(node.right);
-        if (set.contains(node.val)) {
-            if (node.left != null) {
-                result.add(node.left); //child node of the deleted node will become a root
-            }
-            if (node.right != null) {
-                result.add(node.right); //child node of the deleted node will become a root
-            }
-            node = null;
-        }
-        return node;
     }
 }
