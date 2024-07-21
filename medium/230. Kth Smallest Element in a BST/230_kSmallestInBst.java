0ae957c8ab013b40bcb3ce3a1cc15088dae36da4
Divyam Gupta <dgupta92@gatech.edu>
2024-07-20 21:49:51 -0700

    Add 230

--- medium/230. Kth Smallest Element in a BST/230_kSmallestInBst.java
+++ medium/230. Kth Smallest Element in a BST/230_kSmallestInBst.java
@@ -1,11 +1,3 @@
-//I solved the problem by implementing an in-order traversal for the binary search tree (BST), 
-//which processes nodes in ascending order. I defined a helper function that recursively traversed the left subtree, 
-//processed the current node, and then traversed the right subtree. I used a counter to track the number of nodes visited. 
-//When the counter equaled k, I recorded the current node as the kth smallest element. 
-//The main method called this helper function and returned the value of the kth smallest node.
-//Time: n
-//Space: h where h is the height of the tree.
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -21,31 +13,24 @@
  *     }
  * }
  */
-
 class Solution {
-    // Variable to store the kth smallest node
-    TreeNode kthSmallestNode;
-    // Counter to track the number of visited nodes
-    int visitedCount = 0;
-
+    int k;
+    int result;
     public int kthSmallest(TreeNode root, int k) {
-        findKthSmallest(root, k);
-        return kthSmallestNode.val;
+        this.k = k;
+        inorderTraversal(root);
+        return result;
     }
 
-    private void findKthSmallest(TreeNode currentNode, int k) {
-        if (currentNode == null || kthSmallestNode != null) {
+    private void inorderTraversal(TreeNode node) {
+        if (node == null) {
             return;
         }
-        // Traverse left subtree
-        findKthSmallest(currentNode.left, k);
-        visitedCount++;
-        // Check if current node is the kth smallest
-        if (k == visitedCount) {
-            kthSmallestNode = currentNode;
-            return;
+        inorderTraversal(node.left);
+        k--;
+        if (k == 0) {
+            result = node.val;
         }
-        // Traverse right subtree
-        findKthSmallest(currentNode.right, k);
+        inorderTraversal(node.right);
     }
 }
