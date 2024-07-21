3bf660e840ce96146d905a16034a4f069abd86b6
Divyam Gupta <dgupta92@gatech.edu>
2024-07-20 21:50:54 -0700

    Add 129

--- medium/129. Sum Root to Leaf Numbers/129_sum_tree.java
+++ medium/129. Sum Root to Leaf Numbers/129_sum_tree.java
@@ -1,11 +1,4 @@
 /**
-Used DFS because we need to go to the leaf node directly. 
-Because we want the left node after the root, pre-order works the best. 
-Ensured that I am keeping track of the number by multiplying the number by 10 
-and then adding the value of the current node. 
-Made sure to add the number to the result if there are no child nodes of a particular node.
-*/
-/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -20,24 +13,28 @@ Made sure to add the number to the result if there are no child nodes of a parti
  *     }
  * }
  */
-//Time: O(n)
-//Space: O(h) : h is the height of the tree, recursion stack.
 class Solution {
-    int output = 0;
-    
     public int sumNumbers(TreeNode root) {
-        preorder(root, 0);
-        return output;
-    }
-
-    public void preorder(TreeNode node, int numberSoFar) {
-        if (node != null) {
-            numberSoFar = numberSoFar * 10 + node.val;
-            if (node.left == null && node.right == null) { //add to the result if no childs present
-                output += numberSoFar;
-            }
-            preorder(node.left, numberSoFar);
-            preorder(node.right, numberSoFar) ;
+        if (root == null) {
+            return 0;
         }
+        int result = 0;
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair(root, root.val));
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> nodePair = stack.pop();
+            TreeNode node = nodePair.getKey();
+            Integer value = nodePair.getValue();
+            if (node.left == null && node.right == null) {
+                result += value;
+            }
+            if (node.left != null) {
+                stack.push(new Pair(node.left, value * 10 + node.left.val));
+            }
+            if (node.right != null) {
+                stack.push(new Pair(node.right, value * 10 + node.right.val));
+            }
+        }
+        return result;
     }
 }
