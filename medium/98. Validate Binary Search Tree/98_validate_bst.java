1d8287bf54ee2a442a2c04a649bf5bbd2c5ab880
Divyam Gupta <dgupta92@gatech.edu>
2024-07-20 21:49:06 -0700

    Add 98

--- medium/98. Validate Binary Search Tree/98_validate_bst.java
+++ medium/98. Validate Binary Search Tree/98_validate_bst.java
@@ -1,35 +1,24 @@
-//Used a simple recursive approach by keeping track of the minimum and 
-//maximum possible value for each node and then checking if it lies in between them, inclusive.
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-//Time : O(n)
-//Space : O(n)
 class Solution {
+    Integer prev;
     public boolean isValidBST(TreeNode root) {
-        return validate(root, null, null);  
+        prev = null;
+        return inorder(root);
     }
 
-    public boolean validate(TreeNode root, Integer minimumValue, Integer maximumValue) {
+    private boolean inorder(TreeNode root) {
         if (root == null) {
             return true;
         }
-        if ((minimumValue != null && root.val <= minimumValue) || (maximumValue != null && root.val >= maximumValue)) {
+        if (inorder(root.left) == false) {
             return false;
         }
-
-        return validate(root.left, minimumValue, root.val) && validate(root.right, root.val, maximumValue);
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        prev = root.val;
+        if (inorder(root.right) == false) {
+            return false;
+        }
+        return true;
     }
 }
