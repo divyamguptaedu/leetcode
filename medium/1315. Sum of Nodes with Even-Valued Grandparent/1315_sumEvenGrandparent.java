58273b92275bf887dfade327e3bd0ab46e69c2b8
Divyam Gupta <dgupta92@gatech.edu>
2024-07-20 21:50:43 -0700

    Add 1315

--- medium/1315. Sum of Nodes with Even-Valued Grandparent/1315_sumEvenGrandparent.java
+++ medium/1315. Sum of Nodes with Even-Valued Grandparent/1315_sumEvenGrandparent.java
@@ -1,5 +1,3 @@
-//Used a simple BFS traversal approach in which I added the values of the grandchildren 
-//to the sum variable if the parent's value is even. 
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,45 +13,41 @@
  *     }
  * }
  */
- //Time: O(n)
- //Space: O(n)
 class Solution {
     public int sumEvenGrandparent(TreeNode root) {
+        int sum = 0;
         if (root == null) {
-            return 0;
+            return sum;
         }
-        
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        
-        int sum = 0;
         while (!queue.isEmpty()) {
-            TreeNode currentNode = queue.remove();
-            if (currentNode.val % 2 == 0) {
-                if (currentNode.left != null) {
-                    if (currentNode.left.left != null) {
-                        sum += currentNode.left.left.val;
+            TreeNode node = queue.poll();
+            if (node.val % 2 == 0) {
+                if (node.left != null) {
+                    if (node.left.left != null) {
+                        sum += node.left.left.val;
                     }
-                    if (currentNode.left.right != null) {
-                        sum += currentNode.left.right.val;
+                    if (node.left.right != null) {
+                        sum += node.left.right.val;
                     }
                 }
-                if (currentNode.right != null) {
-                    if (currentNode.right.left != null) {
-                        sum += currentNode.right.left.val;
+                if (node.right != null) {
+                    if (node.right.left != null) {
+                        sum += node.right.left.val;
                     }
-                    if (currentNode.right.right != null) {
-                        sum += currentNode.right.right.val;
+                    if (node.right.right != null) {
+                        sum += node.right.right.val;
                     }
                 }
             }
-
-            if (currentNode.left != null) 
-                queue.add(currentNode.left);
-            if (currentNode.right != null)
-                queue.add(currentNode.right);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
         }
-        
         return sum;
     }
 }
