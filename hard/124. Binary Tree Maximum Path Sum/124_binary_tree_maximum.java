61cae225295aa0d1d41ef1565666ae39f4c9d77b
Divyam Gupta <dgupta92@gatech.edu>
2024-07-21 22:18:41 -0700

    Add 124

--- hard/124. Binary Tree Maximum Path Sum/124_binary_tree_maximum.java
+++ hard/124. Binary Tree Maximum Path Sum/124_binary_tree_maximum.java
@@ -37,3 +37,38 @@ class Solution {
         return Math.max(leftSum + root.val, rightSum + root.val);
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxSum;
+    public int maxPathSum(TreeNode root) {
+        maxSum = Integer.MIN_VALUE;
+        traverse(root);
+        return maxSum;
+    }
+
+    private int traverse(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = Math.max(traverse(node.left), 0);
+        int right = Math.max(traverse(node.right), 0);
+        maxSum = Math.max(maxSum, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}
