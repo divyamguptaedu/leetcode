605e357c1bd437dd7407b2663e6d5ebc293f8ac2
Divyam Gupta <dgupta92@gatech.edu>
2024-07-21 22:18:03 -0700

    Add 536

--- medium/536. Construct Binary Tree from String/526_str2tree.java
+++ medium/536. Construct Binary Tree from String/526_str2tree.java
@@ -5,65 +5,129 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
+// class Solution {
+//     public TreeNode str2tree(String s) {
+//         if (s.isEmpty()) {
+//             return null;
+//         }
+//         String[] threeStrings = getThreeStrings(s);
+//         String nodeValue = threeStrings[0];
+//         String leftValue = threeStrings[1];
+//         String rightValue = threeStrings[2];
+//         return new TreeNode(Integer.parseInt(nodeValue),
+//                 leftValue.isEmpty() ? null : str2tree(leftValue),
+//                 rightValue.isEmpty() ? null : str2tree(rightValue));
+//     }
 
-// Time: O(n)
-// Space: O(h) h is the height of the tree
+//     private String[] getThreeStrings(String s) {
+//         if (s.isEmpty()) {
+//             return null;
+//         }
+//         String nodeValue = "";
+//         String leftValue = "";
+//         String rightValue = "";
+//         int index = 0;
+//         while (index < s.length() && s.charAt(index) != '(') {
+//             nodeValue += s.charAt(index);
+//             index++;
+//         }
+//         int openParenthesis = 1;
+//         index++;
+//         while (index < s.length() && openParenthesis != 0) {
+//             if (s.charAt(index) == ')' && openParenthesis == 1) {
+//                 openParenthesis--;
+//             } else {
+//                 if (s.charAt(index) == '(') {
+//                     openParenthesis++;
+//                 }
+//                 if (s.charAt(index) == ')') {
+//                     openParenthesis--;
+//                 }
+//                 leftValue += s.charAt(index);
+//             }
+//             index++;
+//         }
+//         openParenthesis = 1;
+//         index++;
+//         while (index < s.length() && openParenthesis != 0) {
+//             if (s.charAt(index) == ')' && openParenthesis == 1) {
+//                 openParenthesis--;
+//             } else {
+//                 if (s.charAt(index) == '(') {
+//                     openParenthesis++;
+//                 }
+//                 if (s.charAt(index) == ')') {
+//                     openParenthesis--;
+//                 }
+//                 rightValue += s.charAt(index);
+//             }
+//             index++;
+//         }
+//         String[] threeStrings = new String[3];
+//         threeStrings[0] = nodeValue;
+//         threeStrings[1] = leftValue;
+//         threeStrings[2] = rightValue;
+//         return threeStrings;
+//     }
+// }
+
 class Solution {
-    int index = 0;
+    private int i;
 
     public TreeNode str2tree(String s) {
-        if (s == null || s.equals("")) {
+        if (s.length() == 0) {
             return null;
         }
-        TreeNode root = new TreeNode();
-        return buildTree(root, s);
-    }
-    
-    private TreeNode buildTree(TreeNode root, String s) {
-        if (index >= s.length()) {
-            return root;
-        }        
-        if (Character.isDigit(s.charAt(index))) {
-            root = new TreeNode(getNum(s));
-        }
-        if (index < s.length() && s.charAt(index) == '-') {
-            index++;
-            root = new TreeNode(getNum(s));
-            root.val *= -1;
-        }
-        if (index < s.length() && s.charAt(index) == '(') {
-            index++;
-            root.left = str2tree(s);
-            if (index < s.length() && s.charAt(index) == '(') {
-                index++;
-                root.right = str2tree(s);
-            }
-        }
-        
-        if (index < s.length() && s.charAt(index) == ')') {
-            index++;
-        }
-        return root;
+        i = 0;
+        return buildTree(s);
     }
 
-    private int getNum(String s) {
-        int num = 0;
-        while (index < s.length() && Character.isDigit(s.charAt(index))) {
-            num = num * 10 + s.charAt(index) - '0';
-            index++;
+    private TreeNode buildTree(String s) {
+        if (i >= s.length()) {
+            return null;
         }
-        return num;
+        int val = 0;
+        boolean negative = false;
+        if (s.charAt(i) == '-') {
+            negative = true;
+            i++;
+        }
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            val = val * 10 + s.charAt(i) - '0';
+            i++;
+        }
+        TreeNode newNode = new TreeNode(negative ? -val : val);
+        if (i >= s.length() || s.charAt(i) == ')') {
+            i++;
+            return newNode;
+        }
+        if (s.charAt(i) == '(') {
+            i++;
+            newNode.left = buildTree(s);
+        }
+        if (i >= s.length() || s.charAt(i) == ')') {
+            i++;
+            return newNode;
+        }
+        if (s.charAt(i) == '(') {
+            i++;
+            newNode.right = buildTree(s);
+        }
+        if (i < s.length() && s.charAt(i) == ')') {
+            i++;
+        }
+        return newNode;
     }
 }
