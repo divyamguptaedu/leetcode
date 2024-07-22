85e30059453cf441c115ac2243449556cd75fe7f
Divyam Gupta <dgupta92@gatech.edu>
2024-07-21 22:18:51 -0700

    Add 543

--- easy/543. Diameter of Binary Tree/543_diameter_of_binary_tree.java
+++ easy/543. Diameter of Binary Tree/543_diameter_of_binary_tree.java
@@ -40,3 +40,37 @@ class Solution {
         return Math.max(leftPath, rightPath) + 1; //actual logic, for every node, get the height of the left node, then height of the right node, add one to find the height of the currNode.
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {
+        this.diameter = 0;
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        diameter = Math.max(diameter, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
