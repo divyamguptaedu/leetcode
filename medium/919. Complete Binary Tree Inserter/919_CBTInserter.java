02b5b77caec4fc3b73de746758bf90663cac57d7
Divyam Gupta <dgupta92@gatech.edu>
2024-07-22 20:56:43 -0700

    Add 919

--- medium/919. Complete Binary Tree Inserter/919_CBTInserter.java
+++ medium/919. Complete Binary Tree Inserter/919_CBTInserter.java
@@ -58,6 +58,70 @@ class CBTInserter {
     }
 }
 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class CBTInserter {
+    TreeNode root;
+    Deque<TreeNode> deque;
+    public CBTInserter(TreeNode root) {
+        this.root = root;
+        this.deque = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left == null || node.right == null) {
+                deque.add(node);
+            }
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+    
+    public int insert(int val) {
+        TreeNode newNode = new TreeNode(val);
+        TreeNode parentNode = deque.peekFirst();
+        deque.add(newNode);
+        if (parentNode.left == null) {
+            parentNode.left = newNode;
+        } else {
+            parentNode.right = newNode;
+            deque.poll();
+        }
+        return parentNode.val;
+    }
+    
+    public TreeNode get_root() {
+        return this.root;
+    }
+}
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter obj = new CBTInserter(root);
+ * int param_1 = obj.insert(val);
+ * TreeNode param_2 = obj.get_root();
+ */
+
+
 /**
  * Your CBTInserter object will be instantiated and called as such:
  * CBTInserter obj = new CBTInserter(root);
