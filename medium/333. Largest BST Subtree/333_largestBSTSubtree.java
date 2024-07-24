3488c96487d04eb39efd548c586639645d81acb3
Divyam Gupta <dgupta92@gatech.edu>
2024-07-23 22:41:36 -0700

    Add 333

--- medium/333. Largest BST Subtree/333_largestBSTSubtree.java
+++ medium/333. Largest BST Subtree/333_largestBSTSubtree.java
@@ -45,3 +45,32 @@ class Solution {
         return result;
     }
 }
+
+class NodeValue {
+    public int maxNode, minNode, maxSize;
+    
+    NodeValue(int minNode, int maxNode, int maxSize) {
+        this.minNode = minNode;
+        this.maxNode = maxNode;
+        this.maxSize = maxSize;
+    }
+};
+
+class Solution {
+
+    public int largestBSTSubtree(TreeNode root) {
+        return largestBSTSubtreeHelper(root).maxSize;
+    }
+
+    public NodeValue largestBSTSubtreeHelper(TreeNode root) {
+        if (root == null) {
+            return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE, 0);
+        }
+        NodeValue left = largestBSTSubtreeHelper(root.left);
+        NodeValue right = largestBSTSubtreeHelper(root.right);
+        if (left.maxNode < root.val && root.val < right.minNode) {
+            return new NodeValue(Math.min(root.val, left.minNode), Math.max(root.val, right.maxNode), left.maxSize + right.maxSize + 1);
+        }
+        return new NodeValue(Integer.MIN_VALUE, Integer.MAX_VALUE, Math.max(left.maxSize, right.maxSize));
+    }
+}
