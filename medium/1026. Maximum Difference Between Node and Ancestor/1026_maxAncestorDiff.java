aa2ce295706deefb748fb4609ac2b03e60e39a53
Divyam Gupta <dgupta92@gatech.edu>
2024-07-23 22:40:59 -0700

    Add 1026

--- medium/1026. Maximum Difference Between Node and Ancestor/1026_maxAncestorDiff.java
+++ medium/1026. Maximum Difference Between Node and Ancestor/1026_maxAncestorDiff.java
@@ -45,3 +45,38 @@ class Solution {
         return Math.max(left, right);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDiff;
+    public int maxAncestorDiff(TreeNode root) {
+        maxDiff = 0;
+        traverse(root, root.val, root.val);
+        return maxDiff;
+    }
+
+    private void traverse(TreeNode node, int currMax, int currMin) {
+        if (node == null) {
+            return;
+        }
+        maxDiff = Math.max(maxDiff, Math.max(Math.abs(currMax - node.val), Math.abs(currMin - node.val)));
+        currMax = Math.max(currMax, node.val);
+        currMin = Math.min(currMin, node.val);
+        traverse(node.left, currMax, currMin);
+        traverse(node.right, currMax, currMin);
+    }
+}
