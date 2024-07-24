2e6e84be444bf3df1f8556e3303144fb525e1af3
Divyam Gupta <dgupta92@gatech.edu>
2024-07-23 22:41:18 -0700

    Add 623

--- medium/623. Add One Row to Tree/623._addOneRowjava.java
+++ medium/623. Add One Row to Tree/623._addOneRowjava.java
@@ -57,3 +57,57 @@ class Solution {
         return root;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            if (depth == 1) {
+                return new TreeNode(val);
+            }
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int currentDepth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (currentDepth == depth - 1) {
+                    TreeNode originalLeft = node.left;
+                    TreeNode originalRight = node.right;
+                    node.left = new TreeNode(val);
+                    node.right = new TreeNode(val);
+                    node.left.left = originalLeft;
+                    node.right.right = originalRight;
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            currentDepth++;
+        }
+        return root;
+    }
+}
