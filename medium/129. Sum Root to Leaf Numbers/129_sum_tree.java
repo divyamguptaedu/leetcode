2a6fb7b55d3c10fe3737cb5f0ae4359220b7c555
Divyam Gupta <dgupta92@gatech.edu>
2024-07-29 23:02:49 -0700

    Add 129

--- medium/129. Sum Root to Leaf Numbers/129_sum_tree.java
+++ medium/129. Sum Root to Leaf Numbers/129_sum_tree.java
@@ -13,28 +13,49 @@
  *     }
  * }
  */
+
 class Solution {
+    int sum;
     public int sumNumbers(TreeNode root) {
-        if (root == null) {
-            return 0;
+        sum = 0;
+        traverse(root, 0);
+        return sum;
+    }
+
+    private void traverse(TreeNode node, int valueTillNow) {
+        if (node == null) {
+            return;
         }
-        int result = 0;
-        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
-        stack.push(new Pair(root, root.val));
-        while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> nodePair = stack.pop();
-            TreeNode node = nodePair.getKey();
-            Integer value = nodePair.getValue();
-            if (node.left == null && node.right == null) {
-                result += value;
-            }
-            if (node.left != null) {
-                stack.push(new Pair(node.left, value * 10 + node.left.val));
-            }
-            if (node.right != null) {
-                stack.push(new Pair(node.right, value * 10 + node.right.val));
-            }
+        if (node.left == null && node.right == null) {
+            sum += (valueTillNow * 10) + node.val;
         }
-        return result;
+        traverse(node.left, (valueTillNow * 10) + node.val);
+        traverse(node.right, (valueTillNow * 10) + node.val);
     }
 }
+
+// class Solution {
+//     public int sumNumbers(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+//         int result = 0;
+//         Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+//         stack.push(new Pair(root, root.val));
+//         while (!stack.isEmpty()) {
+//             Pair<TreeNode, Integer> nodePair = stack.pop();
+//             TreeNode node = nodePair.getKey();
+//             Integer value = nodePair.getValue();
+//             if (node.left == null && node.right == null) {
+//                 result += value;
+//             }
+//             if (node.left != null) {
+//                 stack.push(new Pair(node.left, value * 10 + node.left.val));
+//             }
+//             if (node.right != null) {
+//                 stack.push(new Pair(node.right, value * 10 + node.right.val));
+//             }
+//         }
+//         return result;
+//     }
+// }
