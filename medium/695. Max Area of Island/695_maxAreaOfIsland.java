09f357e82ba6155e8ddf22a05534e79c400d416d
Divyam Gupta <dgupta92@gatech.edu>
2024-07-30 22:29:44 -0700

    Add 695

--- medium/695. Max Area of Island/695_maxAreaOfIsland.java
+++ medium/695. Max Area of Island/695_maxAreaOfIsland.java
@@ -25,3 +25,40 @@ class Solution {
         return (1 + area(r+1, c) + area(r-1, c) + area(r, c-1) + area(r, c+1));
     }
 }
+
+//implemented again
+class Solution {
+    int[][] grid;
+    int[][] dirs;
+    int rows;
+    int cols;
+    int maxSize;
+    int currSize;
+    public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
+        this.dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        this.maxSize = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    currSize = 0;
+                    findSize(i, j);
+                }
+            }
+        }
+        return maxSize;
+    }
+
+    private void findSize(int i, int j) {
+        if (i >= 0 && i < rows && j >= 0 && j < cols && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            currSize++;
+            maxSize = Math.max(maxSize, currSize);
+            for (int[] dir : dirs) {
+                findSize(i + dir[0], j + dir[1]);
+            }
+        }
+    }
+}
