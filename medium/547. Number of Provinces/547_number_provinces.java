294d1ee61dbf0a0995746262b190674ad089ee34
Divyam Gupta <dgupta92@gatech.edu>
2024-08-01 22:06:06 -0700

    Add 547

--- medium/547. Number of Provinces/547_number_provinces.java
+++ medium/547. Number of Provinces/547_number_provinces.java
@@ -29,3 +29,30 @@ class Solution {
         }
     }
 }
+
+//another solution
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int result = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                result++;
+                dfs(i, isConnected, visited);
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(int node, int[][] isConnected, boolean[] visited) {
+        visited[node] = true;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[node][i] == 1 && !visited[i]) {
+                dfs(i, isConnected, visited);
+            }
+        }
+    }
+}
