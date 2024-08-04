60fcd03655f51bcfc610c51c20fe095c9ec983fa
Divyam Gupta <dgupta92@gatech.edu>
2024-08-03 21:36:27 -0700

    Add 994

--- medium/994. Rotting Oranges/994_rotting_oranges.java
+++ medium/994. Rotting Oranges/994_rotting_oranges.java
@@ -1,58 +1,50 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        int freshOranges = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) { // if a rotten orange, add to queue
+                    queue.add(new Pair(i, j));
+                } else if (grid[i][j] == 1) { // if a fresh orange, increment fresh orange count
+                    freshOranges++;
+                }
+            }
         }
-        Queue<int[]> queue = new LinkedList<>();
-        int countFresh = 0;
-        int row = grid.length;
-        int col = grid[0].length;
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 2) {
-                    queue.offer(new int[] {i, j});
-                }
-                if (grid[i][j] == 1) {
-                    countFresh++;
-                }
-            }
-        }
-        if (countFresh == 0) {
-            return 0;
-        }
-        int time = 0;
-        int[] directionX = {0, 0, 1, -1};
-        int[] directionY = {1, -1, 0, 0};
-        int count = 0 - queue.size();
+        queue.add(new Pair(-1, -1)); // to mark one round
+
+        int minutes = -1;
+        int[][] dirs = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            count += size;
-            for (int i = 0; i < size; i++) {
-                int[] popped = queue.poll();
-                for (int j = 0; j < 4; j++) {
-                    int x = popped[0] + directionX[j];
-                    int y = popped[1] + directionY[j];
-                    if (x < 0 || y < 0 || x >= row || y >= col || grid[x][y] == 0 || grid[x][y] == 2) {
-                        continue;
+            Pair<Integer, Integer> curr = queue.poll();
+            int row = curr.getKey();
+            int col = curr.getValue();
+            if (row == -1) { // marking a new round
+                minutes++;
+                if (!queue.isEmpty()) {
+                    queue.add(new Pair(-1, -1));
+                }
+            } else { // explore all adj cells which have fresh orange
+                for (int[] dir : dirs) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] == 1) {
+                        grid[newRow][newCol] = 2; // convert to rotten
+                        freshOranges--; // decrement fresh count
+                        queue.add(new Pair(newRow, newCol)); // add to queue for next round
                     }
-                    grid[x][y] = 2;
-                    queue.offer(new int[] {x, y});
                 }
             }
-            if (queue.size() != 0) {
-                time++;
-            }
         }
-        if (countFresh == count) {
-            return time;
+
+        if (freshOranges == 0) {
+            return minutes;
         } else {
             return -1;
         }
     }
 }
-
-
-
-
-
